3cc2b4ab48dc543196618a4cccf130279bb923c0
Steve Gifford <sjg@mousebirdconsulting.com>
2012-07-25 13:01:59 -0700

    Added default visual descriptions for the various data types in the Component.  Also added a rendering hints dictionary and hooked up z buffering.

--- WhirlyGlobeSrc/WhirlyGlobeComponent/include/WhirlyGlobeViewController.h
+++ WhirlyGlobeSrc/WhirlyGlobeComponent/include/WhirlyGlobeViewController.h
@@ -26,6 +26,53 @@
 @class WGComponentObject;
 @class WhirlyGlobeViewController;
 
+/// Use this hint to turn the zbuffer on or off.  Pass in an NSNumber boolean.  Takes effect on the next frame.
+#define kWGRenderHintZBuffer @"zbuffer"
+
+/// These are used for all object descriptions.
+
+/// If the z buffer is on, this will let you resolve.  Takes an NSNumber boolean
+#define kWGDrawOffset @"drawOffset"
+/// This helps decide what order things are drawn in.  Useful when the z buffer is off or you're using transparency.
+/// Takes an NSNumber int.
+#define kWGDrawPriority @"drawPriority"
+/// Minimum point at which a feature is visible.  Takes an NSNumber float.  The radius of the globe is 1.0
+#define kWGMinVis @"minVis"
+/// Maximum point at which a feature is visible.  Takes an NSNumber float.  The radius of the globe is 1.0
+#define kWGMaxVis @"maxVis"
+/// The amount of time for a feature to fade in or out.  Takes an NSNumber float for seconds.
+#define kWGFade @"fade"
+
+/// Default draw offset for 3D markers.  Set to avoid label/marker intererence
+#define kWGMarkerDrawOffsetDefault 1
+
+/// These are used just for the screen and regular labels
+
+/// Color of the text being rendered.  Takes a UIColor.
+#define kWGTextColor @"textColor"
+/// Background color for the text.  Takes a UIColor.
+#define kWGBackgroundColor @"backgroundColor"
+/// Font to use in rendering text.  Takes a UIFont.
+#define kWGFont @"font"
+/// Default height of the text.  If for screen space, this in points.  If for 3D, remember that
+//   the radius of the globe is 1.0.  Expects an NSNumber float.
+#define kWGLabelHeight @"height"
+/// Default width of the text.  See height for more info and, in general, use height instead.
+#define kWGLabelWidth @"width"
+/// Justification for label placement.  This takes an NSString with one of:
+///  middle, left, right
+#define kWGJustify @"justify"
+
+/// Default draw offset for 3D labels.  This is set to avoid label/marker interference
+#define kWGLabelDrawOffsetDefault 2
+
+/// These are used for screen and regular markers.
+
+/// Color is used for the polygon generated for a marker.  It will combine with the image,
+///  if there is one or it will be visible if there is no texture.  Takes a UIColor
+#define kWGColor @"color"
+
+
 /** Fill in this protocol to get callbed when the user taps on or near an object for
     selection. 
  */
@@ -62,6 +109,9 @@
 /// Set selection support on or off here
 @property(nonatomic,assign) bool selection;
 
+/// Add rendering and other general hints for the globe view controller.
+- (void)setHints:(NSDictionary *)hintsDict;
+
 /// Animate to the given position over the given amount of time
 - (void)animateToPosition:(WGCoordinate)newPos time:(NSTimeInterval)howLong;
 
@@ -77,14 +127,26 @@
 /// Add a quad tree paged earth layer with 
 - (WGViewControllerLayer *)addQuadEarthLayerWithRemoteSource:(NSString *)baseURL imageExt:(NSString *)ext cache:(NSString *)cachdDir minZoom:(int)minZoom maxZoom:(int)maxZoom;
 
+/// Add visual defaults for the screen markers
+- (void)setScreenMarkerDesc:(NSDictionary *)desc;
+
 /// Add a group of screen (2D) markers
 - (WGComponentObject *)addScreenMarkers:(NSArray *)markers;
+
+/// Add visual defaults for the markers
+- (void)setMarkerDesc:(NSDictionary *)desc;
 
 /// Add a group of 3D markers
 - (WGComponentObject *)addMarkers:(NSArray *)markers;
 
+/// Add visual defaults for the screen labels
+- (void)setScreenLabelDesc:(NSDictionary *)desc;
+
 /// Add a group of screen (2D) labels
 - (WGComponentObject *)addScreenLabels:(NSArray *)labels;
+
+/// Add visual defaults for the labels
+- (void)setLabelDesc:(NSDictionary *)desc;
 
 /// Add a group of 3D labels
 - (WGComponentObject *)addLabels:(NSArray *)labels;
