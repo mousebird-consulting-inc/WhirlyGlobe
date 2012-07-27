cbcf79fe2e4a5892a0a1f663d918c92ccf502bc5
Steve Gifford <sjg@mousebirdconsulting.com>
2012-07-27 10:04:35 -0700

    Moved horrible back globe hack over to the view watcher.

--- WhirlyGlobeSrc/WhirlyGlobeLib/src/ViewPlacementGenerator.mm
+++ WhirlyGlobeSrc/WhirlyGlobeLib/src/ViewPlacementGenerator.mm
@@ -91,20 +91,42 @@ void ViewPlacementGenerator::generateDrawables(WhirlyKitRendererFrameInfo *frame
         {
             // Note: Calculate this ahead of time
             Point3f worldLoc = coordSys->localToGeocentricish(coordSys->geographicToLocal(viewInst.loc));
+            
+            // Note: Copied from the ScreenSpaceGenerator.  Still dumb here.
+            Point3f testPts[2];
+            testPts[0] = worldLoc;
+            testPts[1] = worldLoc*1.5;
+            for (unsigned int ii=0;ii<2;ii++)
+            {
+                Vector4f modelSpacePt = frameInfo.viewAndModelMat * Vector4f(testPts[ii].x(),testPts[ii].y(),testPts[ii].z(),1.0);
+                modelSpacePt.x() /= modelSpacePt.w();  modelSpacePt.y() /= modelSpacePt.w();  modelSpacePt.z() /= modelSpacePt.w();  modelSpacePt.w() = 1.0;
+                Vector4f projSpacePt = frameInfo.projMat * Vector4f(modelSpacePt.x(),modelSpacePt.y(),modelSpacePt.z(),modelSpacePt.w());
+                //        projSpacePt.x() /= projSpacePt.w();  projSpacePt.y() /= projSpacePt.w();  projSpacePt.z() /= projSpacePt.w();  projSpacePt.w() = 1.0;
+                testPts[ii] = Point3f(projSpacePt.x(),projSpacePt.y(),projSpacePt.z());
+            }
+            Vector3f testDir = testPts[1] - testPts[0];
+            testDir.normalize();
+
+            // Note: This is so dumb it hurts.  Figure out why the math is broken.
+            if (testDir.z() > -0.33)
+                hidden = YES;
 
             // If it's pointed away from the user, don't bother
-            if (worldLoc.dot(frameInfo.eyeVec) < 0.0)
-                hidden = YES;
+//            if (worldLoc.dot(frameInfo.eyeVec) < 0.0)
+//                hidden = YES;
 
-            // Project the world location to the screen
-            Eigen::Matrix4f modelTrans = frameInfo.modelTrans;
-            screenPt = [globeView pointOnScreenFromSphere:worldLoc transform:&modelTrans frameSize:Point2f(frameInfo.sceneRenderer.framebufferWidth,frameInfo.sceneRenderer.framebufferHeight)]; 
-            
-            // Note: This check is too simple
-            if (!hidden &&
-                (screenPt.x < frameMbr.ll().x() || screenPt.y < frameMbr.ll().y() || 
-                 screenPt.x > frameMbr.ur().x() || screenPt.y > frameMbr.ur().y()))
-                hidden = YES;
+            if (!hidden)
+            {
+                // Project the world location to the screen
+                Eigen::Matrix4f modelTrans = frameInfo.modelTrans;
+                screenPt = [globeView pointOnScreenFromSphere:worldLoc transform:&modelTrans frameSize:Point2f(frameInfo.sceneRenderer.framebufferWidth,frameInfo.sceneRenderer.framebufferHeight)]; 
+                
+                // Note: This check is too simple
+                if (!hidden &&
+                    (screenPt.x < frameMbr.ll().x() || screenPt.y < frameMbr.ll().y() || 
+                     screenPt.x > frameMbr.ur().x() || screenPt.y > frameMbr.ur().y()))
+                    hidden = YES;
+            }
         }
         
         viewInst.view.hidden = hidden;
