ad4d2f9ed5da00f24593a2c623e787c97b02cfd3
Steve Gifford <sjg@mousebirdconsulting.com>
2012-10-15 20:09:25 -0700

    Turned on poles by default for network tiles.
    Former-commit-id: 4280a64d7a9e480119796848542919247124d344

--- WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/src/MaplyQuadEarthWithRemoteTiles.mm
+++ WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/src/MaplyQuadEarthWithRemoteTiles.mm
@@ -39,6 +39,7 @@
         dataSource.numSimultaneous = 8;
         tileLoader = [[WhirlyKitQuadTileLoader alloc] initWithDataSource:dataSource];
         tileLoader.ignoreEdgeMatching = !edges;
+        tileLoader.coverPoles = true;
         quadLayer = [[WhirlyKitQuadDisplayLayer alloc] initWithDataSource:dataSource loader:tileLoader renderer:renderer];
         [layerThread addLayer:quadLayer];
     }
