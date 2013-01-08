5c401adeeab5eaf90884f1185838a406fc680935
Steve Gifford <sjg@mousebirdconsulting.com>
2013-01-08 14:51:57 -0800

    First cut at specialized NaturalEarthData app

---
+++ WhirlyGlobeSrc/NaturalEarthData/NaturalEarthData/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NaturalEarthData
+//
+//  Created by Steve Gifford on 12/23/12.
+//  Copyright (c) 2012 mousebird consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
