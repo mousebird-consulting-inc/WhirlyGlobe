efc6d8558293bba1d1bb5171934888996146e5c6
Steve Gifford <sjg@mousebirdconsulting.com>
2013-05-10 16:09:26 -0700

    Starting on tile source wrapper.

---
+++ WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/include/MaplyTileSource.h
@@ -0,0 +1,40 @@
+/*
+ *  MaplyTileSource.h
+ *  WhirlyGlobe-MaplyComponent
+ *
+ *  Created by Steve Gifford on 5/7/13.
+ *  Copyright 2011-2013 mousebird consulting
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+#import <Foundation/Foundation.h>
+
+/** The protocol for a Maply Tile Source.  Fill these calls in and your can
+    pass in your own data.
+  */
+@protocol MaplyTileSource
+
+/// Minimum zoom level (e.g. 0)
+- (int)minZoom;
+
+/// Maximum zoom level (e.g. 17)
+- (int)maxZoom;
+
+/// Number of pixels in a single tile
+- (int)numPixels;
+
+/// Return the image for a given 
+- (UIImage *)
+
+@end
