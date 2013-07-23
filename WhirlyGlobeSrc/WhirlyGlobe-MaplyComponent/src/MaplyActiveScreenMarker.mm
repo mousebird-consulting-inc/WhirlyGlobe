61d3f96e0499595620192fd72570799a8c9eab4d
Steve Gifford <sjg@mousebirdconsulting.com>
2013-07-23 16:49:11 -0700

    Switched active objects over to managers.
    Former-commit-id: 467639308562d0dc820bcc007db19ba139fb7a8d

--- WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/src/MaplyActiveScreenMarker.mm
+++ WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/src/MaplyActiveScreenMarker.mm
@@ -21,60 +21,57 @@
 #import "MaplyActiveScreenMarker.h"
 #import "WhirlyGlobe.h"
 #import "MaplyActiveObject_private.h"
+#import "MarkerManager.h"
 
 using namespace Eigen;
 using namespace WhirlyKit;
 
 @implementation MaplyActiveScreenMarker
 {
-    bool changed;
+    MaplyScreenMarker *screenMarker;
     NSDictionary *desc;
     UIImage *image;
     SimpleIdentity texID;
-    SimpleIDSet screenIDs;
+    SimpleIDSet markerIDs;
 }
 
-- (id)initWithDesc:(NSDictionary *)descDict
+- (void)setScreenMarker:(MaplyScreenMarker *)newMarker desc:(NSDictionary *)inDesc;
 {
-    self = [super init];
-    if (!self)
-        return nil;
-    
-    changed = false;
-    desc = descDict;
-    
-    return self;
+    if (dispatchQueue)
+    {
+        dispatch_async(dispatchQueue,
+                       ^{
+                           @synchronized(self)
+                           {
+                               screenMarker = newMarker;
+                               desc = inDesc;
+                               [self update];
+                           }
+                       }
+                       );
+    } else {
+        @synchronized(self)
+        {
+            screenMarker = newMarker;
+            desc = inDesc;
+            [self update];
+        }        
+    }
 }
 
-- (void)setScreenMarker:(MaplyScreenMarker *)newScreenMarker
+// Create new geometry
+- (void)update
 {
-    if ([NSThread currentThread] != [NSThread mainThread])
-        return;
-    
-    changed = true;
-    _screenMarker = newScreenMarker;
-}
-
-- (bool)hasUpdate
-{
-    return changed;
-}
-
-// Flush out changes to the scene
-- (void)updateForFrame:(WhirlyKitRendererFrameInfo *)frameInfo
-{
-    if (!changed)
-        return;
-    
     ChangeSet changes;
-
-    // Remove the old marker
-    for (SimpleIDSet::iterator it = screenIDs.begin();it != screenIDs.end(); ++it)
-        changes.push_back(new ScreenSpaceGeneratorRemRequest(scene->getScreenSpaceGeneratorID(), *it));
-    screenIDs.clear();
+    
+    MarkerManager *markerManager = (MarkerManager *)scene->getManager(kWKMarkerManager);
+    
+    if (markerManager)
+        markerManager->removeMarkers(markerIDs, changes);
+    markerIDs.clear();
     
     // And possibly the image
-    if (image != _screenMarker.image)
+    if (image != screenMarker.image)
     {
         if (texID != EmptyIdentity)
         {
@@ -84,101 +81,50 @@ using namespace WhirlyKit;
         image = nil;
     }
     
-    if (_screenMarker)
+    if (screenMarker)
     {
         // New image
-        if (_screenMarker.image)
+        if (screenMarker.image)
         {
-            Texture *tex = new Texture("Active ScreenLabel",_screenMarker.image);
+            Texture *tex = new Texture("Active ScreenLabel",screenMarker.image);
             texID = tex->getId();
-            image = _screenMarker.image;
+            image = screenMarker.image;
             changes.push_back(new AddTextureReq(tex));
         }
 
         // Need the WK version of the Marker
         WhirlyKitMarker *marker = [[WhirlyKitMarker alloc] init];
-        marker.loc = GeoCoord(_screenMarker.loc.x,_screenMarker.loc.y);
+        marker.loc = GeoCoord(screenMarker.loc.x,screenMarker.loc.y);
         if (texID != EmptyIdentity)
             marker.texIDs.push_back(texID);
-        marker.width = _screenMarker.size.width;
-        marker.height = _screenMarker.size.height;
-        
-        // Note: Deal with selection
-        
-        // Build the marker
-        WhirlyKitMarkerInfo *markerInfo = [[WhirlyKitMarkerInfo alloc] initWithMarkers:nil desc:desc];
-        std::vector<ScreenSpaceGenerator::ConvexShape *> screenShapes;
-        
-        // Build the rectangle for this one
-        Point3f pts[4];
-        Vector3f norm;
-        float width2 = (marker.width == 0.0 ? markerInfo.width : marker.width)/2.0;
-        float height2 = (marker.height == 0.0 ? markerInfo.height : marker.height)/2.0;
-        
-        Point3f localPt = scene->getCoordAdapter()->getCoordSystem()->geographicToLocal(marker.loc);
-        norm = scene->getCoordAdapter()->normalForLocal(localPt);
-        
-        pts[0] = Point3f(-width2,-height2,0.0);
-        pts[1] = Point3f(width2,-height2,0.0);
-        pts[2] = Point3f(width2,height2,0.0);
-        pts[3] = Point3f(-width2,height2,0.0);
-
-        // Build one set of texture coordinates
-        std::vector<TexCoord> texCoord;
-        texCoord.resize(4);
-        texCoord[0].u() = 0.0;  texCoord[0].v() = 0.0;
-        texCoord[1].u() = 1.0;  texCoord[1].v() = 0.0;
-        texCoord[2].u() = 1.0;  texCoord[2].v() = 1.0;
-        texCoord[3].u() = 0.0;  texCoord[3].v() = 1.0;
-
-        ScreenSpaceGenerator::SimpleGeometry smGeom;
-        smGeom.texID = texID;
-        smGeom.color = [markerInfo.color asRGBAColor];
-        for (unsigned int ii=0;ii<4;ii++)
+        marker.width = screenMarker.size.width;
+        marker.height = screenMarker.size.height;
+        if (screenMarker.selectable)
         {
-            smGeom.coords.push_back(Point2f(pts[ii].x(),pts[ii].y()));
-            smGeom.texCoords.push_back(texCoord[ii]);
+            marker.isSelectable = true;
+            marker.selectID = Identifiable::genId();
         }
-        ScreenSpaceGenerator::ConvexShape *shape = new ScreenSpaceGenerator::ConvexShape();
-        if (marker.isSelectable && marker.selectID != EmptyIdentity)
-            shape->setId(marker.selectID);
-        shape->worldLoc = scene->getCoordAdapter()->localToDisplay(localPt);
-        if (marker.lockRotation)
+        marker.layoutImportance = screenMarker.layoutImportance;
+        
+        if (markerManager)
         {
-            shape->useRotation = true;
-            shape->rotation = marker.rotation;
+            SimpleIdentity markerID = markerManager->addMarkers(@[marker], desc, changes);
+            if (markerID != EmptyIdentity)
+                markerIDs.insert(markerID);
         }
-        // Note: No fade
-        shape->minVis = markerInfo.minVis;
-        shape->maxVis = markerInfo.maxVis;
-        shape->drawPriority = markerInfo.drawPriority;
-        shape->geom.push_back(smGeom);
-        screenShapes.push_back(shape);
-        screenIDs.insert(shape->getId());
-
-        // Add all the screen space markers at once
-        if (!screenShapes.empty())
-            changes.push_back(new ScreenSpaceGeneratorAddRequest(scene->getScreenSpaceGeneratorID(),screenShapes));
-        screenShapes.clear();
     }
     
     
     scene->addChangeRequests(changes);
-    
-    changed = false;
 }
 
 - (void)shutdown
 {
-    ChangeSet changes;
-    
-    // Get rid of drawables and screen objects
-    for (SimpleIDSet::iterator it = screenIDs.begin();it != screenIDs.end(); ++it)
-        changes.push_back(new ScreenSpaceGeneratorRemRequest(scene->getScreenSpaceGeneratorID(), *it));
-    if (texID != EmptyIdentity)
-        changes.push_back(new RemTextureReq(texID));
-    
-    scene->addChangeRequests(changes);
+    @synchronized(self)
+    {
+        screenMarker = nil;
+        [self update];
+    }
 }
 
 
