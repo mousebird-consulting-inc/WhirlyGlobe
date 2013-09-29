0c4ee33deb75d8a8f7c9906f962a20af28c21c3e
Steve Gifford <sjg@mousebirdconsulting.com>
2013-09-28 20:44:53 -0700

    Another shift in the documentation mines.

--- WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/include/MaplyMultiplexTileSource.h
+++ WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/include/MaplyMultiplexTileSource.h
@@ -21,15 +21,29 @@
 #import "MaplyTileSource.h"
 #import "MaplyCoordinateSystem.h"
 
-/** This tile source combines a bunch of individual tile sources
-    for animation.
+/** @brief The multiplex tile source is for bunging other tile sources together.
+    @details The Multiplex Tile source takes a bunch of other tiles source
+    objects and switches between them.  Strictly speaking, it doesn't
+    multiplex.  The quad image layer asks for all of its images at
+    once for a given tile.  I just like the work 'multiplex'.
+    Multiplex. Multiplex.  So futuristic in a 1970's way.  Multiplex.
+    @details Anyway, this is useful for animating between different tile
+    sources.
+    @see MaplyTileSource
+    @see MaplyQuadImageTilesLayer
   */
 @interface MaplyMultiplexTileSource : NSObject<MaplyTileSource>
 
-/// Initialize with the various tile sources you want to use
+/** @brief Initialize with an array of MaplyTileSource compliant objects.
+    @details To create one of these you need to pass in an NSArray of objects that implement the MaplyTileSource protocol.  Where this is useful is if you want to animate between them.  Check out the animation functionality in MaplyQuadImageTilesLayer.  To animate effectively, that layer must grab all the images for a tile at once, hence this object.
+    @details A simple example would be taking 6 MaplyMBTileSource objects, sticking them in an array and passing them in here.  Each of them might represent a frame of animation in a video you want to show over the whole earth.  Because wow, man.  The whole earth.
+    @return Returns a working MaplyMultiplexTileSource or nil if it can't.  That might happen if the input tile sources don't match up to each other.
+  */
 - (id)initWithSources:(NSArray *)tileSources;
 
-/// Coordinate system they'd better all share
+/** @brief Coordinate system for the tile source.
+    @details The coordinate system is derived from the first tile source passed in.  They'd better all agree or the results will be odd.
+  */
 @property (nonatomic,readonly) MaplyCoordinateSystem *coordSys;
 
 @end
--- WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/include/MaplyQuadPagingLayer.h
+++ WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/include/MaplyQuadPagingLayer.h
@@ -26,8 +26,10 @@
 
 @class MaplyQuadPagingLayer;
 
-/** The protocol for the Maply Paging Delegate.  Fill this in to
-    provide per tile data for features that you're loading.
+/** The Maply Paging Delegate is used by the MaplyQuadPagingLayer to
+    do feature (e.g. not image) paging.  You set up an object that
+    implements this protocol and talks to the paging layer.  This
+    is how you load things like vector tiles.
   */
 @protocol MaplyPagingDelegate
 
@@ -38,40 +40,81 @@
 - (int)maxZoom;
 
 /** Start fetching data for the given tile.  This will not be called on the 
-    main thread so be prepared for that.  You should immediatley to an
+    main thread so be prepared for that.  You should immediately do an
     async call to your own loading logic and then merge in your results.
     If you do your loading calls in line you'll slow down the loading thread.
-    After you're done you MUST call tileDidLoad in the layer.
+    After you're done you MUST call tileDidLoad in the layer, even for
+    failures.
+    @param tileID The tile the system wants you to start loading.
+    @param layer The quad paging layer you'll hand the data over to when it's loaded.
   */
 - (void)startFetchForTile:(MaplyTileID)tileID forLayer:(MaplyQuadPagingLayer *)layer;
 
 @end
 
-/** This is a generic quad earth paging interface.  Hand it your coordinate system,
-    bounds, and tile source object and it will page tiles for you.
- */
+/** The Maply Quad Paging Layer implements a general purpose paging
+    interface for quad tree based data sources.  This is different
+    from the MaplyQuadImageTilesLayer in that it's meant for paging
+    things like vector tiles, or really any other features that's
+    not images.
+    You set up an object that implements the MaplyPagingDelegate
+    protocol, create the MaplyQuadPagingLayer and respond to the
+    startFetchForTile:forLayer: method.
+    Once you've fetched your data for a given tile, you'll need
+    to call tileDidLoad: or tileFailedToLoad:.  Then you'll want
+    to create the visual objects in the MaplyViewController or
+    WhirlyGlobeViewController and pass them in to addData:forTile:.
+    This is how the paging layer keeps track of which objects you've
+    created for a given tile and can then remove them when the time
+    comes.
+    Objects must be created with @"name" set to @(NO) in the description
+    dictionary.  Tiles are paged before they are needed for dipslay and
+    so the paging layer must have control over when data is displayed.
+    Is uses the enable/disable options for the various Maply objects in
+    the view controllers.
+  */
 @interface MaplyQuadPagingLayer : MaplyViewControllerLayer
 
-/// Change the number of fetches allowed at once
+/// Change the number of fetches allowed at once.  This means you'll
+///  have at most numSimultaneousFetches dispatch queues going.
 @property (nonatomic,assign) int numSimultaneousFetches;
 
-/// The view controller this is part of
+/// The view controller this paging layer is associated with.
+/// This view controller is the one you should create visual objects in.
 @property (nonatomic,readonly) MaplyBaseViewController *viewC;
 
-/// Construct with the coordinate system (which contains bounds) and the tile source
+/** This initializer takes the coordinate system we're working in
+    and the MaplyPagingDelegate object.  Fill out that to do
+    the real work.
+    @param coordSys The coordinate system we're working in.
+    @param tileSource The tile source that will fetch data and create visible objects.
+  */
 - (id)initWithCoordSystem:(MaplyCoordinateSystem *)coordSys delegate:(NSObject<MaplyPagingDelegate> *)tileSource;
 
-/// Add data for the given tileID.  These should be one or more MaplyComponentObject.
-/// Please create them disabled so we enable & disable them as needed.
+/** This method is called by your MaplyPagingDelegate to add MaplyComponentObject's
+    to the data for a given tile.  Please create them disabled by putting @"enable": @(NO)
+    in the description dictionary.  The paging layer will then be responsible for
+    cleaning them up when needed.
+    @param dataObjects An NSArray of MaplyComponentObject objects.
+    @param tileID The tile ID for the data we're handing over.
+  */
 - (void)addData:(NSArray *)dataObjects forTile:(MaplyTileID)tileID;
 
-/// Let the pager know the tile completely failed to load
+/// When you've finished loading the data for a tile in your MaplyPagingDelegate,
+///  but before you've called addData:forTile: call this method to let the
+///  paging layer know you succeeded in loading the tile.
 - (void)tileFailedToLoad:(MaplyTileID)tileID;
 
-/// Let the paging know the tile is completely loaded
+/// If you fail to load data for a given tile in your MaplyPagingDelegate,
+///  call this to let the paging layer know about it.
 - (void)tileDidLoad:(MaplyTileID)tileID;
 
-/// Utility routine to calculate a bounding box given a particular tile
+/** This is a utility method for calculating the extents of a given tile
+    in the local coordinate system (e.g. the one paging layer is using).
+    @param tileID The ID for the tile we're interested in.
+    @param ll The lower left corner of the tile in local coordinates.
+    @param ur The upper right corner of the tile in local coordinates.
+  */
 - (void)geoBoundsforTile:(MaplyTileID)tileID ll:(MaplyCoordinate *)ll ur:(MaplyCoordinate *)ur ;
 
 @end
