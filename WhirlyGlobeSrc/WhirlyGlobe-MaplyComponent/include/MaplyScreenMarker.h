686d6dc395696e898d4fe0e88d25e952125ff8db
Steve Gifford <sjg@mousebirdconsulting.com>
2014-12-01 14:27:40 -0800

    Billboard was missing userObject.  Changed userObjects to be id's.

--- WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/include/MaplyScreenMarker.h
+++ WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/include/MaplyScreenMarker.h
@@ -84,9 +84,9 @@
 @property (nonatomic,assign) bool selectable;
 
 /** @brief User data object for selection
-    @details When the user selects a feature and the developer gets it in their delegate, this is an object they can use to figure out what the screen label means to them.
+ @details When the user selects a feature and the developer gets it in their delegate, this is an object they can use to figure out what the screen marker means to them.
  */
-@property (nonatomic,strong) NSObject *userObject;
+@property (nonatomic,strong) id userObject;
 
 @end
 
