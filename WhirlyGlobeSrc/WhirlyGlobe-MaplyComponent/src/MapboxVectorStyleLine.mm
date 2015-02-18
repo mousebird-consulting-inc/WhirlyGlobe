a05b1cb3b233049cb5bfea82eefbd16344a12136
Steve Gifford <sjg@mousebirdconsulting.com>
2015-02-18 10:04:10 -0800

    Mapbox style sheets partially implemented.
    Former-commit-id: 2852134753997ee73ba6663816fc8e8d6fa0325c

--- WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/src/MapboxVectorStyleLine.mm
+++ WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/src/MapboxVectorStyleLine.mm
@@ -20,7 +20,70 @@
 
 #import "MapboxVectorStyleLine.h"
 
+@implementation MapboxVectorLineLayout
+
+- (id)initWithStyleEntry:(NSDictionary *)styleEntry styleSet:(MaplyMapboxVectorStyleSet *)styleSet viewC:(MaplyBaseViewController *)viewC
+{
+    self = [super init];
+    if (!self)
+        return nil;
+    
+    [styleSet unsupportedCheck:@"visibility" in:@"line-layout" styleEntry:styleEntry];
+    
+    _cap = (MapboxVectorLineCap)[styleSet enumValue:styleEntry[@"line-cap"] options:@[@"butt",@"round",@"square"] defVal:MBLineCapButt];
+    _join = (MapboxVectorLineJoin)[styleSet enumValue:styleEntry[@"line-join"] options:@[@"bevel",@"round",@"miter"] defVal:MBLineJoinMiter];
+    _miterLimit = [styleSet doubleValue:@"line-miter-limit" dict:styleEntry defVal:2.0];
+    _roundLimit = [styleSet doubleValue:@"line-round-limit" dict:styleEntry defVal:1.0];
+    
+    return self;
+}
+
+@end
+
+@implementation MapboxVectorLinePaint
+
+- (id)initWithStyleEntry:(NSDictionary *)styleEntry styleSet:(MaplyMapboxVectorStyleSet *)styleSet viewC:(MaplyBaseViewController *)viewC
+{
+    self = [super init];
+    if (!self)
+        return nil;
+
+    [styleSet unsupportedCheck:@"line-translate" in:@"line-paint" styleEntry:styleEntry];
+    [styleSet unsupportedCheck:@"line-translate-anchor" in:@"line-paint" styleEntry:styleEntry];
+    [styleSet unsupportedCheck:@"line-gap-width" in:@"line-paint" styleEntry:styleEntry];
+    [styleSet unsupportedCheck:@"line-blur" in:@"line-paint" styleEntry:styleEntry];
+    [styleSet unsupportedCheck:@"line-image" in:@"line-paint" styleEntry:styleEntry];
+    [styleSet unsupportedCheck:@"line-dasharray" in:@"line-paint" styleEntry:styleEntry];
+
+    id opEntry = styleEntry[@"line-opacity"];
+    if (opEntry)
+    {
+        if ([opEntry isKindOfClass:[NSNumber class]])
+            _opacity = [styleSet doubleValue:opEntry defVal:1.0];
+        else
+            _opacityFunc = [styleSet stopsValue:opEntry defVal:nil];
+    } else
+        _opacity = 1.0;
+    _color = [styleSet colorValue:@"line-color" dict:styleEntry defVal:[UIColor blackColor]];
+    id widthEntry = [styleSet constantSubstitution:styleEntry[@"line-width"] forField:@"line-width"];
+    if (widthEntry)
+    {
+        if ([widthEntry isKindOfClass:[NSNumber class]])
+            _width = [styleSet doubleValue:widthEntry defVal:1.0];
+        else
+            _widthFunc = [styleSet stopsValue:widthEntry defVal:nil];
+    } else
+        _width = 1.0;
+    
+    return self;
+}
+
+@end
+
 @implementation MapboxVectorLayerLine
+{
+    NSMutableDictionary *lineDesc;
+}
 
 - (id)initWithStyleEntry:(NSDictionary *)styleEntry parent:(MaplyMapboxVectorStyleLayer *)refLayer styleSet:(MaplyMapboxVectorStyleSet *)styleSet drawPriority:(int)drawPriority viewC:(MaplyBaseViewController *)viewC
 {
@@ -28,13 +91,67 @@
     if (!self)
         return nil;
     
+    _layout = [[MapboxVectorLineLayout alloc] initWithStyleEntry:styleEntry[@"layout"] styleSet:styleSet viewC:viewC];
+    _paint = [[MapboxVectorLinePaint alloc] initWithStyleEntry:styleEntry[@"paint"] styleSet:styleSet viewC:viewC];
+    
+    if (!_paint)
+    {
+        NSLog(@"Expecting paint in line layer");
+        return nil;
+    }
+    
+    lineDesc = [NSMutableDictionary dictionaryWithDictionary:
+            @{kMaplyVecWidth: @(_paint.width),
+              kMaplyColor: _paint.color,
+              kMaplyDrawPriority: @(self.drawPriority),
+              kMaplyFade: @0.0,
+              kMaplyVecCentered: @YES,
+              kMaplySelectable: @NO,
+              kMaplyEnable: @NO
+              }];
+    
     return self;
 }
 
 
 - (NSArray *)buildObjects:(NSArray *)vecObjs forTile:(MaplyTileID)tileID  viewC:(MaplyBaseViewController *)viewC
 {
-    return nil;
+    NSMutableArray *compObjs = [NSMutableArray array];
+    
+    NSDictionary *desc = lineDesc;
+    bool include = true;
+    if (_paint.widthFunc)
+    {
+        double width = [_paint.widthFunc valueForZoom:tileID.level];
+        if (width > 0.0)
+        {
+            NSMutableDictionary *mutDesc = [NSMutableDictionary dictionaryWithDictionary:desc];
+            mutDesc[kMaplyVecWidth] = @(width);
+            desc = mutDesc;
+        } else
+            include = false;
+    }
+    if (_paint.opacityFunc && include)
+    {
+        double opacity = [_paint.opacityFunc valueForZoom:tileID.level];
+        if (opacity > 0.0)
+        {
+            UIColor *color = [self.styleSet color:_paint.color withOpacity:opacity];
+            NSMutableDictionary *mutDesc = [NSMutableDictionary dictionaryWithDictionary:desc];
+            mutDesc[kMaplyColor] = color;
+            desc = mutDesc;
+        } else
+            include = false;
+    }
+    
+    if (include)
+    {
+        MaplyComponentObject *compObj = [viewC addWideVectors:vecObjs desc:desc mode:MaplyThreadCurrent];
+        if (compObj)
+            [compObjs addObject:compObj];
+    }
+    
+    return compObjs;
 }
 
 @end
