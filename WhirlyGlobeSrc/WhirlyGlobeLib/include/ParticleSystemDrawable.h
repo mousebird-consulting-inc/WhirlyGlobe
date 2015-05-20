93afdf479e230a06b75f84a72437ff8a4f8c1da4
Steve Gifford <sjg@mousebirdconsulting.com>
2015-05-20 11:39:20 -0700

    Part way through particle system implementation.

---
+++ WhirlyGlobeSrc/WhirlyGlobeLib/include/ParticleSystemDrawable.h
@@ -0,0 +1,109 @@
+/*
+ *  ParticleSystemDrawable.h
+ *  WhirlyGlobeLib
+ *
+ *  Created by Steve Gifford on 4/28/15.
+ *  Copyright 2011-2015 mousebird consulting
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+#import "Drawable.h"
+#import "CoordSystem.h"
+
+namespace WhirlyKit
+{
+    
+// Low level drawable used to manage particle systems
+class ParticleSystemDrawable : public Drawable
+{
+public:
+    // A group of attribute data passed in at once
+    class AttributeData
+    {
+    public:
+        std::string name;
+        NSData *data;
+    };
+    
+    ParticleSystemDrawable(const std::string &name,const SingleVertexAttributeInfoSet &vertAttrs,int numPoints);
+    virtual ~ParticleSystemDrawable();
+    
+    /// No bounding box, since these change constantly
+    Mbr getLocalMbr() const { return Mbr(); }
+
+    /// No offset matrix (at the moment)
+    const Eigen::Matrix4d *getMatrix() const { return NULL; }
+
+    /// Draw priority for ordering
+    unsigned int getDrawPriority() const { return drawPriority; }
+    void setDrawPriority(int newPriority) { drawPriority = newPriority; }
+
+    /// Program to use for rendering
+    virtual SimpleIdentity getProgram() const { return programId; }
+    /// Set the shader program.  Empty (default) by default
+    virtual void setProgram(SimpleIdentity newProgId) { programId = newProgId; }
+
+    /// Whether it's currently displaying
+    bool isOn(WhirlyKitRendererFrameInfo *frameInfo) const { return enable; }
+    /// True to turn it on, false to turn it off
+    void setOnOff(bool onOff) { enable = onOff; }
+    
+    /// Create our buffers in GL
+    void setupGL(WhirlyKitGLSetupInfo *setupInfo,OpenGLMemManager *memManager);
+    
+    /// Destroy GL buffers
+    void teardownGL(OpenGLMemManager *memManager);
+    
+    /// Called on the rendering thread to draw
+    void draw(WhirlyKitRendererFrameInfo *frameInfo,Scene *scene);
+    
+    /// Just points for now
+    GLenum getType() const { return GL_POINTS; }
+    
+    /// Not using this mechanism
+    bool hasAlpha(WhirlyKitRendererFrameInfo *frameInfo) const { return false; }
+    
+    /// Don't need to update the renderer particularly
+    void updateRenderer(WhirlyKitSceneRendererES *renderer);
+
+    /// If set, we want to use the z buffer
+    bool getRequestZBuffer() const { return requestZBuffer; }
+    void setRequestZBuffer(bool enable) { requestZBuffer = enable; }
+    
+    /// If set, we want to write to the z buffer
+    bool getWriteZbuffer() const { return writeZBuffer; }
+    void setWriteZbuffer(bool enable) { writeZBuffer = enable; }
+    
+    /// Add the vertex data (all of it) at once
+    void addAttributeData(const std::vector<AttributeData> &attrData);
+
+protected:
+    bool enable;
+    int numPoints;
+    int vertexSize;
+    std::vector<SingleVertexAttributeInfo> vertAttrs;
+    SimpleIdentity programId;
+    int drawPriority;
+    bool requestZBuffer,writeZBuffer;
+    float minVis,maxVis,minVisibleFadeBand,maxVisibleFadeBand;
+    GLuint pointBuffer;
+
+    // The vertex attributes we're representing in the buffers
+    std::vector<VertexAttribute> vertexAttributes;
+    
+    /// Called when a new VAO is bound.  Set up your VAO-related state here.
+    virtual void setupAdditionalVAO(OpenGLES2Program *prog,GLuint vertArrayObj) { }
+};
+    
+}
--- WhirlyGlobeSrc/WhirlyGlobeLib/src/ParticleSystemManager.mm
+++ WhirlyGlobeSrc/WhirlyGlobeLib/src/ParticleSystemManager.mm
@@ -2,7 +2,7 @@
  *  ParticleSystemManager.mm
  *  WhirlyGlobeLib
  *
- *  Created by Steve Gifford on 7/30/13.
+ *  Created by Steve Gifford on 4/26/15.
  *  Copyright 2011-2015 mousebird consulting. All rights reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,152 +19,88 @@
  */
 
 #import "ParticleSystemManager.h"
-#import "NSDictionary+Stuff.h"
-#import "GlobeMath.h"
-#import "UIColor+Stuff.h"
-
-using namespace Eigen;
-using namespace WhirlyKit;
-
-@implementation WhirlyKitParticleSystem
-
-@end
-
-@interface ParticleSystemInfo : NSObject
-{
-    NSArray *colors;
-}
-
-@property (nonatomic,assign) SimpleIdentity destId;
-@property (nonatomic) NSArray *systems;
-@property (nonatomic) NSDictionary *desc;
-
-- (id)initWithSystems:(NSArray *)inSystems desc:(NSDictionary *)inDesc;
-
-@end
-
-@implementation ParticleSystemInfo
-
-- (id)initWithSystems:(NSArray *)inSystems desc:(NSDictionary *)inDesc
-{
-    self = [super init];
-    if (self)
-    {
-        self.systems = inSystems;
-        self.desc = inDesc;
-    }
-    
-    return self;
-}
-
-@end
-
 
 namespace WhirlyKit
 {
+
+ParticleSystemSceneRep::ParticleSystemSceneRep()
+{
+}
+
+ParticleSystemSceneRep::ParticleSystemSceneRep(SimpleIdentity inId)
+: Identifiable(inId)
+{
+}
+    
+ParticleSystemSceneRep::~ParticleSystemSceneRep()
+{
+}
+    
+void ParticleSystemSceneRep::clearContents(ChangeSet &changes)
+{
+    for (SimpleIDSet::iterator it = drawIDs.begin();
+         it != drawIDs.end(); ++it)
+        changes.push_back(new RemDrawableReq(*it));
+}
     
 ParticleSystemManager::ParticleSystemManager()
 {
-    pthread_mutex_init(&partLock, NULL);
+    pthread_mutex_init(&partSysLock, NULL);
 }
     
 ParticleSystemManager::~ParticleSystemManager()
 {
-    pthread_mutex_destroy(&partLock);
-    for (ParticleSysSceneRepSet::iterator it = partReps.begin();
-         it != partReps.end(); ++it)
-        delete *it;
-    partReps.clear();
-}
- 
-    // Parse the basic particle system parameters out of an NSDictionary
-ParticleGenerator::ParticleSystem ParticleSystemManager::parseParams(NSDictionary *desc,ParticleGenerator::ParticleSystem *defaultParams)
-{
-    ParticleGenerator::ParticleSystem params;
-    if (defaultParams)
-        params = *defaultParams;
-        
-        params.minLength = [desc floatForKey:@"minLength" default:params.minLength];
-        params.maxLength = [desc floatForKey:@"maxLength" default:params.maxLength];
-        params.numPerSecMin = [desc intForKey:@"minNumPerSec" default:params.numPerSecMin];
-        params.numPerSecMax = [desc intForKey:@"maxNumPerSec" default:params.numPerSecMax];
-        params.minLifetime = [desc floatForKey:@"minLifetime" default:params.minLifetime];
-        params.maxLifetime = [desc floatForKey:@"maxLifetime" default:params.maxLifetime];
-        params.minPhi = [desc floatForKey:@"minPhi" default:params.minPhi];
-        params.maxPhi = [desc floatForKey:@"maxPhi" default:params.maxPhi];
-        params.minVis = [desc floatForKey:@"minVis" default:DrawVisibleInvalid];
-        params.maxVis = [desc floatForKey:@"maxVis" default:DrawVisibleInvalid];
-        UIColor *color = [desc objectForKey:@"color"];
-        NSArray *colors = [desc objectForKey:@"colors"];
-        if (!colors && color)
-            colors = [NSArray arrayWithObject:color];
-            
-            for (UIColor *thisColor in colors)
-            {
-                params.colors.push_back([thisColor asRGBAColor]);
-            }
-    
-    return params;
+    pthread_mutex_destroy(&partSysLock);
+    for (auto it : sceneReps)
+        delete it;
+    sceneReps.clear();
 }
     
-/// Add a group of particle systems
-SimpleIdentity ParticleSystemManager::addParticleSystems(NSArray *partSystems,NSDictionary *desc,SimpleIdentity genId,ChangeSet &changes)
+SimpleIdentity ParticleSystemManager::addParticleSystem(const ParticleSystem &newSystem,ChangeSet &changes)
 {
-    ParticleSystemInfo *systemInfo = [[ParticleSystemInfo alloc] initWithSystems:partSystems desc:desc];
-    systemInfo.destId = Identifiable::genId();
+    ParticleSystemSceneRep *sceneRep = new ParticleSystemSceneRep();
 
-    CoordSystemDisplayAdapter *coordAdapter = scene->getCoordAdapter();
-    ParticleSysSceneRep *sceneRep = new ParticleSysSceneRep();
-    SimpleIdentity partID = systemInfo.destId;
-    sceneRep->setId(systemInfo.destId);
+    sceneRep->partSys = newSystem;
     
-    // Parse out the general parameters
-    ParticleGenerator::ParticleSystem defaultSystem = ParticleGenerator::ParticleSystem::makeDefault();
-    ParticleGenerator::ParticleSystem baseParams = parseParams(systemInfo.desc,&defaultSystem);
+    SimpleIdentity partSysID = sceneRep->getId();
     
-    // Now run through the particle systems and kick them off
-    for (WhirlyKitParticleSystem *partSys in systemInfo.systems)
+    pthread_mutex_lock(&partSysLock);
+    sceneReps.insert(sceneRep);
+    pthread_mutex_unlock(&partSysLock);
+    
+    return partSysID;
+}
+    
+void ParticleSystemManager::enableParticleSystem(SimpleIdentity sysID,bool enable,ChangeSet &changes)
+{
+    pthread_mutex_lock(&partSysLock);
+    
+    ParticleSystemSceneRep dummyRep(sysID);
+    auto it = sceneReps.find(&dummyRep);
+    if (it != sceneReps.end())
+        (*it)->enableContents(enable, changes);
+    
+    pthread_mutex_unlock(&partSysLock);
+}
+    
+void ParticleSystemManager::removeParticleSystem(SimpleIdentity sysID,ChangeSet &changes)
+{
+    pthread_mutex_lock(&partSysLock);
+    
+    ParticleSystemSceneRep dummyRep(sysID);
+    auto it = sceneReps.find(&dummyRep);
+    if (it != sceneReps.end())
     {
-        // Set up the specifics of this one
-        ParticleGenerator::ParticleSystem *newPartSys = new ParticleGenerator::ParticleSystem(baseParams);
-        newPartSys->setId(Identifiable::genId());
-        sceneRep->partSysIDs.insert(newPartSys->getId());
-        newPartSys->loc = coordAdapter->localToDisplay(coordAdapter->getCoordSystem()->geographicToLocal([partSys loc]));
-        // Note: Won't work at the poles
-        newPartSys->dirUp = [partSys norm];
-        newPartSys->dirE = Vector3f(0,0,1).cross(newPartSys->dirUp);
-        newPartSys->dirN = newPartSys->dirUp.cross(newPartSys->dirE);
-        
-        changes.push_back(new ParticleGeneratorAddSystemRequest(genId,newPartSys));
+        (*it)->removeContents(changes);
+        sceneReps.erase(it);
     }
     
-    pthread_mutex_lock(&partLock);
-    partReps.insert(sceneRep);
-    pthread_mutex_unlock(&partLock);
-    
-    return partID;
+    pthread_mutex_unlock(&partSysLock);
 }
-
-/// Remove one or more particle systems
-void ParticleSystemManager::removeParticleSystems(SimpleIDSet partIDs,SimpleIdentity genId,ChangeSet &changes)
+    
+void ParticleSystemManager::housekeeping(NSTimeInterval now,ChangeSet &changes)
 {
-    pthread_mutex_lock(&partLock);
-
-    for (SimpleIDSet::iterator it = partIDs.begin(); it != partIDs.end(); ++it)
-    {
-        ParticleSysSceneRep dummyRep(*it);
-        ParticleSysSceneRepSet::iterator pit = partReps.find(&dummyRep);
-        if (pit != partReps.end())
-        {
-            ParticleSysSceneRep *sceneRep = *pit;
-            for (SimpleIDSet::iterator idIt = sceneRep->partSysIDs.begin(); idIt != sceneRep->partSysIDs.end(); ++idIt)
-                changes.push_back(new ParticleGeneratorRemSystemRequest(genId,*idIt));
-            
-            partReps.erase(pit);
-        }
-    }
-
-    pthread_mutex_unlock(&partLock);
+    // Note: Fill this in
 }
     
 }
