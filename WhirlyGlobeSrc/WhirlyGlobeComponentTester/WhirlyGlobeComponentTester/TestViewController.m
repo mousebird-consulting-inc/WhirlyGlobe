45d3f3dfa7b8301f79e05aa389a2db36d7f563b2
Steve Gifford <sjg@mousebirdconsulting.com>
2015-07-01 15:41:55 -0700

    Small fix to mapbox vector support.  Atmosphere almost working.
    Former-commit-id: ef7416771c85ee14cca0568f7099547053efa1c3

--- WhirlyGlobeSrc/WhirlyGlobeComponentTester/WhirlyGlobeComponentTester/TestViewController.m
+++ WhirlyGlobeSrc/WhirlyGlobeComponentTester/WhirlyGlobeComponentTester/TestViewController.m
@@ -1442,8 +1442,13 @@ static const int NumMegaMarkers = 15000;
                 // Note: Debugging
                 thisCacheDir = nil;
                 thisCacheDir = [NSString stringWithFormat:@"%@/mapbox-streets-vectiles",cacheDir];
-                // You need your own access token here
-                [MaplyMapnikVectorTiles StartRemoteVectorTilesWithTileSpec:@"https://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6.json" accessToken:@"pk.eyJ1IjoicGV0ZXJxbGl1IiwiYSI6ImpvZmV0UEEifQ._D4bRmVcGfJvo1wjuOpA1g" style:@"https://raw.githubusercontent.com/mapbox/mapbox-gl-styles/mb-pages/styles/emerald-v8.json" styleType:MapnikMapboxGLStyle cacheDir:thisCacheDir viewC:baseViewC
+                [MaplyMapnikVectorTiles StartRemoteVectorTilesWithTileSpec:@"https://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v6.json"
+                    // Note: You need your own access token here
+                    accessToken:@"pk.eyJ1IjoicGV0ZXJxbGl1IiwiYSI6ImpvZmV0UEEifQ._D4bRmVcGfJvo1wjuOpA1g"
+                    style:@"https://raw.githubusercontent.com/mapbox/mapbox-gl-styles/mb-pages/styles/emerald-v8.json"
+                    styleType:MapnikMapboxGLStyle
+                    cacheDir:thisCacheDir
+                    viewC:baseViewC
                  success:
                  ^(MaplyMapnikVectorTiles *vecTiles)
                  {
