f97f3f014e43010e907ba4a8f03eb0342f0bf8b6
Steve Gifford <sjg@mousebirdconsulting.com>
2015-07-10 15:56:02 -0700

    Now explicitly clearing dynamic textures when regions are released.

--- WhirlyGlobeSrc/WhirlyGlobeLib/include/DynamicTextureAtlas.h
+++ WhirlyGlobeSrc/WhirlyGlobeLib/include/DynamicTextureAtlas.h
@@ -63,6 +63,7 @@ public:
     void addTextureData(int startX,int startY,int width,int height,NSData *data);
     
     /// Clear out the area given
+    void clearRegion(const Region &region);
     void clearTextureData(int startX,int startY,int width,int height);
     
     /// Set or clear a given region
@@ -70,6 +71,9 @@ public:
     
     /// Look for an open region of the given cell extents
     bool findRegion(int cellsX,int cellsY,Region &region);
+    
+    /// Return a list of released regions
+    void getReleasedRegions(std::vector<DynamicTexture::Region> &toClear);
     
     /// Add a region to the list of ones to be cleared.
     /// This is called by the renderer
