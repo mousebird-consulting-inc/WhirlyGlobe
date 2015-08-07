1d3f2f67bba92635ceb2ef4691d9835d70406127
Steve Gifford <sjg@mousebirdconsulting.com>
2015-08-07 13:29:07 -0700

    Messing animation test tile source.

--- WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/src/MaplyAnimationTestTileSource.m
+++ WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/src/MaplyAnimationTestTileSource.m
@@ -140,7 +140,7 @@ static const int debugColors[MaxDebugColors] = {0x86812D, 0x5EB9C9, 0x2A7E3E, 0x
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0),
                    ^{
                        // Random delay
-                       usleep(drand48()* 0.115 * 1e6);
+                       usleep(drand48()* 3.0 * 1e6);
 
                        NSMutableArray *images = [NSMutableArray array];
                        for (unsigned int ii=0;ii<layer.imageDepth;ii++)
