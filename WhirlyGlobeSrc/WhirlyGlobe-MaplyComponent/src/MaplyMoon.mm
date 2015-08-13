33e8394b262fe7e272e29229679ebd1436ea3ab4
Steve Gifford <sjg@mousebirdconsulting.com>
2015-08-13 11:30:36 -0700

    Expose moon phase.

--- WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/src/MaplyMoon.mm
+++ WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/src/MaplyMoon.mm
@@ -56,12 +56,15 @@
     double sunEclipticLong = CAASun::ApparentEclipticLongitude(jd);
     double sunEclipticLat = CAASun::ApparentEclipticLatitude(jd);
     CAA2DCoordinate sunEquatorial = CAACoordinateTransformation::Ecliptic2Equatorial(sunEclipticLong,sunEclipticLat,obliquity);
-    
-    // Now for the illuminated fraction
+
+    // Now for the phase
     double geo_elongation = CAAMoonIlluminatedFraction::GeocentricElongation(moonEquatorial.X, moonEquatorial.Y, sunEquatorial.X, sunEquatorial.Y);
-    
-    _phaseAngle = CAAMoonIlluminatedFraction::PhaseAngle(geo_elongation, 368410.0, 149971520.0);
-    _illuminatedFraction = CAAMoonIlluminatedFraction::IlluminatedFraction(_phaseAngle);
+
+    double phaseAngle = CAAMoonIlluminatedFraction::PhaseAngle(geo_elongation, 368410.0, 149971520.0);
+    double positionAngle = CAAMoonIlluminatedFraction::PositionAngle(sunEclipticLong, sunEclipticLat, moonEclipticLong, moonEclipticLat);
+    _illuminatedFraction = CAAMoonIlluminatedFraction::IlluminatedFraction(phaseAngle);
+
+    _phase = (positionAngle < 180 ? phaseAngle + 180 : 180 - phaseAngle);
 
     return self;
 }
