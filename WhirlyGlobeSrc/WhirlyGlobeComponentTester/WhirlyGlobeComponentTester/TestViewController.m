0f6faa93024b44b779d02d80dee353fe71ab19e9
JM <jmnavarro@gmail.com>
2015-08-23 22:46:13 +0200

    Adapted tester to new APIs

--- WhirlyGlobeSrc/WhirlyGlobeComponentTester/WhirlyGlobeComponentTester/TestViewController.m
+++ WhirlyGlobeSrc/WhirlyGlobeComponentTester/WhirlyGlobeComponentTester/TestViewController.m
@@ -215,7 +215,7 @@ static const int BaseEarthPriority = kMaplyImageLayerDrawPriorityDefault;
             maxLayerTiles = 128;
             break;
         case Maply3DMap:
-            mapViewC = [[MaplyViewController alloc] init];
+            mapViewC = [[MaplyViewController alloc] initWithMapType:MaplyMapType3D];
             mapViewC.doubleTapZoomGesture = true;
             mapViewC.twoFingerTapGesture = true;
             mapViewC.viewWrap = true;
@@ -223,7 +223,7 @@ static const int BaseEarthPriority = kMaplyImageLayerDrawPriorityDefault;
             baseViewC = mapViewC;
             break;
         case Maply2DMap:
-            mapViewC = [[MaplyViewController alloc] initAsFlatMap];
+            mapViewC = [[MaplyViewController alloc] initWithMapType:MaplyMapTypeFlat];
             mapViewC.viewWrap = true;
             mapViewC.doubleTapZoomGesture = true;
             mapViewC.twoFingerTapGesture = true;
@@ -361,7 +361,7 @@ static const int BaseEarthPriority = kMaplyImageLayerDrawPriorityDefault;
         bbox.ll = MaplyCoordinateMakeWithDegrees(7.05090689853, 47.7675500593);
         bbox.ur = MaplyCoordinateMakeWithDegrees(8.06813647023, 49.0562323851);
         MaplyCoordinate center = MaplyCoordinateMakeWithDegrees((7.05090689853+8.06813647023)/2, (47.7675500593+49.0562323851)/2);
-        double height = [globeViewC findHeightToViewBounds:&bbox pos:center];
+        double height = [globeViewC findHeightToViewBounds:bbox pos:center];
         mapViewC.height = height;
         [globeViewC animateToPosition:center time:1.0];
         NSLog(@"height = %f",height);
@@ -881,7 +881,7 @@ static const int BaseEarthPriority = kMaplyImageLayerDrawPriorityDefault;
     operation.responseSerializer = [AFHTTPResponseSerializer serializer];
     [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject)
      {
-         MaplyVectorObject *vecObj = [MaplyVectorObject VectorObjectFromGeoJSON:responseObject];
+         MaplyVectorObject *vecObj = [[MaplyVectorObject alloc] initWithGeoJSON:responseObject];
          if (vecObj)
          {
              arcGisObj = [baseViewC addVectors:@[vecObj] desc:@{kMaplyColor: [UIColor redColor]}];
@@ -948,7 +948,7 @@ static const bool CountryTextures = true;
                          NSData *jsonData = [NSData dataWithContentsOfFile:fileName];
                          if (jsonData)
                          {
-                             MaplyVectorObject *wgVecObj = [MaplyVectorObject VectorObjectFromGeoJSON:jsonData];
+                             MaplyVectorObject *wgVecObj = [[MaplyVectorObject alloc] initWithGeoJSON:jsonData];
                              NSString *vecName = [[wgVecObj attributes] objectForKey:@"ADMIN"];
                              wgVecObj.userObject = vecName;
                              NSMutableDictionary *desc = [NSMutableDictionary dictionaryWithDictionary:@{
