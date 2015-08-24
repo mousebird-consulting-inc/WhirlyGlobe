1d0717149b5eb7345fb7c377cea3f8a21213661b
JM <jmnavarro@gmail.com>
2015-08-24 13:53:46 +0200

    Minor refinements

--- WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/include/MaplyStarsModel.h
+++ WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/include/MaplyStarsModel.h
@@ -41,7 +41,7 @@
     @param desc Additional parameters that may related to the geometry.
     @param mode Thread mode to use when adding the geometry.
   */
-- (void)addToViewC:(WhirlyGlobeViewController *__nonnull)viewC date:(NSDate *__nonnull)date desc:(NSDictionary *__nonnull)desc mode:(MaplyThreadMode)mode;
+- (void)addToViewC:(WhirlyGlobeViewController *__nonnull)viewC date:(NSDate *__nonnull)date desc:(NSDictionary *__nullable)desc mode:(MaplyThreadMode)mode;
 
 /** @brief Remove star geometry from the registered view controller.
     @details Removes any objects created for the star geometry.
