b58ff63b567c618af89b34da24e2fa0bc829243e
Steve Gifford <sjg@mousebirdconsulting.com>
2015-08-28 13:28:08 -0700

    Found a race condition on startup.  If you start the layer thread and then demand an immediate update, it crashes.  Will now just put the request on the queue.

--- WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/src/MaplyBaseInteractionLayer.mm
+++ WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/src/MaplyBaseInteractionLayer.mm
@@ -516,7 +516,11 @@ typedef std::set<ThreadChanges> ThreadChangeSet;
 {
     if (changes.empty())
         return;
-    
+
+    // This means we beat the layer thread setup, so we'll put this in orbit
+    if (!scene)
+        threadMode = MaplyThreadAny;
+
     switch (threadMode)
     {
         case MaplyThreadCurrent:
