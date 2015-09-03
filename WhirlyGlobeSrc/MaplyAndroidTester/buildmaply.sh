bbe3b77f9011455a5e4c16b0397dc7b60489a225
JM <jmnavarro@gmail.com>
2015-09-03 21:57:46 +0200

    Support architecture as command line paramter

--- WhirlyGlobeSrc/MaplyAndroidTester/buildmaply.sh
+++ WhirlyGlobeSrc/MaplyAndroidTester/buildmaply.sh
@@ -1,7 +1,14 @@
 rm app/libs/Maply.aar
 
+# You can use several architectures here, like x86,mips64
+if [ -n "$1" ]; then
+    arch=${1}
+else
+    arch="x86"
+fi
+
 pushd ../Android/
-./gradlew assembleRelease
+./gradlew assembleRelease -Parchitecture=$arch
 popd
 
 # Note: This is the debug version
