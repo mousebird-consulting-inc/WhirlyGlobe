3d9b6609e9c06de83fc7d2f56423e7a31b0450ec
Steve Gifford <sjg@mousebirdconsulting.com>
2015-09-11 15:22:09 -0700

    Messing with GDB Server internal to app.

--- WhirlyGlobeSrc/MaplyAndroidTester/app/src/main/java/com/mousebirdconsulting/maplyandroidtester/MapActivity.java
+++ WhirlyGlobeSrc/MaplyAndroidTester/app/src/main/java/com/mousebirdconsulting/maplyandroidtester/MapActivity.java
@@ -12,6 +12,7 @@ public class MapActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
