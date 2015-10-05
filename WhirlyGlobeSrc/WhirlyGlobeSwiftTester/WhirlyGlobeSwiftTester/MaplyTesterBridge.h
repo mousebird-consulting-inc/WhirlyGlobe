9aaf74cf6c82a3a8cdbd21695dc7f8f0e3264a91
JM <jmnavarro@gmail.com>
2015-10-05 23:17:15 +0200

    Complete Swift tester

---
+++ WhirlyGlobeSrc/WhirlyGlobeSwiftTester/WhirlyGlobeSwiftTester/MaplyTesterBridge.h
@@ -0,0 +1,18 @@
+//
+//  MaplyTesterBridge.h
+//  WhirlyGlobeSwiftTester
+//
+//  Created by jmnavarro on 16/09/15.
+//  Copyright (c) 2015 Mousebird. All rights reserved.
+//
+
+#ifndef WhirlyGlobeSwiftTester_MaplyTesterBridge_h
+#define WhirlyGlobeSwiftTester_MaplyTesterBridge_h
+
+#import "MaplyBridge.h"
+
+#import "AFNetworking.h"
+#import "DDXML.h"
+#import "MaplyPagingElevationTestTileSource.h"
+
+#endif
