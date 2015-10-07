f35fa5219e2c1cf0053bc65d0d2323ba56cb4897
Steve Gifford <sjg@mousebirdconsulting.com>
2015-10-07 16:02:03 -0700

    Turned off a few output statements.

--- WhirlyGlobeSrc/Android/jni/QuadImageTileLayer_jni.cpp
+++ WhirlyGlobeSrc/Android/jni/QuadImageTileLayer_jni.cpp
@@ -1296,7 +1296,6 @@ JNIEXPORT void JNICALL Java_com_mousebird_maply_QuadImageTileLayer_nativeTileDid
 		ChangeSet *changes = ChangeSetClassInfo::getClassInfo()->getObject(env,changesObj);
 		if (!adapter || !changes)
 		  {
-		    __android_log_print(ANDROID_LOG_VERBOSE, "Maply", "nativeTileDidLoad got bad adapter or changes.");
 			return;
 		  }
 
@@ -1322,7 +1321,7 @@ JNIEXPORT void JNICALL Java_com_mousebird_maply_QuadImageTileLayer_nativeTileDid
 
 		adapter->tileLoaded(level,x,y,frame,rawDataRef,info.width,info.height,*changes);
 		AndroidBitmap_unlockPixels(env, bitmapObj);
-		__android_log_print(ANDROID_LOG_VERBOSE, "Maply", "Tile did load: %d: (%d,%d) %d",level,x,y,frame);
+//		__android_log_print(ANDROID_LOG_VERBOSE, "Maply", "Tile did load: %d: (%d,%d) %d",level,x,y,frame);
     }
 	catch (...)
 	{
@@ -1340,7 +1339,7 @@ JNIEXPORT void JNICALL Java_com_mousebird_maply_QuadImageTileLayer_nativeTileDid
 		if (!adapter || !changes)
 			return;
 
-		__android_log_print(ANDROID_LOG_VERBOSE, "Maply", "Tile did not load: %d: (%d,%d) %d",level,x,y,frame);
+//		__android_log_print(ANDROID_LOG_VERBOSE, "Maply", "Tile did not load: %d: (%d,%d) %d",level,x,y,frame);
 		adapter->tileLoaded(level,x,y,frame,RawDataRef(),1,1,*changes);
 	}
 	catch (...)
