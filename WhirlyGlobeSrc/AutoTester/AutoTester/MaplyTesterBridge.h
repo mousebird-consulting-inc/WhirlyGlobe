eb00bafc7fb7b0961815e7df976d9ed5b142ba7e
JM <jmnavarro@gmail.com>
2015-10-13 16:12:26 +0200

    Basic scaffolding for AutoTester
    Former-commit-id: ff949e1544816810e22b788737181bc7c17dac1d

---
+++ WhirlyGlobeSrc/AutoTester/AutoTester/MaplyTesterBridge.h
@@ -0,0 +1,18 @@
+//
+//  MaplyTesterBridge.h
+//  WhirlyGlobeSwiftTester
+//
+//  Created by jmnavarro on 16/09/15.
+//  Copyright (c) 2015 Mousebird. All rights reserved.
+//
+
+#ifndef AutoTester_MaplyTesterBridge_h
+#define AutoTester_MaplyTesterBridge_h
+
+#import "MaplyBridge.h"
+
+#import "MaplyTestCase.h"
+#import "GeographyClassTestCase.h"
+
+
+#endif
