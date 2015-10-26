edf52fa481d1e0fa1b269077f06733cc6124d894
JM <jmnavarro@gmail.com>
2015-10-27 00:51:47 +0100

    Working on Cesium Elevation test (doesn’t work yet)

--- WhirlyGlobeSrc/AutoTester/AutoTester/testCases/CesiumElevationTestCase.m
+++ WhirlyGlobeSrc/AutoTester/AutoTester/testCases/CesiumElevationTestCase.m
@@ -7,6 +7,12 @@
 //
 
 #import "CesiumElevationTestCase.h"
+#import "MaplyPagingElevationTestTileSource.h"
+#import "MaplyRemoteTileElevationSource.h"
+#import "MaplyCoordinateSystem.h"
+#import "WhirlyGlobeComponent.h"
+
+
 
 @implementation CesiumElevationTestCase
 
@@ -15,7 +21,7 @@
 {
 	if (self = [super init]) {
 		self.name = @"Celsium Elevation";
-		self.captureDelay = 2;
+		self.captureDelay = 5;
 	}
     
 	return self;
@@ -24,6 +30,37 @@
 
 - (BOOL)setUpWithGlobe:(WhirlyGlobeViewController *)globeVC
 {
+	[globeVC setTiltMinHeight:0.001 maxHeight:0.04 minTilt:1.40 maxTilt:0.0];
+	globeVC.frameInterval = 2;  // 30fps
+
+	globeVC.clearColor = [UIColor colorWithWhite:0.5 alpha:1.0];
+
+	// Cesium as an elevation source
+	MaplyRemoteTileElevationCesiumSource *cesiumElev = [[MaplyRemoteTileElevationCesiumSource alloc] initWithBaseURL:@"http://cesiumjs.org/stk-terrain/tilesets/world/tiles/" ext:@"terrain" minZoom:0 maxZoom:16];
+
+	NSString *cacheDir = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES)  objectAtIndex:0];
+
+	cesiumElev.cacheDir = [NSString stringWithFormat:@"%@/cesiumElev/",cacheDir];
+
+	globeVC.elevDelegate = cesiumElev;
+
+	// Don't forget to turn on the z buffer permanently
+	[globeVC setHints:@{kMaplyRenderHintZBuffer: @(YES)}];
+
+	// Set up their odd tiling system
+	MaplyCesiumCoordSystem *cesiumCoordSys = [[MaplyCesiumCoordSystem alloc] init];
+	MaplyAnimationTestTileSource *tileSource = [[MaplyAnimationTestTileSource alloc] initWithCoordSys:cesiumCoordSys minZoom:1 maxZoom:16 depth:1];
+	tileSource.useDelay = false;
+	tileSource.transparentMode = false;
+	tileSource.pixelsPerSide = 128;
+	MaplyQuadImageTilesLayer *layer = [[MaplyQuadImageTilesLayer alloc] initWithCoordSystem:tileSource.coordSys tileSource:tileSource];
+	layer.requireElev = true;
+	layer.maxTiles = 256;
+	layer.handleEdges = true;
+	layer.numSimultaneousFetches = 8;
+	[globeVC addLayer:layer];
+	layer.drawPriority = 0;//BaseEarthPriority;
+
 	return true;
 }
 
