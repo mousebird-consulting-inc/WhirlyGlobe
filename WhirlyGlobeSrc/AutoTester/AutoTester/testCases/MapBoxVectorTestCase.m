8cfe9b014febc21692ab46e4fed52d506d927620
JM <jmnavarro@gmail.com>
2015-11-03 23:33:12 +0100

    MapboxVectorTestCase added
    Former-commit-id: 3e69e7f7dcc0d63d1b57ee748d6c0b78f030989c

--- WhirlyGlobeSrc/AutoTester/AutoTester/testCases/MapBoxVectorTestCase.m
+++ WhirlyGlobeSrc/AutoTester/AutoTester/testCases/MapBoxVectorTestCase.m
@@ -7,6 +7,7 @@
 //
 
 #import "MapBoxVectorTestCase.h"
+#import "MaplyMapnikVectorTiles.h"
 
 @implementation MapBoxVectorTestCase
 
@@ -23,6 +24,39 @@
 
 - (BOOL)setUpWithMap:(MaplyViewController *)mapVC
 {
+	// For network paging layers, where we'll store temp files
+	NSString *cacheDir = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES)  objectAtIndex:0];
+	NSString *thisCacheDir = nil;
+	thisCacheDir = [NSString stringWithFormat:@"%@/mapbox-streets-vectiles",cacheDir];
+	[MaplyMapnikVectorTiles StartRemoteVectorTilesWithTileSpec:@"http://a.tiles.mapbox.com/v3/mapbox.mapbox-streets-v4.json"
+			   style:[[NSBundle mainBundle] pathForResource:@"osm-bright" ofType:@"xml"]
+			cacheDir:thisCacheDir
+			   viewC:baseViewC
+			 success: ^(MaplyMapnikVectorTiles *vecTiles) {
+				// Don't load the lowest levels for the globe
+				if (globeViewC)
+				 vecTiles.minZoom = 5;
+
+				// Note: These are set after the MapnikStyleSet has already been initialized
+				MapnikStyleSet *styleSet = (MapnikStyleSet *)vecTiles.styleDelegate;
+				styleSet.tileStyleSettings.markerImportance = 10.0;
+				styleSet.tileStyleSettings.fontName = @"Gill Sans";
+
+				// Now for the paging layer itself
+				MaplyQuadPagingLayer *pageLayer = [[MaplyQuadPagingLayer alloc] initWithCoordSystem:[[MaplySphericalMercator alloc] initWebStandard] delegate:vecTiles];
+				pageLayer.numSimultaneousFetches = 6;
+				pageLayer.flipY = false;
+				pageLayer.importance = 1024*1024*2;
+				pageLayer.useTargetZoomLevel = true;
+				pageLayer.singleLevelLoading = true;
+				[baseViewC addLayer:pageLayer];
+				ovlLayers[layerName] = pageLayer;
+			}
+			failure: ^(NSError *error) {
+				NSLog(@"Failed to load Mapnik vector tiles because: %@",error);
+			}
+	];
+
 	return true;
 }
 
