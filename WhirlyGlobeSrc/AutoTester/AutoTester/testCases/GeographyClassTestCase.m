e78adf8e6a39e41ff145c353ae2fbed6f0d9db82
JM <jmnavarro@gmail.com>
2015-11-03 23:27:25 +0100

    Refactor repeated code

--- WhirlyGlobeSrc/AutoTester/AutoTester/testCases/GeographyClassTestCase.m
+++ WhirlyGlobeSrc/AutoTester/AutoTester/testCases/GeographyClassTestCase.m
@@ -10,7 +10,17 @@
 #import "WhirlyGlobeComponent.h"
 
 
+@interface GeographyClassTestCase()
+
+@property (nonatomic, strong) MaplyMBTileSource *tileSource;
+@property (nonatomic, strong) MaplyQuadImageTilesLayer *layer;
+
+@end
+
+
 @implementation GeographyClassTestCase
+
+
 
 - (instancetype)init
 {
@@ -22,22 +32,26 @@
 	return self;
 }
 
+- (void) setup{
+	// set up the data source
+	self.tileSource =
+	[[MaplyMBTileSource alloc] initWithMBTiles:@"geography-class_medres"];
+
+	// set up the layer
+	self.layer = [[MaplyQuadImageTilesLayer alloc] initWithTileSource:self.tileSource];
+	self.layer.handleEdges = true;
+	self.layer.coverPoles = true;
+	self.layer.requireElev = false;
+	self.layer.waitLoad = false;
+	self.layer.drawPriority = 0;
+	self.layer.singleLevelLoading = false;
+}
+
 
 - (BOOL)setUpWithGlobe:(WhirlyGlobeViewController *)globeVC
 {
-	// set up the data source
-	MaplyMBTileSource *tileSource =
-		[[MaplyMBTileSource alloc] initWithMBTiles:@"geography-class_medres"];
-
-	// set up the layer
-	MaplyQuadImageTilesLayer *layer = [[MaplyQuadImageTilesLayer alloc] initWithTileSource:tileSource];
-	layer.handleEdges = true;
-	layer.coverPoles = true;
-	layer.requireElev = false;
-	layer.waitLoad = false;
-	layer.drawPriority = 0;
-	layer.singleLevelLoading = false;
-	[globeVC addLayer:layer];
+	[self setup];
+	[globeVC addLayer:self.layer];
 
 	globeVC.height = 0.8;
 	[globeVC animateToPosition:MaplyCoordinateMakeWithDegrees(-3.6704803, 40.5023056) time:1.0];
@@ -47,19 +61,8 @@
 
 - (BOOL)setUpWithMap:(MaplyViewController *)mapVC
 {
-	// set up the data source
-	MaplyMBTileSource *tileSource =
-	[[MaplyMBTileSource alloc] initWithMBTiles:@"geography-class_medres"];
-
-	// set up the layer
-	MaplyQuadImageTilesLayer *layer = [[MaplyQuadImageTilesLayer alloc] initWithTileSource:tileSource];
-	layer.handleEdges = true;
-	layer.coverPoles = true;
-	layer.requireElev = false;
-	layer.waitLoad = false;
-	layer.drawPriority = 0;
-	layer.singleLevelLoading = false;
-	[mapVC addLayer:layer];
+	[self setup];
+	[mapVC addLayer:self.layer];
 
 	mapVC.height = 0.8;
 	[mapVC animateToPosition:MaplyCoordinateMakeWithDegrees(-3.6704803, 40.5023056) time:1.0];
