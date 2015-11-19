5cb18696598bc348d3506b3200dab64e8f2b6ad2
Steve Gifford <sjg@mousebirdconsulting.com>
2015-11-19 08:44:27 -0800

    Part way through new widen implementation.
    Former-commit-id: e8933226a3975bceb8860318006f84050214366d

--- WhirlyGlobeSrc/WhirlyGlobeLib/src/WideVectorDrawable.mm
+++ WhirlyGlobeSrc/WhirlyGlobeLib/src/WideVectorDrawable.mm
@@ -31,31 +31,32 @@ namespace WhirlyKit
 WideVectorDrawable::WideVectorDrawable() : BasicDrawable("WideVector"), texRepeat(1.0)
 {
     lineWidth = 10.0/1024.0;
-    offsetIndex = addAttribute(BDFloat3Type, "a_dir");
-    lenIndex = addAttribute(BDFloatType, "a_maxlen");
+    p01_index = addAttribute(BDFloat3Type, "a_p01");
+    t0_limit_index = addAttribute(BDFloatType, "a_t0_limit");
+    n0_index = addAttribute(BDFloat3Type, "a_n0");
+    c0_index = addAttribute(BDFloatType, "a_c0");
 }
     
-void WideVectorDrawable::addDir(const Point3f &dir)
+void WideVectorDrawable::add_P01(const Point3f &dir)
 {
-    addAttributeValue(offsetIndex, dir);
-}
-    
-void WideVectorDrawable::addDir(const Point3d &dir)
-{
-    addAttributeValue(offsetIndex, Point3f(dir.x(),dir.y(),dir.z()));
-
-//    // Note: Debugging
-//    dirs.push_back(Point3d(dir.x(),dir.y(),dir.z()));
+    addAttributeValue(p01_index, dir);
 }
 
-void WideVectorDrawable::addMaxLen(double len)
+void WideVectorDrawable::add_n0(const Point3f &dir)
 {
-    addAttributeValue(lenIndex, len);
-    
-//    // Note: Debugging
-//    lens.push_back(len);
+    addAttributeValue(n0_index, dir);
 }
-    
+
+void WideVectorDrawable::add_t0_limit(float minVal,float maxVal)
+{
+    addAttributeValue(t0_limit_index, Point2f(minVal,maxVal));
+}
+
+void WideVectorDrawable::add_c0(float val)
+{
+    addAttributeValue(c0_index, val);
+}
+
 void WideVectorDrawable::draw(WhirlyKitRendererFrameInfo *frameInfo, Scene *scene)
 {
 //    double u_length = 0.0;
@@ -66,11 +67,11 @@ void WideVectorDrawable::draw(WhirlyKitRendererFrameInfo *frameInfo, Scene *scen
     {
         float scale = std::max(frameInfo.sceneRenderer.framebufferWidth,frameInfo.sceneRenderer.framebufferHeight);
         float screenSize = frameInfo.screenSizeInDisplayCoords.x();
-        float pixDispSize = std::min(frameInfo.screenSizeInDisplayCoords.x(),frameInfo.screenSizeInDisplayCoords.y()) / scale;
-        frameInfo.program->setUniform("u_scale", 1.f/scale);
-        frameInfo.program->setUniform("u_length", lineWidth/scale);
-        frameInfo.program->setUniform("u_pixDispSize", pixDispSize);
-        frameInfo.program->setUniform("u_lineWidth", lineWidth);
+//        float pixDispSize = std::min(frameInfo.screenSizeInDisplayCoords.x(),frameInfo.screenSizeInDisplayCoords.y()) / scale;
+//        frameInfo.program->setUniform("u_scale", 1.f/scale);
+        frameInfo.program->setUniform("u_w2", lineWidth/(2.f*scale));
+//        frameInfo.program->setUniform("u_pixDispSize", pixDispSize);
+//        frameInfo.program->setUniform("u_lineWidth", lineWidth);
         float texScale = scale/(screenSize*texRepeat);
         frameInfo.program->setUniform("u_texScale", texScale);
         
@@ -96,19 +97,21 @@ void WideVectorDrawable::draw(WhirlyKitRendererFrameInfo *frameInfo, Scene *scen
 
 static const char *vertexShaderTri =
 "uniform mat4  u_mvpMatrix;\n"
-"uniform mat4  u_mvMatrix;\n"
-"uniform mat4  u_pMatrix;\n"
+//"uniform mat4  u_mvMatrix;\n"
+//"uniform mat4  u_pMatrix;\n"
 "uniform float u_fade;\n"
-"uniform float u_length;\n"
+"uniform float u_w2;\n"
 "uniform float u_texScale;\n"
-"uniform float u_pixDispSize;\n"
-"uniform float u_lineWidth;\n"
+//"uniform float u_pixDispSize;\n"
+//"uniform float u_lineWidth;\n"
 "\n"
 "attribute vec3 a_position;\n"
 "attribute vec2 a_texCoord0;\n"
 "attribute vec4 a_color;\n"
-"attribute vec3 a_dir;\n"
-"attribute float a_maxlen;\n"
+"attribute vec3 a_p01;\n"
+"attribute vec2 a_t0_limit;\n"
+"attribute vec3 a_n0;\n"
+"attribute float a_c0;\n"
 "\n"
 "varying vec2 v_texCoord;\n"
 "varying vec4 v_color;\n"
@@ -119,10 +122,11 @@ static const char *vertexShaderTri =
 "   v_color = a_color;\n"
 "   vec4 vertPos = u_mvpMatrix * vec4(a_position,1.0);\n"
 "   vertPos /= vertPos.w;\n"
-"   vec2 screenDir = (u_mvpMatrix * vec4(a_dir,0.0)).xy;\n"
-"   vec2 calcOff = screenDir * u_length;\n"
-"   if (u_pixDispSize * length(a_dir) * u_lineWidth > a_maxlen && a_maxlen > 0.0)\n"
-"      calcOff = vec2(0,0);\n"
+"   vec2 screen_p01 = (u_mvpMatrix * vec4(a_p01,0.0)).xy;\n"
+"   vec2 screen_n0 = (u_mvpMatrix * vec4(a_n0,0.0)).xy;\n"
+"   float t0 = a_c0 * u_w2;\n"
+"   t0 = clamp(t0,a_t0_limit.x,a_t0_limit.y);\n"
+"   vec2 calcOff = screen_p01 * t0 + screen_n0 * a_w2;\n"
 "   gl_Position = vertPos + vec4(calcOff,0,0);\n"
 "}\n"
 ;
