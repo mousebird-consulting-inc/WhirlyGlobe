f284ea02c5cab3d8e9c6bb37727bd5ea7a4cba4c
JM <jmnavarro@gmail.com>
2015-11-27 11:53:33 +0100

    Add pods stuff as Cocoapods doc recommends
    Former-commit-id: 94835877fc52e517ab0adb24f8ae10c1b8189ea3

---
+++ WhirlyGlobeSrc/AutoTester/Pods/SwiftyDropbox/Source/SwiftyDropbox.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyDropbox.
+FOUNDATION_EXPORT double SwiftyDropboxVersionNumber;
+
+//! Project version string for SwiftyDropbox.
+FOUNDATION_EXPORT const unsigned char SwiftyDropboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyDropbox/PublicHeader.h>
+
+
