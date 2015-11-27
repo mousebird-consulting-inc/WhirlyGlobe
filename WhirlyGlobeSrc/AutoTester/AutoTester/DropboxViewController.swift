24a24069cda5577183705dbddea703b804c79351
JM <jmnavarro@gmail.com>
2015-11-27 13:01:56 +0100

    Add resize image and change some styles

--- WhirlyGlobeSrc/AutoTester/AutoTester/DropboxViewController.swift
+++ WhirlyGlobeSrc/AutoTester/AutoTester/DropboxViewController.swift
@@ -22,7 +22,7 @@ class DropboxSection {
 		case LogoutUser = "Logout"
 		
 		case Upload = "Upload Tests"
-        case Back = "Back"
+		case Back = "Back"
 	}
 	
 	var section : Section
@@ -36,7 +36,7 @@ class DropboxSection {
 		}
 		else {
 			self.rows = isLogin ? [.Upload] : []
-            self.rows.append(.Back)
+			self.rows.append(.Back)
 		}
 	}
 
@@ -130,11 +130,11 @@ class DropboxViewController: UIViewController, UITableViewDataSource, UITableVie
 				case .Upload:
 					uploadToDropbox()
 					break;
-                case .Back:
-                    self.navigationController?.popViewControllerAnimated(true)
-                    break;
+				case .Back:
+					self.navigationController?.popViewControllerAnimated(true)
+					break;
 			}
-            tableView.deselectRowAtIndexPath(indexPath, animated: false)
+			tableView.deselectRowAtIndexPath(indexPath, animated: false)
 			
 	}
 
@@ -149,150 +149,245 @@ class DropboxViewController: UIViewController, UITableViewDataSource, UITableVie
 		if processLogin {
 			// Quick & dirty hack. Instead of add delay, we should
 			// wait for dropbox notification
-            let delay = 2 * Double(NSEC_PER_SEC)
-            let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
-            dispatch_after(time, dispatch_get_main_queue()) {
-                if Dropbox.authorizedClient != nil {
-                    self.loadValues()
-                }
-                self.tableView?.reloadData()
-                self.processLogin = false
-            }
+			let delay = 2 * Double(NSEC_PER_SEC)
+			let time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+			dispatch_after(time, dispatch_get_main_queue()) {
+				if Dropbox.authorizedClient != nil {
+					self.loadValues()
+				}
+				self.tableView?.reloadData()
+				self.processLogin = false
+			}
 		}
 	}
 	
-	func uploadToDropbox () {
-		if let client = Dropbox.authorizedClient {
-			let loadingNotification = MBProgressHUD.showHUDAddedTo(self.view, animated:true)
-			loadingNotification.mode = MBProgressHUDMode.Indeterminate
-			loadingNotification.labelText = "Starting"
-			let date = NSDate()
-			let dateFormatter = NSDateFormatter()
-			dateFormatter.dateFormat = "dd-MM-yyy HH:mm"
-			self.folder = dateFormatter.stringFromDate(date) as String
-			self.createFolderAndUploadImages(client,  loadingNotification: loadingNotification)
-		}
-	}
-	
-	func createFolderAndUploadImages (client: DropboxClient, loadingNotification: MBProgressHUD?) -> (Void){
-		loadingNotification?.labelText = "Creating folder"
-		client.files.createFolder(path: "/\(self.folder!)").response { (response, error) -> Void in
-			if response != nil {
-				self.uploadImages(client, loadingNotification: loadingNotification)
+	func resizeImage(image: UIImage) -> (NSData?) {
+		var actualHeight = image.size.height
+		var actualWidth = image.size.width
+
+		// Magic number: this is the size of baselines images.
+		let maxHeight = 1008.0
+		let maxWidth = 702.0
+
+		var imgRatio = actualWidth / actualHeight
+		let maxRatio = maxWidth / maxHeight
+		let compressionQuality = 0.5 as CGFloat
+
+		if actualHeight > CGFloat(maxHeight) || actualWidth > CGFloat(maxWidth) {
+			if imgRatio < CGFloat(maxRatio) {
+				imgRatio = CGFloat(maxHeight) / actualHeight
+				actualWidth = imgRatio * actualWidth
+				actualHeight = CGFloat(maxHeight)
+			}
+			else if imgRatio > CGFloat(maxRatio) {
+				imgRatio = CGFloat(maxWidth) / actualWidth
+				actualHeight = imgRatio * actualHeight
+				actualWidth = CGFloat(maxWidth)
 			}
 			else {
-				print(error!)
-				loadingNotification?.hide(true)
+				actualHeight = CGFloat(maxHeight)
+				actualWidth = CGFloat (maxWidth)
+			}
+		}
+
+		let rect = CGRectMake(0.0, 0.0, actualWidth, actualHeight)
+		UIGraphicsBeginImageContext(rect.size)
+		image.drawInRect(rect)
+		let img = UIGraphicsGetImageFromCurrentImageContext()
+		let imageData = UIImageJPEGRepresentation(img, compressionQuality)
+		UIGraphicsEndImageContext()
+
+		return imageData
+	}
+
+	func uploadToDropbox () {
+		if let client = Dropbox.authorizedClient {
+			let hud = MBProgressHUD.showHUDAddedTo(self.view, animated:true)
+			hud.mode = MBProgressHUDMode.Indeterminate
+			hud.labelText = "Starting..."
+
+			let date = NSDate()
+			let dateFormatter = NSDateFormatter()
+			dateFormatter.dateFormat = "yyy-MM-dd HH-mm"
+			self.folder = dateFormatter.stringFromDate(date) as String
+
+			self.createFolder(client, hud: hud) {
+				self.uploadImages(client, hud: hud) {
+					self.uploadMetadataJSON(client, hud: hud) {
+						self.createHTMLTestResult(client, hud: hud) {
+							self.uploadCSSFile(client, hud: hud)
+						}
+					}
+				}
 			}
 		}
 	}
 	
-	func uploadImages(client: DropboxClient, loadingNotification: MBProgressHUD?){
-		loadingNotification?.labelText = "Uploading images"
+	func createFolder(client: DropboxClient,
+			hud: MBProgressHUD,
+			continuation: () -> ()) {
+
+		hud.labelText = "Creating folders..."
+
+		client.files.createFolder(
+			path: "/\(self.folder!)").response { response, error in
+				if let error = error {
+					print("Error creating folder structure: \(error)")
+					hud.hide(true)
+				}
+				else {
+					continuation()
+				}
+		}
+	}
+	
+	func uploadImages(client: DropboxClient,
+			hud: MBProgressHUD,
+			continuation: () -> ()) {
+
+		hud.labelText = "Uploading images"
 		var uploads = self.results.count
-		for var i = 0; i < self.results.count; ++i {
-			let result = self.results[i]
+
+		for (i,result) in self.results.enumerate() {
 			let title = titles[i]
-			if  let image = result.actualImageFile {
-				let data = NSData.init(contentsOfFile: image)
+			if let actualImage = result.actualImageFile,
+					data = NSData.init(contentsOfFile: actualImage),
+					image = UIImage(data: data),
+					imageToUpload = self.resizeImage(image) {
+
 				let imageName = title.stringByReplacingOccurrencesOfString(" ", withString: "")
-				client.files.upload(path: "/\(self.folder!)/images/\(imageName)-actual.png", body: data!).response({ (response, error) -> Void in
-					if let file = response {
-						print("Uploaded file name:\(file.name)")
-					}
-					else {
-						print(error!)
-					}
-					uploads--
-					if uploads == 0 {
-						self.createJson(client, loadingNotification: loadingNotification)
-					}
+
+				client.files.upload(
+					path: "/\(self.folder!)/images/\(imageName)-actual.jpg",
+					body: imageToUpload).response({ (response, error) -> () in
+						if let error = error {
+							print("Error uploading \(imageName): \(error)")
+						}
+						else {
+							print("Uploaded file name: \(response?.name)")
+						}
+
+						uploads--
+						if uploads == 0 {
+							continuation()
+						}
 				})
 			}
 		}
 	}
 
-	func createJson(client: DropboxClient, loadingNotification: MBProgressHUD?) {
-		loadingNotification?.labelText = "Generating json"
-		let device = UIDevice.currentDevice()
-		let jsonObject: [String: AnyObject] = [
-			"Test Name" : self.folder!,
-			"Name" : device.name,
-			"System Name": device.systemName,
-			"System Version": device.systemVersion,
-			"Model": device.model,
-			"Localized Model": device.localizedModel,
-			"tests count" : self.results.count
-		]
-		let valid = NSJSONSerialization.isValidJSONObject(jsonObject)
-		if valid {
-			do {
-				let data :NSData? = try NSJSONSerialization.dataWithJSONObject(jsonObject, options: .PrettyPrinted)
-				client.files.upload(path:"/\(self.folder!)/data.json", body: data!).response({ (response, error) -> Void in
-					if  let file = response {
-						print("Uploaded json file name:\(file.name)")
-					}
-					else {
-						print("Error creating json file")
-					}
-					self.createHTMLTestResult(client, loadingNotification: loadingNotification)
-				})
-			}
-			catch let myJSONError  {
-				print(myJSONError)
-				loadingNotification?.hide(true)
-			}
+	func uploadMetadataJSON(client: DropboxClient,
+			hud: MBProgressHUD,
+			continuation: () -> ()) {
+
+		func createMetadata() -> [String : AnyObject] {
+			let device = UIDevice.currentDevice()
+
+			return [
+				"Test Name" : self.folder!,
+				"Name" : device.name,
+				"System Name": device.systemName,
+				"System Version": device.systemVersion,
+				"Model": device.model,
+				"Localized Model": device.localizedModel,
+				"tests count" : self.results.count
+			]
 		}
-		else {
-			loadingNotification?.hide(true)
-			print("JSON not valid")
-		}
-		
-	}
-	
-	func createHTMLTestResult (client: DropboxClient, loadingNotification: MBProgressHUD?){
-		
-		var htmlString : String?
-		loadingNotification?.labelText = "Creating html file"
-		htmlString = "<!DOCTYPE html><html<head><meta charset=\"UTF-8\"><meta name=\"description\" content=\"Execution results of tests\"><link type=\"text/css\" rel=\"stylesheet\" href=\"css/styles.css\"><Title>\(self.folder!)</Title></head><body><h1>Tests Results - \(self.folder!)</h1>"
-		for var i = 0; i < self.results.count; ++i {
-			let result = self.results[i]
-			let compImgName = result.baselineImageFile.componentsSeparatedByString("/")
-			let baselineImage = compImgName[compImgName.count-1]
-			htmlString = htmlString! + "<div id=\"tests\"><h2>TEST \(result.testName)</h2><div id=\"expected\"><h3>Expected</h3><img src=\"images/\(baselineImage)\"></div><div id=\"actual\"><h3>Actual</h3><img src=\"images/\(self.titles[i].stringByReplacingOccurrencesOfString(" ", withString: ""))-actual.png\"></div></div>"
-		}
-		htmlString = htmlString! + "</body></html>"
-		let data = htmlString?.dataUsingEncoding(NSUTF8StringEncoding)
-		client.files.upload(path: "/\(self.folder!)/index.html", body: data!).response { (response, error) -> Void in
-			if let file = response{
-				print("Uploaded html file name:\(file.name)")
-				self.uploadCSSFile(client, loadingNotification: loadingNotification)
-			}
-			else {
-				print("Error creating html file")
-				loadingNotification?.hide(true)
-			}
-		}
-	}
-	
-	func uploadCSSFile(client: DropboxClient, loadingNotification: MBProgressHUD?) {
-		loadingNotification?.labelText = "Uploading CSS File"
-		let path = NSBundle.mainBundle().pathForResource("styles", ofType: "css")
-		if let path = path {
-			let data = NSData(contentsOfFile: path)
-			client.files.upload(path: "/\(self.folder!)/css/styles.css", body: data!).response({ (response, error) -> Void in
-				if let file = response{
-					print("Uploaded css file name:\(file.name)")
+
+		func createJSONDataFromDict(dict: [String: AnyObject]) throws -> NSData {
+			if NSJSONSerialization.isValidJSONObject(dict) {
+				//			do {
+				if let data = try? NSJSONSerialization.dataWithJSONObject(dict,
+					options: .PrettyPrinted) {
+						return data
 				}
 				else {
-					print("Error uploadling css file")
+					throw NSError(domain: "AutoTester", code: 1, userInfo: nil)
 				}
-				loadingNotification?.hide(true)
+			}
+			else {
+				throw NSError(domain: "AutoTester", code: 2, userInfo: nil)
+			}
+			
+		}
+
+		hud.labelText = "Generating metadata..."
+
+		do {
+			let data = try createJSONDataFromDict(createMetadata())
+			client.files.upload(
+				path:"/\(self.folder!)/data.json",
+				body: data).response({ (response, error) in
+					if let error = error {
+						print("Error uploading json file: \(error)")
+					}
+					else {
+						print("Uploaded json file name: \(response?.name)")
+						continuation()
+					}
+				})
+
+		}
+		catch let error {
+			print("Error creating json file: \(error)")
+			hud.hide(true)
+		}
+	}
+
+
+	func createHTMLTestResult(client: DropboxClient,
+			hud: MBProgressHUD,
+			continuation: () -> ()) {
+
+		hud.labelText = "Creating html file"
+
+		var htmlString = "<!DOCTYPE html><html<head><meta charset=\"UTF-8\"><meta name=\"description\" content=\"Execution results of tests\"><link type=\"text/css\" rel=\"stylesheet\" href=\"css/styles.css\"><Title>\(self.folder!)</Title></head><body><h1>Tests Results - \(self.folder!)</h1>"
+
+		for (i,result) in self.results.enumerate() {
+			let title = self.titles[i].stringByReplacingOccurrencesOfString(" ", withString: "")
+			let compImgName = result.baselineImageFile.componentsSeparatedByString("/")
+			let baselineImage = compImgName[compImgName.count-1]
+
+			htmlString += "<div id=\"tests\"><h2>TEST \(result.testName)</h2><div id=\"expected\"><h3>Expected</h3><img src=\"images/\(baselineImage)\"></div><div id=\"actual\"><h3>Actual</h3><img src=\"images/\(title)-actual.jpg\"></div></div>"
+		}
+
+		htmlString += "</body></html>"
+		let data = htmlString.dataUsingEncoding(NSUTF8StringEncoding)
+
+		client.files.upload(
+			path: "/\(self.folder!)/index.html",
+			body: data!).response { (response, error) in
+				if let file = response{
+					print("Uploaded html file name: \(file.name)")
+					continuation()
+				}
+				else {
+					print("Error creating html file")
+					hud.hide(true)
+				}
+			}
+	}
+	
+	func uploadCSSFile(client: DropboxClient, hud: MBProgressHUD) {
+		hud.labelText = "Uploading CSS File"
+
+		if let path = NSBundle.mainBundle().pathForResource("styles", ofType: "css"),
+				data = NSData(contentsOfFile: path) {
+			client.files.upload(
+				path: "/\(self.folder!)/css/styles.css",
+				body: data).response({ response, error in
+					if let error = error {
+						print("Error uploadling css file: \(error)")
+					}
+					else {
+						print("Uploaded css file name:\(response?.name)")
+						hud.hide(true)
+					}
 			})
 		}
 		else {
-			print("Error uploading css file")
-			loadingNotification?.hide(true)
+			print("Error getting css file")
+			hud.hide(true)
 		}
 	}
 
