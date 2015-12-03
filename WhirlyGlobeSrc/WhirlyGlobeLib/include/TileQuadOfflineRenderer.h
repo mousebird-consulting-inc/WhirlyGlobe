623b9bf6ec96ae5372c29e61f6379c5c193d5809
Steve Gifford <sjg@mousebirdconsulting.com>
2015-12-03 13:38:01 -0800

    First step in porting offline renderer.

---
+++ WhirlyGlobeSrc/WhirlyGlobeLib/include/TileQuadOfflineRenderer.h
@@ -0,0 +1,212 @@
+/*
+ *  TileQuadOfflineRenderer.h
+ *  WhirlyGlobeLib
+ *
+ *  Created by Steve Gifford on 10/7/13.
+ *  Copyright 2011-2013 mousebird consulting
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+#import <math.h>
+#import "WhirlyVector.h"
+#import "Scene.h"
+#import "GlobeMath.h"
+#import "Quadtree.h"
+#import "QuadDisplayController.h"
+#import "TileQuadLoader.h"
+
+namespace WhirlyKit
+{
+
+/// The offline renderer passes over images like so
+class QuadTileOfflineImage
+{
+public:
+    /// Size of each of the corner pixels in meters
+    Point2d cornerSizes[4];
+
+    /// Bounding box for the rendered area
+    Mbr mbr;
+    
+    /// Textures produced by the offline renderer.  Delegate is responsible for cleanup
+    std::vector<WhirlyKit::SimpleIdentity> textures;
+    
+    /// Size of the center pixel in meters
+    Point2d centerSize;
+};
+    
+class QuadTileOfflineLoader;
+
+// Internal storage for offline tile
+class OfflineTile
+{
+public:
+    OfflineTile();
+    OfflineTile(const WhirlyKit::Quadtree::Identifier &ident);
+    OfflineTile(std::vector<LoadedImage *>loadImages);
+    ~OfflineTile();
+    
+    // Return the size of this tile
+    void GetTileSize(int &numX,int &numY);
+    
+    // Details of which node we're representing
+    WhirlyKit::Quadtree::Identifier ident;
+    
+    /// Set if this is just a placeholder (no geometry)
+    bool placeholder;
+    /// Set if this tile is in the process of loading
+    bool isLoading;
+    
+    std::vector<LoadedImage *> images;
+};
+
+typedef struct
+{
+    /// Comparison operator based on node identifier
+    bool operator() (const OfflineTile *a,const OfflineTile *b)
+    {
+        return a->ident < b->ident;
+    }
+} OfflineTileSorter;
+
+/// A set that sorts loaded MB Tiles by Quad tree identifier
+typedef std::set<OfflineTile *,OfflineTileSorter> OfflineTileSet;
+
+/** Fill in this delegate to receive the UIImage this layer
+    generates every period seconds.
+  */
+class QuadTileOfflineDelegate
+{
+public:
+    virtual ~QuadTileOfflineDelegate() { }
+    /// Here's the generated image.  Query the loader for extents.
+    virtual void offlineRender(QuadTileOfflineLoader *loader,QuadTileOfflineImage *image);
+};
+    
+/** This version of the quad tile loader requests and tracks images the same
+    as the normal one.  Then it assembles them into a single large image
+    covering a given set of extents.  This lets us do 'postage stamp' style
+    display of a big complex, areal.
+  */
+class QuadTileOfflineLoader : public QuadLoader, QuadTileLoaderSupport
+{
+public:
+    /// Set this up with an object that'll return an image per tile and a name (for debugging)
+    QuadTileOfflineLoader(const std::string &name,QuadTileImageDataSource *imageSource);
+    ~QuadTileOfflineLoader();
+    void clear();
+    
+    /// Set if we're doing any rendering.  On by default.
+    void setOn(bool newOn) { on = newOn; }
+
+    /// Depth of the image stack per tile
+    void setNumImages(int newNumImages) { numImages = newNumImages; }
+    
+    /// Size (in pixels) of the output image we're building
+    void setImageSize(int newSizeX,int newSizeY) { sizeX = newSizeX;  sizeY = newSizeY; }
+    
+    /// If set, the output size is a maximum.  We'll try to track input resolution
+    bool setAutoRes(bool newAutoRes) { autoRes = newAutoRes; }
+    
+    /// The bounding box for the image we're trying to build
+    void setMbr(Mbr newMbr);
+    
+    /// We want the image generate no more often than this
+    void setPeriod(TimeInterval newPeriod);
+    
+    /// When the MBR changes we get a preview render down to this many levels
+    void setPreviewLevels(int newPreviewLevels) { previewLevels = newPreviewLevels; }
+    
+    /// If set, the delegate that receives the image we're generating every period seconds
+    void setOutputDelegate(QuadTileOfflineDelegate *newOutputDelegate) { outputDelegate = newOutputDelegate; }
+
+    /// Called when the layer first starts up.  Keep this around if you need it.
+    virtual void init(QuadDisplayController *inControl,Scene *inScene) { control = inControl; scene = inScene;}
+    
+    /// The quad layer uses this to see if a loader is capable of loading
+    ///  another tile.  Use this to track simultaneous loads
+    virtual bool isReady();
+    
+    /// Called right before we start a series of updates
+    virtual void startUpdates(ChangeSet &changes) { }
+    
+    /// Called right after we finish a series of updates
+    virtual void endUpdates(ChangeSet &changes) { }
+    
+    /// The quad tree wants to load the given tile.
+    /// Call the layer back when the tile is loaded.
+    /// This is in the layer thread.
+    virtual void loadTile(const Quadtree::NodeInfo &tileInfo,int frame) = 0;
+    
+    /// Quad tree wants to unload the given tile immediately.
+    /// This is in the layer thread.
+    virtual void unloadTile(const Quadtree::NodeInfo &tileInfo) = 0;
+    
+    /// The layer is checking to see if it's allowed to traverse below the given tile.
+    /// If the loader is still trying to load that given tile (or has some other information about it),
+    ///  then return false.  If the tile is loaded and the children may be valid, return true.
+    virtual bool canLoadChildrenOfTile(const Quadtree::NodeInfo &tileInfo) = 0;
+    
+    /// Called when the layer is about to shut down.  Clear out any drawables and caches.
+    virtual void shutdownLayer(ChangeSet &changes) = 0;
+    
+    /// Number of frames of animation per tile (if we're doing animation)
+    virtual int numFrames() = 0;
+    
+    /// If we're doing animation, currently active frame.
+    /// If numFrames = 1, this should be -1.  If numFrames > 1, -1 means load all frames at once
+    virtual int currentFrame() = 0;
+    
+    /// Set if the loader can handle individual frame loading
+    virtual bool canLoadFrames() = 0;
+    
+    /// Called right before the view update to determine if we should even be paging
+    /// You can use this to temporarily suspend paging.
+    /// isInitial is set if this is the first time through
+    virtual bool shouldUpdate(ViewState *viewState,bool isInitial) = 0;
+    
+    /// If this is filled in, we can do a hard reset while the layer is running.
+    /// This is pretty much identical to shutdownLayer:scene: but we expect to run again afterwards.
+    virtual void reset(ChangeSet &changes) = 0;
+    
+    void loadedImage(QuadTileImageDataSource *dataSource,LoadedImage *loadImage,int level,int col,int row,int frame,ChangeSet &changes);
+    
+protected:
+    void imageRenderToLevel(int deep);
+    Point2d calculateSize();
+    Point2d pixelSizeForMbr(const Mbr &theMbr,const Point2d &texSize,const Point2d &texel);
+    OfflineTile *getTile(const WhirlyKit::Quadtree::Identifier &ident);
+    
+    std::string name;
+    QuadTileImageDataSource *imageSource;
+    
+    bool on;
+    int numImages;
+    int sizeX,sizeY;
+    bool autoRes;
+    Mbr theMbr;
+    TimeInterval period;
+    int previewLevels;
+    QuadTileOfflineDelegate *outputDelegate;
+    
+    QuadDisplayController *quadControl;
+    OfflineTileSet tiles;
+    int numFetches;
+    bool renderScheduled,immediateScheduled;
+    TimeInterval lastRender;
+    bool somethingChanged;
+    int currentMbr;
+};
+    
+}
---
+++ WhirlyGlobeSrc/WhirlyGlobeLib/src/TileQuadOfflineRenderer.cpp
@@ -0,0 +1,534 @@
+/*
+ *  TileQuadOfflineRenderer.mm
+ *  WhirlyGlobeLib
+ *
+ *  Created by Steve Gifford on 10/7/13.
+ *  Copyright 2011-2013 mousebird consulting
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+#import "TileQuadOfflineRenderer.h"
+#import "FlatMath.h"
+
+using namespace Eigen;
+
+namespace WhirlyKit
+{
+    
+OfflineTile::OfflineTile()
+ : isLoading(false), placeholder(false)
+{
+};
+
+OfflineTile::OfflineTile(const WhirlyKit::Quadtree::Identifier &ident)
+: ident(ident), isLoading(false), placeholder(false)
+{
+}
+
+OfflineTile::OfflineTile(std::vector<LoadedImage *>loadImages)
+: isLoading(false), placeholder(false)
+{
+}
+
+OfflineTile::~OfflineTile()
+{
+}
+
+void OfflineTile::GetTileSize(int &numX,int &numY)
+{
+    numX = numY = 0;
+    if (images.size() > 0)
+    {
+        numX = images[0]->getWidth();
+        numY = images[0]->getHeight();
+    }
+}
+
+
+QuadTileOfflineLoader::QuadTileOfflineLoader(const std::string &name,QuadTileImageDataSource *imageSource)
+    : name(name), imageSource(imageSource), on(true), numImages(1), sizeX(1024), sizeY(1024), autoRes(true),
+    period(10.0), previewLevels(-1), outputDelegate(NULL), quadControl(NULL), numFetches(0), renderScheduled(false),
+    immediateScheduled(false), lastRender(0), somethingChanged(true), currentMbr(-1)
+{
+    theMbr.addPoint(Point2f(0,0));
+    theMbr.addPoint(Point2f(1.0*M_PI/180.0, 1.0*M_PI/180.0));
+}
+    
+QuadTileOfflineLoader::~QuadTileOfflineLoader()
+{
+    clear();
+}
+
+void QuadTileOfflineLoader::clear()
+{
+    for (OfflineTileSet::iterator it = tiles.begin();it != tiles.end();++it)
+        delete *it;
+    tiles.clear();
+    somethingChanged = true;
+}
+
+    // Note: Porting  Needs to go on Java side
+//void QuadTileOfflineLoader::setPeriod(TimeInterval newPeriod)
+//{
+//    period = newPeriod;
+//    
+//    if (!quadControl)
+//        return;
+//    
+//    if (period > 0.0 && !renderScheduled)
+//    {
+//        renderScheduled = true;
+//        [self performSelector:@selector(imageRenderPeriodic) withObject:nil afterDelay:_period];
+//    }
+//}
+
+void QuadTileOfflineLoader::setMbr(Mbr newMbr)
+{
+    if (!quadControl)
+        return;
+
+    if (newMbr.ll().x() < 0 && newMbr.ur().x() > 0 && (newMbr.ur().x() - newMbr.ll().x() > M_PI))
+    {
+        float tmp = newMbr.ll().x();
+        newMbr.ll().x() = newMbr.ur().x();
+        newMbr.ur().x() = tmp;
+        newMbr.ur().x() += 2*M_PI;
+    }
+
+    // Note: Porting
+//    @synchronized(self)
+//    {
+        theMbr = newMbr;
+        currentMbr++;
+//    }
+    somethingChanged = true;
+    
+    // Note: Porting
+//    if (!immediateScheduled)
+//    {
+//        [self performSelector:@selector(imageRenderImmediate) onThread:_quadLayer.layerThread withObject:nil waitUntilDone:NO];
+//        immediateScheduled = true;
+//    }
+}
+
+// Note: Porting
+//void QuadTileOfflineLoader::imageRenderImmediate()
+//{
+////    NSLog(@"Render:: Immediate");
+//    immediateScheduled = false;
+//    
+//    if (_on)
+//    {
+//        [self imageRenderToLevel:_previewLevels];
+//        if (_previewLevels > 0)
+//        {
+//            lastRender = 0;
+//            if (!renderScheduled)
+//            {
+//                [self performSelector:@selector(imageRenderPeriodic)];
+//                renderScheduled = true;
+//            }
+//        }
+//    }
+//}
+
+    // Note: Porting
+//- (void)imageRenderPeriodic
+//{
+//    renderScheduled = false;
+//
+//    if (_on && somethingChanged)
+//    {
+////        NSLog(@"Render:: Periodic");
+//        CFTimeInterval now = CFAbsoluteTimeGetCurrent();
+//        if (now - lastRender >= _period)
+//            [self imageRenderToLevel:-1];
+//    }
+//    
+//    if (_period > 0.0 && !renderScheduled)
+//    {
+//        renderScheduled = true;
+//        [self performSelector:@selector(imageRenderPeriodic) withObject:nil afterDelay:_period];
+//    }
+//}
+
+Point2d QuadTileOfflineLoader::calculateSize()
+{
+    if (autoRes)
+    {
+        Point2f imageRes(MAXFLOAT,MAXFLOAT);
+        
+        Mbr mbr;
+        // Note: Porting
+//        @synchronized(self)
+//        {
+            mbr = theMbr;
+//        }
+
+        // Note: Assuming geographic or spherical mercator
+        GeoMbr geoMbr(GeoCoord(mbr.ll().x(), mbr.ll().y()),GeoCoord(mbr.ur().x(),mbr.ur().y()));
+        std::vector<Mbr> testMbrs;
+        if (geoMbr.ur().x() > M_PI)
+        {
+            testMbrs.push_back(Mbr(geoMbr.ll(),Point2f((float)M_PI,geoMbr.ur().y())));
+            testMbrs.push_back(Mbr(Point2f((float)(M_PI),geoMbr.ll().y()),geoMbr.ur()));
+        } else {
+            testMbrs.push_back(Mbr(geoMbr.ll(),geoMbr.ur()));
+        }
+
+        // Work our way through the tiles looking for overlaps
+        for (OfflineTileSet::iterator it = tiles.begin(); it != tiles.end(); ++it)
+        {
+            OfflineTile *tile = *it;
+            if (tile->isLoading)
+                continue;
+            // Scale the extents to the output image
+            Mbr tileMbr[2];
+            tileMbr[0] = quadControl->getQuadtree()->generateMbrForNode(tile->ident);
+            bool overlaps = tileMbr[0].overlaps(testMbrs[0]);
+            if (testMbrs.size() > 1 && !overlaps)
+            {
+                tileMbr[1] = tileMbr[0];
+                tileMbr[1].ll().x() += 2*M_PI;
+                tileMbr[1].ur().x() += 2*M_PI;
+                overlaps = tileMbr[1].overlaps(testMbrs[1]);
+            }
+            if (!overlaps)
+                continue;
+            
+            for (unsigned int jj=0;jj<testMbrs.size();jj++)
+            {
+                Mbr &testMbr = testMbrs[jj];
+                if (!tileMbr[jj].overlaps(testMbr))
+                    continue;
+                
+                // Figure out how big a pixel is
+                int numX,numY;
+                tile->GetTileSize(numX,numY);
+                if (numX <= 0 || numY <= 0)
+                    continue;
+                Point2f texSize = tileMbr[0].ur() - tileMbr[0].ll();
+                texSize.x() /= numX;  texSize.y() /= numY;
+                imageRes.x() = std::min(imageRes.x(),texSize.x());
+                imageRes.y() = std::min(imageRes.y(),texSize.y());
+            }
+        }
+        
+        Point2f numPix = mbr.ur()-mbr.ll();
+        numPix.x() /= imageRes.x();  numPix.y() /= imageRes.y();
+        
+        numPix.x() = std::min((float)sizeX,numPix.x());  numPix.y() = std::min((float)sizeY,numPix.y());
+        numPix.x() = std::max(numPix.x(),16.f);  numPix.y() = std::max(numPix.y(),16.f);
+        return Point2d((int)numPix.x(), (int)numPix.y());
+    } else
+        return Point2d(sizeX, sizeY);
+}
+
+void QuadTileOfflineLoader::imageRenderToLevel(int deep)
+{
+    if (outputDelegate)
+    {
+        lastRender = TimeGetCurrent();
+        int whichMbr;
+        
+        Mbr mbr;
+        // Note: Porting
+//        @synchronized(self)
+//        {
+            mbr = theMbr;
+            whichMbr = currentMbr;
+//        }
+
+        // Note: Assuming geographic or spherical mercator
+        GeoMbr geoMbr(GeoCoord(mbr.ll().x(), mbr.ll().y()),GeoCoord(mbr.ur().x(),mbr.ur().y()));
+        std::vector<Mbr> testMbrs;
+        if (geoMbr.ur().x() > M_PI)
+        {
+            testMbrs.push_back(Mbr(geoMbr.ll(),Point2f((float)M_PI,geoMbr.ur().y())));
+            testMbrs.push_back(Mbr(Point2f((float)(M_PI),geoMbr.ll().y()),geoMbr.ur()));
+        } else {
+            testMbrs.push_back(Mbr(geoMbr.ll(),geoMbr.ur()));
+        }
+
+        // Note: Porting
+//        NSMutableArray *images = [NSMutableArray array];
+        
+        Point2d texSize = calculateSize();
+//        NSLog(@"Tex Size = (%f,%f)",texSize.width,texSize.height);
+        
+        // Draw each entry in the image stack individually
+        // Note: Porting
+//        CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+//        CGContextRef theContext = CGBitmapContextCreate(NULL, texSize.width, texSize.height, 8, texSize.width * 4, colorSpace, kCGImageAlphaPremultipliedLast);
+        int numRenderedTiles = 0;
+        for (unsigned int ii=0;ii<numImages;ii++)
+        {
+            // Note: Debugging
+            // Note: Porting
+//            CGContextSetRGBFillColor(theContext, 1, 0, 0, 1);
+//            CGContextFillRect(theContext, CGRectMake(0, 0, texSize.width, texSize.height));
+            // Work through the tiles, drawing as we go
+            for (OfflineTileSet::iterator it = tiles.begin(); it != tiles.end(); ++it)
+            {
+                OfflineTile *tile = *it;
+                if (tile->isLoading)
+                    continue;
+                if (deep > 0 && tile->ident.level > deep)
+                    continue;
+                
+                // Scale the extents to the output image
+                Mbr tileMbr[2];
+                tileMbr[0] = quadControl->getQuadtree()->generateMbrForNode(tile->ident);
+                bool overlaps = tileMbr[0].overlaps(testMbrs[0]);
+                if (testMbrs.size() > 1 && !overlaps)
+                {
+                    tileMbr[1] = tileMbr[0];
+                    tileMbr[1].ll().x() += 2*M_PI;
+                    tileMbr[1].ur().x() += 2*M_PI;
+                    overlaps = tileMbr[1].overlaps(testMbrs[1]);
+                }
+                if (!overlaps)
+                    continue;
+                
+                for (unsigned int jj=0;jj<testMbrs.size();jj++)
+                {
+                    Mbr &testMbr = testMbrs[jj];
+                    if (!tileMbr[jj].overlaps(testMbr))
+                        continue;
+                    
+                    Point2f org;
+                    org.x() = texSize.x() * (tileMbr[jj].ll().x() - mbr.ll().x()) / (mbr.ur().x()-mbr.ll().x());
+                    org.y() = texSize.y() * (tileMbr[jj].ll().y() - mbr.ll().y()) / (mbr.ur().y()-mbr.ll().y());
+                    Point2f span;
+                    span.x() = texSize.x() * (tileMbr[jj].ur().x()-tileMbr[jj].ll().x()) / (mbr.ur().x()-mbr.ll().x());
+                    span.y() = texSize.y() * (tileMbr[jj].ur().y()-tileMbr[jj].ll().y()) / (mbr.ur().y()-mbr.ll().y());
+                    
+                    // Find the right input image
+                    // Note: Porting
+//                    UIImage *imageToDraw = nil;
+                    if (ii < tile->images.size())
+                    {
+                        // Note: Porting
+//                        imageToDraw = (UIImage *)tile->images[ii].imageData;
+//                        if ([imageToDraw isKindOfClass:[NSData class]])
+//                            imageToDraw = [UIImage imageWithData:(NSData *)imageToDraw];
+//                        if (![imageToDraw isKindOfClass:[UIImage class]])
+//                            imageToDraw = nil;
+                    }
+
+                    // Note: Porting
+//                    if (imageToDraw)
+//                        CGContextDrawImage(theContext, CGRectMake(org.x(),org.y(),span.x(),span.y()), imageToDraw.CGImage);
+                }
+                numRenderedTiles++;
+                
+                // If this happens, they've changed the MBR while we were working on this one.  Punt.
+                if (whichMbr != currentMbr)
+                {
+                    // Note: Porting
+//                    CGContextRelease(theContext);
+//                    CGColorSpaceRelease(colorSpace);
+//                    NSLog(@"Aborted render");
+                    return;
+                }
+            }
+            
+            // Note: Porting
+//            CGImageRef imageRef = CGBitmapContextCreateImage(theContext);
+//            UIImage *image = [UIImage imageWithCGImage:imageRef];
+//            CGImageRelease(imageRef);
+//            if (image)
+//                [images addObject:image];
+        }
+        // Note: Porting
+//        CGContextRelease(theContext);
+//        CGColorSpaceRelease(colorSpace);
+        
+//        NSLog(@"Rendered %d tiles of %d",numRenderedTiles,(int)tiles.size());
+        
+        // Convert the images into OpenGL ES textures
+        bool aborted = false;
+        ChangeSet changes;
+        std::vector<WhirlyKit::SimpleIdentity> texIDs;
+        // Note: Porting
+//        for (UIImage *image in images)
+//        {
+//            Texture *tex = new Texture("TileQuadOfflineRenderer",image,true);
+//            texIDs.push_back(tex->getId());
+//            tex->createInGL(_quadLayer.scene->getMemManager());
+//            changes.push_back(new AddTextureReq(tex));
+//            if (whichMbr != currentMbr)
+//            {
+//                aborted = true;
+//                break;
+//            }
+//        }
+        
+        // The texture setup can take a while, so let's be ready to abort here
+        if (aborted)
+        {
+            for (unsigned int ii=0;ii<changes.size();ii++)
+            {
+                AddTextureReq *texReq = (AddTextureReq *)changes[ii];
+                texReq->getTex()->destroyInGL(quadControl->getScene()->getMemManager());
+                delete changes[ii];
+            }
+            changes.clear();
+            return;
+        }
+
+        // Note: Porting
+//        [_quadLayer.layerThread addChangeRequests:changes];
+//        [_quadLayer.layerThread flushChangeRequests];
+        
+        QuadTileOfflineImage *image = new QuadTileOfflineImage();
+        image->textures = texIDs;
+        image->mbr = mbr;
+        image->centerSize = pixelSizeForMbr(mbr,texSize,Point2d(texSize.x()/2.0, texSize.y()/2.0));
+        image->cornerSizes[0] = pixelSizeForMbr(mbr,texSize,Point2d(0.0, 0.0));
+        image->cornerSizes[1] = pixelSizeForMbr(mbr,texSize,Point2d(texSize.x(), 0.0));
+        image->cornerSizes[2] = pixelSizeForMbr(mbr,texSize,Point2d(texSize.x(), texSize.y()));
+        image->cornerSizes[3] = pixelSizeForMbr(mbr,texSize,Point2d(0.0, texSize.y()));
+        outputDelegate->offlineRender(this, image);
+    }
+    
+    // If we did a quick render, we need to go back again
+    if (deep > 0)
+        somethingChanged = true;
+    else
+        somethingChanged = false;
+}
+
+// Calculate the real world size of a given pixel
+Point2d QuadTileOfflineLoader::pixelSizeForMbr(const Mbr &theMbr,const Point2d &texSize,const Point2d &texel)
+{
+    Point2f texelSize((theMbr.ur().x()-theMbr.ll().x())/texSize.x(),(theMbr.ur().y()-theMbr.ll().y())/texSize.y());
+    
+    // Coordinates in the local space
+    Point2f l[3];
+    l[0] = theMbr.ll() + Point2f(texel.x()*texelSize.x(),texel.y()*texelSize.y());
+    l[1] = theMbr.ll() + Point2f((texel.x()+1)*texelSize.x(),texel.y()*texelSize.y());
+    l[2] = theMbr.ll() + Point2f(texel.x()*texelSize.x(),(texel.y()+1)*texelSize.y());
+    
+    // Project the points into display space
+    CoordSystemDisplayAdapter *coordAdapter = quadControl->getScene()->getCoordAdapter();
+    CoordSystem *localCoordSys = coordAdapter->getCoordSystem();
+    CoordSystem *srcCoordSys = quadControl->getCoordSys();
+    Point3d d[3];
+    for (unsigned int ii=0;ii<3;ii++)
+        d[ii] = coordAdapter->localToDisplay(localCoordSys->geocentricToLocal(srcCoordSys->localToGeocentric(Point3d(l[ii].x(),l[ii].y(),0.0))));
+
+    double da = (d[1] - d[0]).norm() * EarthRadius;
+    double db = (d[2] - d[0]).norm() * EarthRadius;
+    
+    return Point2d(da, db);
+}
+
+#pragma mark - WhirlyKitQuadLoader
+
+void QuadTileOfflineLoader::shutdownLayer(ChangeSet &changes)
+{
+    clear();
+}
+
+bool QuadTileOfflineLoader::isReady()
+{
+    return (numFetches <= imageSource->maxSimultaneousFetches());
+}
+
+// Note: Porting
+//- (void)setQuadLayer:(WhirlyKitQuadDisplayLayer *)layer
+//{
+//    _quadLayer = layer;
+//    
+//    if (_period > 0.0 && !renderScheduled) {
+//        renderScheduled = true;
+//        [self performSelector:@selector(imageRenderPeriodic) withObject:nil afterDelay:_period];
+//    }
+//}
+
+void QuadTileOfflineLoader::loadTile(const Quadtree::NodeInfo &tileInfo,int frame)
+{
+    OfflineTile *newTile = new OfflineTile(tileInfo.ident);
+    newTile->isLoading = true;
+    
+    tiles.insert(newTile);
+    
+    // Note: Porting
+//    [_imageSource quadTileLoader:self startFetchForLevel:tileInfo.ident.level col:tileInfo.ident.x row:tileInfo.ident.y attrs:tileInfo.attrs];
+    numFetches++;
+    somethingChanged = true;
+}
+
+OfflineTile *QuadTileOfflineLoader::getTile(const WhirlyKit::Quadtree::Identifier &ident)
+{
+    OfflineTile dummyTile(ident);
+    OfflineTileSet::iterator it = tiles.find(&dummyTile);
+    if (it == tiles.end())
+        return NULL;
+    
+    return *it;
+}
+
+void QuadTileOfflineLoader::unloadTile(const Quadtree::NodeInfo &tileInfo)
+{
+    OfflineTile dummyTile(tileInfo.ident);
+    OfflineTileSet::iterator it = tiles.find(&dummyTile);
+    if (it != tiles.end())
+    {
+        OfflineTile *theTile = *it;
+        delete theTile;
+        tiles.erase(it);
+    }
+    somethingChanged = true;
+}
+
+bool QuadTileOfflineLoader::canLoadChildrenOfTile(const Quadtree::NodeInfo &tileInfo)
+{
+    OfflineTile *tile = getTile(tileInfo.ident);
+    if (!tile)
+        return false;
+    
+    return !tile->isLoading;
+}
+
+void QuadTileOfflineLoader::loadedImage(QuadTileImageDataSource *dataSource,LoadedImage *loadImage,int level,int col,int row,int frame,ChangeSet &changes)
+{
+    numFetches--;
+    Quadtree::Identifier tileIdent(col,row,level);
+    OfflineTile *tile = getTile(tileIdent);
+    if (!tile)
+        return;
+
+    tile->isLoading = false;
+
+    // Assemble the images
+    std::vector<LoadedImage *> loadImages;
+    loadImages.push_back(loadImage);
+    // Note: Porting
+//    if (_numImages != loadImages.size())
+//    {
+//        if (loadTile)
+//            NSLog(@"TileQuadLoader: Got %ld images in callback, but was expecting %d.  Punting tile.",loadImages.size(),_numImages);
+//        [_quadLayer loader:self tileDidNotLoad:tileIdent];
+//        return;
+//    }
+    tile->images = loadImages;
+
+//    NSLog(@"Loaded tile %d: (%d,%d)",level,col,row);
+    quadControl->tileDidLoad(tileIdent, frame);
+    somethingChanged = true;
+}
+
+}
