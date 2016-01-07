b5715bbdc49e4e3244a42cea78ab5a44347dbb86
Steve Gifford <sjg@mousebirdconsulting.com>
2016-01-06 16:04:42 -0800

    Dealing with straight and turn-back cases.
    Former-commit-id: b2787d905c81daed1624cc202e79708d251fe990

--- WhirlyGlobeSrc/WhirlyGlobeComponentTester/WhirlyGlobeComponentTester/TestViewController.m
+++ WhirlyGlobeSrc/WhirlyGlobeComponentTester/WhirlyGlobeComponentTester/TestViewController.m
@@ -482,6 +482,8 @@ static const int BaseEarthPriority = kMaplyImageLayerDrawPriorityDefault;
     [self addGeoJson:@"spiral.geojson"];
     [self addGeoJson:@"square.geojson"];
     [self addGeoJson:@"track.geojson"];
+//    [self addGeoJson:@"straight.geojson"];
+//    [self addGeoJson:@"uturn.geojson"];
 
     if (mapViewC)
         [mapViewC setPosition:MaplyCoordinateMakeWithDegrees(-100, 30) height:0.0046618999913334846];
@@ -1071,39 +1073,40 @@ static const int BaseEarthPriority = kMaplyImageLayerDrawPriorityDefault;
 {
     UIColor *color = [UIColor blueColor];
     float fade = 0.25;
-    MaplyComponentObject *lines = [baseViewC addVectors:@[vecObj] desc:@{kMaplyColor: color,
-                                                                         kMaplyVecWidth: @(4.0),
-                                                                         kMaplyFade: @(fade),
-                                                                         kMaplyVecCentered: @(true),
-                                                                         kMaplyMaxVis: @(10.0),
-                                                                         kMaplyMinVis: @(0.00032424763776361942)
-                                                                         }];
+//    MaplyComponentObject *lines = [baseViewC addVectors:@[vecObj] desc:@{kMaplyColor: color,
+//                                                                         kMaplyVecWidth: @(4.0),
+//                                                                         kMaplyFade: @(fade),
+//                                                                         kMaplyVecCentered: @(true),
+//                                                                         kMaplyMaxVis: @(10.0),
+//                                                                         kMaplyMinVis: @(0.00032424763776361942)
+//                                                                         }];
     
     MaplyComponentObject *screenLines = [baseViewC addWideVectors:@[vecObj] desc:@{kMaplyColor: [UIColor colorWithRed:0.5 green:0.0 blue:0.0 alpha:0.5],
                                                                                    kMaplyFade: @(fade),
                                                                                    kMaplyVecWidth: @(3.0),
-                                                                                   kMaplyVecTexture: filledLineTex,
+//                                                                                   kMaplyVecTexture: filledLineTex,
                                                                                    kMaplyWideVecCoordType: kMaplyWideVecCoordTypeScreen,
-                                                                                   kMaplyWideVecJoinType: kMaplyWideVecMiterJoin,
-                                                                                   kMaplyWideVecMiterLimit: @(1.01),
-                                                                                   kMaplyWideVecTexRepeatLen: @(8),
-                                                                                   kMaplyMaxVis: @(0.00032424763776361942),
-                                                                                   kMaplyMinVis: @(0.00011049506429117173)
+//                                                                                   kMaplyWideVecJoinType: kMaplyWideVecMiterJoin,
+//                                                                                   kMaplyWideVecMiterLimit: @(1.01),
+//                                                                                   kMaplyWideVecTexRepeatLen: @(8),
+//                                                                                   kMaplyMaxVis: @(0.00032424763776361942),
+//                                                                                   kMaplyMinVis: @(0.00011049506429117173)
                                                                                    }];
     
-    MaplyComponentObject *realLines = [baseViewC addWideVectors:@[vecObj] desc:@{kMaplyColor: color,
-                                                                                 kMaplyFade: @(fade),
-                                                                                 kMaplyVecTexture: dashedLineTex,
-                                                                                 // 8m in display coordinates
-                                                                                 kMaplyVecWidth: @(10.0/6371000),
-                                                                                 kMaplyWideVecCoordType: kMaplyWideVecCoordTypeReal,
-                                                                                 kMaplyWideVecJoinType: kMaplyWideVecMiterJoin,
-                                                                                 kMaplyWideVecMiterLimit: @(1.01),
-                                                                                 // Repeat every 10m
-                                                                                 kMaplyWideVecTexRepeatLen: @(10/6371000.f),
-                                                                                 kMaplyMaxVis: @(0.00011049506429117173),
-                                                                                 kMaplyMinVis: @(0.0)
-                                                                                 }];
+    // Note: Put this back
+//    MaplyComponentObject *realLines = [baseViewC addWideVectors:@[vecObj] desc:@{kMaplyColor: color,
+//                                                                                 kMaplyFade: @(fade),
+//                                                                                 kMaplyVecTexture: dashedLineTex,
+//                                                                                 // 8m in display coordinates
+//                                                                                 kMaplyVecWidth: @(10.0/6371000),
+//                                                                                 kMaplyWideVecCoordType: kMaplyWideVecCoordTypeReal,
+//                                                                                 kMaplyWideVecJoinType: kMaplyWideVecMiterJoin,
+//                                                                                 kMaplyWideVecMiterLimit: @(1.01),
+//                                                                                 // Repeat every 10m
+//                                                                                 kMaplyWideVecTexRepeatLen: @(10/6371000.f),
+//                                                                                 kMaplyMaxVis: @(0.00011049506429117173),
+//                                                                                 kMaplyMinVis: @(0.0)
+//                                                                                 }];
     
     // Look for some labels
     MaplyComponentObject *labelObj = nil;
@@ -1138,7 +1141,8 @@ static const int BaseEarthPriority = kMaplyImageLayerDrawPriorityDefault;
                   kMaplyDrawPriority: @(200)
                   }];
     
-    return @[lines,screenLines,realLines,labelObj];
+//    return @[lines,screenLines,realLines,labelObj];
+    return @[screenLines,labelObj];
 }
 
 - (void)addShapeFile:(NSString *)shapeFileName
@@ -1217,7 +1221,7 @@ static const int BaseEarthPriority = kMaplyImageLayerDrawPriorityDefault;
                                          kMaplyVecTexture: lineTexture,
                                          kMaplyWideVecJoinType: kMaplyWideVecMiterJoin,
                                          kMaplyWideVecCoordType: kMaplyWideVecCoordTypeScreen,
-                                         kMaplyVecWidth: @(40)}
+                                         kMaplyVecWidth: @(6)}
                                  mode:MaplyThreadCurrent];
             [baseViewC addVectors:@[vecObj]
                              desc: @{kMaplyColor: [UIColor blackColor],
