0e442cb4f3bb783e31be20fe3a4c40024d7466f9
Steve Gifford <sjg@mousebirdconsulting.com>
2016-01-07 09:41:46 -0800

    Letter case fix from TimSylvester.
    Former-commit-id: c4bda830f0f6efa26a6f989f1b251f90bdf9b7b6

--- WhirlyGlobeSrc/WhirlyGlobeLib/include/ShapeDrawableBuilder.h
+++ WhirlyGlobeSrc/WhirlyGlobeLib/include/ShapeDrawableBuilder.h
@@ -21,7 +21,7 @@
 #import "Identifiable.h"
 #import "WhirlyVector.h"
 #import "DataLayer.h"
-#import "layerThread.h"
+#import "LayerThread.h"
 #import "BaseInfo.h"
 
 /// Used to pass shape info between the shape layer and the drawable builder
