d8cd4190056e87af28a6287c306cf1d5c29f88fb
Steve Gifford <sjg@mousebirdconsulting.com>
2016-01-11 21:56:27 -0800

    Scaled down the generated texture width.
    Former-commit-id: f5e9c5bc55d00df09f96fae3d87d4be1004bbc00

--- WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/src/MaplyTextureBuilder.mm
+++ WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/src/MaplyTextureBuilder.mm
@@ -59,7 +59,8 @@
     if (size == 0)
         return nil;
     
-    int width = 16;
+    // Note: If OpenGL ES doesn't like this size, just bump it up
+    int width = 1;
     CGSize imgSize = CGSizeMake(width, size);
     UIGraphicsBeginImageContext(imgSize);
     CGContextRef ctx = UIGraphicsGetCurrentContext();
