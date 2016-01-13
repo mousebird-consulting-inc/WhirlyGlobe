185e3768593aaa41a10966e68dd8515db12f6290
Steve Gifford <sjg@mousebirdconsulting.com>
2016-01-13 15:19:06 -0800

    Updating tester app

--- WhirlyGlobeSrc/MaplyAndroidTester/buildmaply.sh
+++ WhirlyGlobeSrc/MaplyAndroidTester/buildmaply.sh
@@ -4,15 +4,29 @@ rm app/libs/Maply.aar
 if [ -n "$1" ]; then
     vers=${1}
 else
-    vers="debug"
+    vers="Release"
 fi
 
 # You can use several architectures here, like x86,mips64
 if [ -n "$2" ]; then
     arch=${2}
 else
-    arch="x86,x86_64,armeabi,armeabi-v7a,arm64-v8a,mips"
+    arch="x86,armeabi,armeabi-v7a"
 fi
+
+rm -rf app/src/main/jniLibs/
+mkdir app/src/main/jniLibs
+
+oldIFS=$IFS
+IFS=","
+if [ "$vers" = "Debug" ]
+then
+    for v in $arch
+        do
+            cp -r gdb_jniLibs/$v app/src/main/jniLibs
+        done
+fi
+IFS=$oldIFS
 
 pushd ../Android/
 ./gradlew assemble$vers -Parchitecture=$arch
--- WhirlyGlobeSrc/MaplyAndroidTester/rundebug_note4.sh
+++ WhirlyGlobeSrc/MaplyAndroidTester/rundebug_note4.sh
@@ -1,8 +1,8 @@
 # Source this script instead of executing it
 
 export device=android_note4_bin
-export arch=armeabi
-export app=maplyandroidtester
+export arch=armeabi-v7a
+export app=darkskytest
 export adb=/Users/sjg/Android_sdk/platform-tools//adb
 
 # Kill off the old adb
@@ -13,11 +13,9 @@ export pid=`$adb shell ps | grep $app | grep -v gdbserver | awk '{print $2}'`
 echo "Attaching to process $pid"
 
 $adb forward tcp:5039 localfilesystem:/data/data/com.mousebirdconsulting.$app/debug-pipe
+rm nohup.out
 nohup $adb shell run-as com.mousebirdconsulting.$app /data/data/com.mousebirdconsulting.$app/lib/gdbserver.so +debug-pipe --attach $pid &
 sleep 1
 
-echo "***Run the following***"
-echo "~/Android/android-ndk-r10e/toolchains/x86-4.9/prebuilt/darwin-x86_64/bin/i686-linux-android-gdb ~/$device/app_process -i=mi -ex \"target remote :5039;\" -ex \"set solib-search-path ~/$device/:~/$device/system_lib:~/$device/vendor_lib:~/$device/vendor_lib/egl:~/iPhone/WhirlyGlobe-Maply3/WhirlyGlobeSrc/Android/obj/local/$arch/\""
+echo "~/Android/android-ndk-r10e/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-gdb  ~/$device/app_process -i=mi -ex \"target remote :5039;\" -ex \"set solib-search-path ~/$device/:~/$device/system_lib:~/$device/vendor_lib:~/$device/vendor_lib/egl:~/iPhone/DarkSkyAndroid/libs/WhirlyGlobe-Maply/WhirlyGlobeSrc/Android/obj/local/$arch/\""
 
-# Kill off the old adb
-# kill -9 `ps aux | grep adb | grep $app | awk '{print $2}'`
