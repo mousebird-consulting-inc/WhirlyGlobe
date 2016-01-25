2aa1d73ef1b84a0e8bc90c5639feb65a39ea7e94
Steve Gifford <sjg@mousebirdconsulting.com>
2016-01-25 10:49:55 -0800

    Cleaning up AutoTester build a bit.

---
+++ WhirlyGlobeSrc/AutoTesterAndroid/buildmaply.sh
@@ -0,0 +1,36 @@
+rm app/libs/Maply.aar
+
+#  Debug vs. Release
+if [ -n "$1" ]; then
+    vers=${1}
+else
+    vers="Release"
+fi
+
+# You can use several architectures here, like x86,mips64
+if [ -n "$2" ]; then
+    arch=${2}
+else
+    arch="x86,armeabi,armeabi-v7a"
+fi
+
+rm -rf app/src/main/jniLibs/
+mkdir app/src/main/jniLibs
+
+oldIFS=$IFS
+IFS=","
+if [ "$vers" = "Debug" ]
+then
+    for v in $arch
+        do
+            cp -r gdb_jniLibs/$v app/src/main/jniLibs
+        done
+fi
+IFS=$oldIFS
+
+pushd ../Android/
+./gradlew assemble$vers -Parchitecture=$arch
+popd
+
+# Note: This is the debug version
+cp ../Android/build/outputs/aar/Android-$vers.aar Maply/Maply.aar
