780ab25106ba1beae74fd26eca4c1661c43968a9
Steve Gifford <sjg@mousebirdconsulting.com>
2016-01-29 12:32:18 -0800

    More complex test case for model builder implemented.  Not yet tested.
    Former-commit-id: 96d65bcefc161f0235c25a8d13cc5dcd88849b9e

--- WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/include/MaplyCoordinate.h
+++ WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/include/MaplyCoordinate.h
@@ -137,6 +137,15 @@ MaplyCoordinate MaplyCoordinateMakeWithDegrees(float degLon,float degLat);
   */
 MaplyCoordinate3d MaplyCoordinate3dMake(float x,float y,float z);
 
+/** @brief Construct a MaplyCoordinat3d from the values given.
+    @param x The x value, or longitude in radians if we're making geo coordinates.
+    @param y The y value, or latitude in radians if we're making geo coordinates.
+    @param z The z value, sometimes this is display coordinates (radius == 1.0 for a sphere)
+ and sometimes this is meters.  It depends on how you're using it.
+    @return A 3D MaplyCoordinate3d in radians + other (if representing a lon/lat value).
+ */
+MaplyCoordinate3dD MaplyCoordinate3dDMake(double x,double y,double z);
+
 /** @brief Construct a MaplyBoundingBox from the values given.
     @details The inputs are in degrees and the order is longitude *then* latitude.
     @param degLon0 The left side of the bounding box in degrees.
