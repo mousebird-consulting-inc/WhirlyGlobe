fdeca37fa2203e183c7636613a7fc1d2c10e2874
JM <jmnavarro@gmail.com>
2016-02-03 23:37:05 +0100

    Particle system test
    Former-commit-id: da59e56e2b735c4d1941e2b8f6fd6c46bbc47f94

---
+++ WhirlyGlobeSrc/AutoTester/AutoTester/testCases/ParticleTestCase.swift
@@ -0,0 +1,34 @@
+//
+//  ParticleTestCase.swift
+//  AutoTester
+//
+//  Created by jmnavarro on 18/1/16.
+//  Copyright © 2016 mousebird consulting. All rights reserved.
+//
+import UIKit
+
+class ParticleTestCase: MaplyTestCase {
+	
+	override init() {
+		super.init()
+		
+		self.name = "Wind Particle Test"
+		self.captureDelay = 5
+	}
+	
+	private func setUpOverlay (baseView: MaplyBaseViewController) {
+		let partDelegate = ParticleTileDelegate(URL: "http://tilesets.s3-website-us-east-1.amazonaws.com/wind_test/{dir}_tiles/{z}/{x}/{y}.png", minZoom: Int32(2), maxZoom: Int32(5), viewC: baseView)
+		let layer = MaplyQuadPagingLayer(coordSystem: partDelegate.coordSys, delegate: partDelegate)
+		layer?.flipY = false;
+		baseView.addLayer(layer!)
+	}
+	
+	override func setUpWithGlobe(globeVC: WhirlyGlobeViewController) -> Bool {
+		let baseLayer = GeographyClassTestCase()
+		baseLayer.setUpWithGlobe(globeVC)
+		setUpOverlay(globeVC)
+		globeVC.animateToPosition(MaplyCoordinateMakeWithDegrees(-122.4192, 37.7793) , time: 1.0)
+		return true
+	}
+
+}
