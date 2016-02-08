1a03cd88c45a2bf990a7ec33c2d0064486f201b6
Steve Gifford <sjg@mousebirdconsulting.com>
2016-02-08 11:42:45 -0800

    Script for building the library and copying it into place.

---
+++ WhirlyGlobeSrc/AutoTesterAndroid/buildmaply.sh
@@ -0,0 +1,38 @@
+rm Maply/Maply.aar
+
+#  Debug vs. Release
+if [ -n "$1" ]; then
+    vers=${1}
+else
+    vers="Release"
+fi
+
+# You can use several architectures here, like x86,mips64
+if [ -n "$2" ]; then
+    arch=${2}
+else
+    arch="x86,armeabi,armeabi-v7a"
+fi
+
+rm -rf app/src/main/jniLibs/
+mkdir app/src/main/jniLibs
+
+oldIFS=$IFS
+IFS=","
+if [ "$vers" = "Debug" ]
+then
+    for v in $arch
+        do
+            cp -r gdb_jniLibs/$v app/src/main/jniLibs
+        done
+fi
+IFS=$oldIFS
+
+WGTREE=~/iPhone/WhirlyGlobe-Maply3/WhirlyGlobeSrc/Android
+
+pushd $WGTREE
+./gradlew assemble$vers -Parchitecture=$arch
+popd
+
+# Note: This is the debug version
+cp $WGTREE/build/outputs/aar/Android-$vers.aar Maply/Maply.aar
