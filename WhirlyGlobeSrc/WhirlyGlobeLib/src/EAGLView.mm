8710c3731e8dc7a54d2b04880cc8f39d9517a907
Steve Gifford <sjg@mousebirdconsulting.com>
2016-02-09 13:00:51 -0800

    Pausing the display link again when doing a stopAnimation. Option is still there to keep it running if you need to process things in the background.

--- WhirlyGlobeSrc/WhirlyGlobeLib/src/EAGLView.mm
+++ WhirlyGlobeSrc/WhirlyGlobeLib/src/EAGLView.mm
@@ -55,7 +55,8 @@
         _frameInterval = 1;
         self.useRetina = TRUE;
         resizeFail = false;
-        resizeFailRetry = 0;        
+        resizeFailRetry = 0;
+        _pauseDisplayLink = true;
     }
     
     return self;
@@ -108,7 +109,7 @@
                 [displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];            
         } else
             displayLink.paused = NO;
-        
+
         _animating = TRUE;
     }
 }
@@ -118,6 +119,8 @@
     if (_animating)
     {
         _animating = FALSE;
+        if (_pauseDisplayLink)
+            displayLink.paused = true;
     }
 }
 
