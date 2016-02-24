dedfa259545ecd614ff6822bc638f8f6bc935f9b
Ranen Ghosh <ranen@ranenghosh.com>
2016-02-23 21:38:06 -0500

    remove afnetworking; forgot to remove one import
    Former-commit-id: 576f27ae064df1e48e11c6f645e5cb588317e6c0

--- WhirlyGlobeSrc/AutoTester/AutoTester/MaplyTesterBridge.h
+++ WhirlyGlobeSrc/AutoTester/AutoTester/MaplyTesterBridge.h
@@ -14,7 +14,6 @@
 #import "MaplyTestCase.h"
 #import "MaplyTestResult.h"
 #import "GeographyClassTestCase.h"
-#import "AFNetworking.h"
 #import "CesiumElevationTestCase.h"
 #import "MapBoxVectorTestCase.h"
 #import "MaplyMapnikVectorTiles.h"
