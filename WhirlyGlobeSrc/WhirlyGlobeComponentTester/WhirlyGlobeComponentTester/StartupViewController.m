7d1bcc77759f5cdb3c8c91e6da421f84f3987c95
Steve Gifford <sjg@mousebirdconsulting.com>
2016-03-01 16:50:49 -0800

    Cleaned up ComponentTester for imminent release.
    Former-commit-id: 866513ce701114d11740ef4eaef689ae1397e726

--- WhirlyGlobeSrc/WhirlyGlobeComponentTester/WhirlyGlobeComponentTester/StartupViewController.m
+++ WhirlyGlobeSrc/WhirlyGlobeComponentTester/WhirlyGlobeComponentTester/StartupViewController.m
@@ -104,12 +104,6 @@
         case Maply2DMap:
             cell.textLabel.text = @"Map (2D)";
             break;
-        case Maply3DMap:
-            cell.textLabel.text = @"Map (3D)";
-            break;
-//        case MaplyScrollViewMap:
-//            cell.textLabel.text = @"Map - UIScrollView (2D)";
-//            break;
         default:
             break;
     }
