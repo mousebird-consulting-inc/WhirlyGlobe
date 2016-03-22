ac58c52daada712dcc02010340ff826e74f7c6cc
JM <jmnavarro@gmail.com>
2016-03-23 00:27:38 +0100

    Global replace: update copyright

--- WhirlyGlobeSrc/Android/jni/ParticleSystem_jni.cpp
+++ WhirlyGlobeSrc/Android/jni/ParticleSystem_jni.cpp
@@ -3,7 +3,7 @@
  *  WhirlyGlobeLib
  *
  *  Created by jmnavarro on 23/1/16.
- *  Copyright 2011-2015 mousebird consulting
+ *  Copyright 2011-2016 mousebird consulting
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
