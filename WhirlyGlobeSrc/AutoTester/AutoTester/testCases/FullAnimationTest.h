34f32df3a9d1db3c0d55baed148a22fc757a8bff
Ranen Ghosh <ranen@ranenghosh.com>
2016-04-08 00:26:38 -0400

    autotest cases; test #527 #528 #529 #530
    Former-commit-id: 9a120f4bbe101fb9e494e2d809c0941fb1aae427

---
+++ WhirlyGlobeSrc/AutoTester/AutoTester/testCases/FullAnimationTest.h
@@ -0,0 +1,15 @@
+//
+//  FullAnimationTest
+//  AutoTester
+//
+//  Created by Ranen Ghosh on 4/7/16.
+//  Copyright © 2016 mousebird consulting. All rights reserved.
+//
+
+#import "MaplyTestCase.h"
+
+@interface FullAnimationTest : MaplyTestCase
+
+
+@end
+
