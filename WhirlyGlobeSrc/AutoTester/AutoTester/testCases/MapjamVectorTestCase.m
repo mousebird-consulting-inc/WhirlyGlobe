affa4a741e973137786f5010556cf21c1a335e3c
Steve Gifford <sjg@mousebirdconsulting.com>
2016-04-26 17:24:30 -0700

    Added Mapjam test case.
    Former-commit-id: 694db6c3439466655ec6cba933942408e4e7e4b1

---
+++ WhirlyGlobeSrc/AutoTester/AutoTester/testCases/MapjamVectorTestCase.m
@@ -0,0 +1,13 @@
+//
+//  MapjamVectorTestCase.m
+//  AutoTester
+//
+//  Created by Steve Gifford on 4/18/16.
+//  Copyright © 2016 mousebird consulting. All rights reserved.
+//
+
+#import "MapjamVectorTestCase.h"
+
+@implementation MapjamVectorTestCase
+
+@end
