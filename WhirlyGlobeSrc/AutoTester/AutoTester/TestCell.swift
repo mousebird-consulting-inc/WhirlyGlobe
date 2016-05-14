569124ad35549e2447456ab5fed9a210b01e8e4a
JM <jmnavarro@gmail.com>
2016-05-15 00:59:06 +0200

    Remote resources download completed
    Former-commit-id: 212941c73302ca6e64366b10f50605e2f26c0891

--- WhirlyGlobeSrc/AutoTester/AutoTester/TestCell.swift
+++ WhirlyGlobeSrc/AutoTester/AutoTester/TestCell.swift
@@ -13,11 +13,59 @@ class TestCell: UITableViewCell {
 	@IBOutlet weak var testName: UILabel!
 	@IBOutlet weak var mapButton: UIButton!
 	@IBOutlet weak var globeButton: UIButton!
-    @IBOutlet weak var downloadProgress: UIProgressView!
 
+	@IBOutlet weak var retryButton: UIButton!
+	@IBOutlet weak var downloadIndicator: UIActivityIndicatorView!
 	var globeTestExecution: (() -> ())?
 	var mapTestExecution: (() -> ())?
+	var retryDownloadResources: (() -> ())?
 	var rowPosition : Int?
+
+	var state: MaplyTestCaseState  = .Error {
+		didSet {
+			switch state {
+			case MaplyTestCaseState.Downloading:
+				downloadIndicator.hidden = false;
+				downloadIndicator.startAnimating()
+				globeButton.hidden = true
+				mapButton.hidden = true
+				retryButton.hidden = true;
+				accessoryType = .None
+				break
+			case MaplyTestCaseState.Error:
+				downloadIndicator.hidden = true;
+				downloadIndicator.stopAnimating()
+				globeButton.hidden = true
+				retryButton.hidden = false;
+				mapButton.hidden = true
+				accessoryType = .None
+				backgroundColor = UIColor.redColor()
+				break
+			default:
+				downloadIndicator.stopAnimating()
+				downloadIndicator.hidden = true;
+				retryButton.hidden = true;
+				backgroundColor = UIColor.whiteColor()
+			}
+		}
+	}
+
+	var interactive: Bool = false {
+		didSet {
+			globeButton.hidden = !interactive
+			mapButton.hidden = !interactive;
+			accessoryType = .None
+
+			switch state {
+			case MaplyTestCaseState.Running:
+				accessoryType = .DisclosureIndicator
+			case MaplyTestCaseState.Selected:
+				accessoryType = .Checkmark
+			default:
+				accessoryType = .None
+			}
+		}
+	}
 
 	required init?(coder aDecoder: NSCoder) {
 		super.init(coder: aDecoder)
@@ -29,6 +77,10 @@ class TestCell: UITableViewCell {
 	
 	@IBAction func runMapInteractiveTest(sender: AnyObject) {
 		mapTestExecution?()
+	}
+
+	@IBAction func retryAction(sender: AnyObject) {
+		retryDownloadResources?()
 	}
 
 }
