a22c8549a6acec22c57d9e977af85fb8f806b000
Steve Gifford <sjg@mousebirdconsulting.com>
2016-05-31 15:41:47 -0700

    Background polygons working for screen labels. Updated screen space shader to use color when texture is missing.

--- WhirlyGlobeSrc/WhirlyGlobeLib/src/ScreenSpaceDrawable.cpp
+++ WhirlyGlobeSrc/WhirlyGlobeLib/src/ScreenSpaceDrawable.cpp
@@ -265,17 +265,18 @@ static const char *vertexShader2dMotionTri =
 ;
     
 static const char *fragmentShaderTri =
-"precision lowp float;"
-""
-"uniform sampler2D s_baseMap0;"
-""
-"varying vec2      v_texCoord;"
-"varying vec4      v_color;"
-""
-"void main()"
-"{"
-"  vec4 baseColor = texture2D(s_baseMap0, v_texCoord);"
-"  gl_FragColor = v_color * baseColor;"
+"precision lowp float;\n"
+"\n"
+"uniform sampler2D s_baseMap0;\n"
+"uniform bool  u_hasTexture;\n"
+"\n"
+"varying vec2      v_texCoord;\n"
+"varying vec4      v_color;\n"
+"\n"
+"void main()\n"
+"{\n"
+"  vec4 baseColor = u_hasTexture ? texture2D(s_baseMap0, v_texCoord) : vec4(1.0,1.0,1.0,1.0); \n"
+"  gl_FragColor = v_color * baseColor;\n"
 "}"
 ;
 
