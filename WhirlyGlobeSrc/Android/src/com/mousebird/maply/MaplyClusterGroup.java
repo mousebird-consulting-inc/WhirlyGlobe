8254147576eda39f2addb378f336a9549d3f1189
JM <jmnavarro@gmail.com>
2016-06-13 22:19:06 +0200

    Add Android API for clustering support

---
+++ WhirlyGlobeSrc/Android/src/com/mousebird/maply/MaplyClusterGroup.java
@@ -0,0 +1,49 @@
+/*
+ *  MaplyClusterGroup.java
+ *  WhirlyGlobeLib
+ *
+ *  Created by jmnavarro
+ *  Copyright 2011-2014 mousebird consulting
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package com.mousebird.maply;
+
+import android.graphics.Bitmap;
+
+
+/**
+ * Visual representation for a group of markers.
+ */
+public class MaplyClusterGroup {
+
+    /**
+     * The image to use for the group
+     */
+    public Bitmap image;
+
+    /**
+     * Screen size to use for the resulting marker
+     */
+    public Point2d size = new Point2d();
+
+    public MaplyClusterGroup(Bitmap image, Point2d size) {
+        this.image = image;
+        this.size = size;
+    }
+
+    public MaplyClusterGroup() {
+        this.image = null;
+        this.size = new Point2d();
+    }
+}
