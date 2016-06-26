3368dce0b3bae7fb9a89a692bae8e3a686502c61
Steve Gifford <sjg@mousebirdconsulting.com>
2016-06-25 17:16:51 -0700

    The VectorDatabase setup was confused by an attribute that happens to be a SQL keyword.
    Former-commit-id: c422a870f3ffc9528d883ac9a5d0e460c8653f1d

---
+++ WhirlyGlobeSrc/AutoTester/AutoTester/testCases/ShapefileTestCase.m
@@ -0,0 +1,83 @@
+//
+//  ShapefileTestCase.m
+//  AutoTester
+//
+//  Created by Steve Gifford on 6/25/16.
+//  Copyright © 2016 mousebird consulting. All rights reserved.
+//
+
+#import "ShapefileTestCase.h"
+#import "GeographyClassTestCase.h"
+
+@implementation ShapefileTestCase
+{
+    MaplyBaseViewController *baseViewC;
+    MaplyTestCase *baseCase;
+    MaplyComponentObject *compObj;
+}
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        self.name = @"Shapefile";
+        self.captureDelay = 5;
+        self.implementations = MaplyTestCaseImplementationMap | MaplyTestCaseImplementationGlobe;
+        
+    }
+    
+    return self;
+}
+
+- (void)overlayShapefile
+{
+    NSString *fileName = [[NSBundle mainBundle] pathForResource:@"ne_10m_roads" ofType:@"shp"];
+    MaplyVectorDatabase *vecDb = [[MaplyVectorDatabase alloc] initWithShape:@"ne_10m_roads"];
+    MaplyVectorObject *vecObj = [vecDb fetchAllVectors];
+    
+    compObj = [baseViewC addVectors:@[vecObj] desc:@{kMaplyColor: [UIColor whiteColor]}];
+}
+
+- (void)setUpWithGlobe:(WhirlyGlobeViewController *)globeVC
+{
+    baseViewC = globeVC;
+    baseCase = [[GeographyClassTestCase alloc]init];
+    [baseCase setUpWithGlobe:globeVC];
+    
+    [self overlayShapefile];
+}
+
+
+- (void)setUpWithMap:(MaplyViewController *)mapVC
+{
+    baseViewC = mapVC;
+    baseCase = [[GeographyClassTestCase alloc]init];
+    [baseCase setUpWithMap:mapVC];
+
+    [self overlayShapefile];
+}
+
+- (void) tearDownWithMap:(MaplyViewController *)mapVC {
+    [baseViewC removeObject:compObj];
+}
+
+- (void)tearDownWithGlobe:(WhirlyGlobeViewController *)globeVC{
+    [baseViewC removeObject:compObj];
+}
+- (void) handleSelection:(MaplyBaseViewController *)viewC
+                selected:(NSObject *)selectedObj
+{
+    // ensure it's a MaplyVectorObject. It should be one of our outlines.
+    if ([selectedObj isKindOfClass:[MaplyVectorObject class]]) {
+        MaplyVectorObject *theVector = (MaplyVectorObject *)selectedObj;
+        MaplyCoordinate location;
+        
+        if ([theVector centroid:&location]) {
+            MaplyAnnotation *annotate = [[MaplyAnnotation alloc]init];
+            annotate.title = @"Selected";
+            annotate.subTitle = (NSString *)theVector.userObject;
+            [viewC addAnnotation:annotate forPoint:location offset:CGPointZero];
+        }
+    }
+}
+
+@end
