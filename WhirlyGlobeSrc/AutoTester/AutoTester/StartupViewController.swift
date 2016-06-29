4a6c087c6cccaa61cd7f102391231687ad3cf159
Steve Gifford <sjg@mousebirdconsulting.com>
2016-06-28 17:21:18 -0700

    Added paging layer test case.

--- WhirlyGlobeSrc/AutoTester/AutoTester/StartupViewController.swift
+++ WhirlyGlobeSrc/AutoTester/AutoTester/StartupViewController.swift
@@ -29,6 +29,7 @@ class StartupViewController: UITableViewController, UIPopoverControllerDelegate 
 		MarkersTestCase(),
 		StickersTestCase(),
 
+		PagingLayerTestCase(),
 		MapzenVectorTestCase(),
 		MapjamVectorTestCase(),
 		VectorMBTilesTestCase(),
