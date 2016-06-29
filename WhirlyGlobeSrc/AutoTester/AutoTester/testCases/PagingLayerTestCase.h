efe96aa6da33780e3ec6aa381e7ce4ad4f9ae068
Steve Gifford <sjg@mousebirdconsulting.com>
2016-06-28 17:21:18 -0700

    Added paging layer test case.
    Former-commit-id: e54d536a2b86db36f4d50f931d733cd855a5492a

---
+++ WhirlyGlobeSrc/AutoTester/AutoTester/testCases/PagingLayerTestCase.h
@@ -0,0 +1,13 @@
+//
+//  PagingLayerTestCase.h
+//  AutoTester
+//
+//  Created by Steve Gifford on 6/28/16.
+//  Copyright © 2016 mousebird consulting. All rights reserved.
+//
+
+#import "MaplyTestCase.h"
+
+@interface PagingLayerTestCase : MaplyTestCase
+
+@end
