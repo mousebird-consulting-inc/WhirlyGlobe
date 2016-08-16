3850b1066d37d3dbe1197839a292da87408acee7
Steve Gifford <sjg@mousebirdconsulting.com>
2016-08-16 12:00:41 -0700

    Grid clipper now clips all loops rather than just one outer loop.

--- WhirlyGlobeSrc/WhirlyGlobeLib/src/GridClipper.mm
+++ WhirlyGlobeSrc/WhirlyGlobeLib/src/GridClipper.mm
@@ -164,6 +164,52 @@ bool ClipLoopToMbr(const VectorRing &ring,const Mbr &mbr, bool closed,std::vecto
     return true;
 }
 
+// Clip the given loop to the given MBR
+bool ClipLoopsToMbr(const std::vector<VectorRing> &rings,const Mbr &mbr, bool closed,std::vector<VectorRing> &rets)
+{
+    Clipper c;
+    
+    for (const auto &ring: rings)
+    {
+        Path subject(ring.size());
+        for (unsigned int ii=0;ii<ring.size();ii++)
+        {
+            const Point2f &pt = ring[ii];
+            subject[ii] = IntPoint(pt.x()*PolyScale,pt.y()*PolyScale);
+        }
+        c.AddPath(subject, ptSubject, closed);
+    }
+
+    Path clip(4);
+    clip[0] = IntPoint(mbr.ll().x()*PolyScale,mbr.ll().y()*PolyScale);
+    clip[1] = IntPoint(mbr.ur().x()*PolyScale,mbr.ll().y()*PolyScale);
+    clip[2] = IntPoint(mbr.ur().x()*PolyScale,mbr.ur().y()*PolyScale);
+    clip[3] = IntPoint(mbr.ll().x()*PolyScale,mbr.ur().y()*PolyScale);
+    
+    c.AddPath(clip, ptClip, true);
+    Paths solution;
+    if (!c.Execute(ctIntersection, solution))
+    {
+        return false;
+    }
+    
+    for (unsigned int ii=0;ii<solution.size();ii++)
+    {
+        Path &outPoly = solution[ii];
+        VectorRing outRing;
+        for (unsigned jj=0;jj<outPoly.size();jj++)
+        {
+            IntPoint &outPt = outPoly[jj];
+            outRing.push_back(Point2f(outPt.X/PolyScale,outPt.Y/PolyScale));
+        }
+        
+        if (outRing.size() > 2)
+            rets.push_back(outRing);
+    }
+
+    return true;
+}
+
 // Clip the given loop to the given grid (org and spacing)
 // Return true on success and the new polygons in the rets
 // Note: Not deeply efficient
@@ -186,6 +232,49 @@ bool ClipLoopToGrid(const VectorRing &ring,Point2f org,Point2f spacing,std::vect
         
         std::vector<VectorRing> leftStrip;
         ClipLoopToMbr(ring,left, true, leftStrip);
+        
+        // Now clip the left strip vertically
+        for (int iy=ll_iy;iy<=ur_iy;iy++)
+        {
+            
+            Point2f b0(mbr.ll().x(),iy*spacing.y()+org.y());
+            Point2f b1(mbr.ur().x(),(iy+1)*spacing.y()+org.y());
+            Mbr bot(b0,b1);
+            for (unsigned int ic=0;ic<leftStrip.size();ic++)
+                ClipLoopToMbr(leftStrip[ic], bot, true, rets);
+        }
+    }
+    
+    for (unsigned int ii=startRet;ii<rets.size();ii++)
+    {
+        VectorRing &theRing = rets[ii];
+        std::reverse(theRing.begin(),theRing.end());
+    }
+    
+    return true;
+}
+    
+bool ClipLoopsToGrid(const std::vector<VectorRing> &rings,Point2f org,Point2f spacing,std::vector<VectorRing> &rets)
+{
+    Mbr mbr;
+    for (const auto &ring : rings)
+        mbr.addPoints(ring);
+    int startRet = (int)(rets.size());
+    
+    int ll_ix = (int)std::floor((mbr.ll().x()-org.x())/spacing.x());
+    int ll_iy = (int)std::floor((mbr.ll().y()-org.y())/spacing.y());
+    int ur_ix = (int)std::ceil((mbr.ur().x()-org.x())/spacing.x());
+    int ur_iy = (int)std::ceil((mbr.ur().y()-org.y())/spacing.y());
+    
+    // Clip in strips from left to right
+    for (int ix=ll_ix;ix<=ur_ix;ix++)
+    {
+        Point2f l0(ix*spacing.x()+org.x(),mbr.ll().y());
+        Point2f l1((ix+1)*spacing.x()+org.x(),mbr.ur().y());
+        Mbr left(l0,l1);
+        
+        std::vector<VectorRing> leftStrip;
+        ClipLoopsToMbr(rings,left, true, leftStrip);
         
         // Now clip the left strip vertically
         for (int iy=ll_iy;iy<=ur_iy;iy++)
