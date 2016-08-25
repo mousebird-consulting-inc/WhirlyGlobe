e1afcfa753746ce41b39920a11edd98ab3a0714e
Ranen Ghosh <ranen@ranenghosh.com>
2016-08-25 16:41:14 -0400

    Styled layer descriptor; delay styled layer creation to allow valid view for min and max scale enforcement
    Former-commit-id: e52a1371fd319c8e38c77c11fc84c38eb4480361

--- WhirlyGlobeSrc/AutoTester/AutoTester/testCases/MapzenVectorTestCase.swift
+++ WhirlyGlobeSrc/AutoTester/AutoTester/testCases/MapzenVectorTestCase.swift
@@ -23,31 +23,37 @@ class MapzenVectorTestCase: MaplyTestCase {
 //		baseLayer.setUpWithMap(mapVC)
 
 		//let styleData = NSData(contentsOfFile: NSBundle.mainBundle().pathForResource("MapzenGLStyle", ofType: "json")!)
-        let styleData = NSData(contentsOfFile: NSBundle.mainBundle().pathForResource("MapzenSLDStyle", ofType: "sld")!)
 
-		let mzSource = MapzenSource(
-			base: "http://vector.mapzen.com/osm",
-			layers: ["all"],
-			apiKey: "vector-tiles-ejNTZ28",
-			sourceType: MapzenSourcePBF,
-			styleData: styleData,
-			styleType: .SLDStyle,
-			viewC: mapVC)
-
-		mzSource.minZoom = Int32(0)
-		mzSource.maxZoom = Int32(24)
-
-		let pageLayer = MaplyQuadPagingLayer(
-			coordSystem: MaplySphericalMercator(),
-			delegate: mzSource)
-
-		pageLayer?.numSimultaneousFetches = Int32(8)
-		pageLayer?.flipY = false
-		pageLayer?.importance = 512*512
-		pageLayer?.useTargetZoomLevel = true
-		pageLayer?.singleLevelLoading = true
-		mapVC.addLayer(pageLayer!)
-		mapVC.animateToPosition(MaplyCoordinateMakeWithDegrees(-122.290,37.7793), height: 0.0005, time: 0.1)
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC))), dispatch_get_main_queue(), {
+            
+            let styleData = NSData(contentsOfFile: NSBundle.mainBundle().pathForResource("MapzenSLDStyle", ofType: "sld")!)
+            
+            let mzSource = MapzenSource(
+                base: "http://vector.mapzen.com/osm",
+                layers: ["all"],
+                apiKey: "vector-tiles-ejNTZ28",
+                sourceType: MapzenSourcePBF,
+                styleData: styleData,
+                styleType: .SLDStyle,
+                viewC: mapVC)
+            
+            mzSource.minZoom = Int32(0)
+            mzSource.maxZoom = Int32(24)
+            
+            let pageLayer = MaplyQuadPagingLayer(
+                coordSystem: MaplySphericalMercator(),
+                delegate: mzSource)
+            
+            pageLayer?.numSimultaneousFetches = Int32(8)
+            pageLayer?.flipY = false
+            pageLayer?.importance = 512*512
+            pageLayer?.useTargetZoomLevel = true
+            pageLayer?.singleLevelLoading = true
+            mapVC.addLayer(pageLayer!)
+            mapVC.animateToPosition(MaplyCoordinateMakeWithDegrees(-122.290,37.7793), height: 0.0005, time: 0.1)
+            
+            });
 	}
 
 }
