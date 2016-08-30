9a80851cdefe1b05bfced60c955aa82115bd674a
Steve Gifford <sjg@mousebirdconsulting.com>
2016-08-30 16:22:36 -0700

    Remove Mapbox GL Style Sheet Support. Also removed all examples that used Mapbox vector data sources.  These are not compatible with the Mapbox Terms of Service.

--- WhirlyGlobeSrc/AutoTester/AutoTester/StartupViewController.swift
+++ WhirlyGlobeSrc/AutoTester/AutoTester/StartupViewController.swift
@@ -35,7 +35,6 @@ class StartupViewController: UITableViewController, UIPopoverControllerDelegate 
 
 		PagingLayerTestCase(),
 		MapzenVectorTestCase(),
-		MapjamVectorTestCase(),
 		VectorMBTilesTestCase(),
 
 		StarsSunTestCase(),
