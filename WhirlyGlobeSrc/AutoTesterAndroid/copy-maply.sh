86dff09549002d8d0d058ee7b4c16be500eb980e
JM <jmnavarro@gmail.com>
2016-09-01 09:16:57 +0200

    Add more options to copy-maply
    Dear Mr. Jenkins: 
    
    Would you mind to fix it?
    
    Sincerely, JM

--- WhirlyGlobeSrc/AutoTesterAndroid/copy-maply.sh
+++ WhirlyGlobeSrc/AutoTesterAndroid/copy-maply.sh
@@ -2,11 +2,23 @@
 #        then run this script to copy the aar file to the right 
 #        directory for AutoTester app
 
-if [ Maply/Maply.aar -ot ../Android/build/outputs/aar/Android-debug.aar ]; then
-    echo "Copying Maply..."
-    cp ../Android/build/outputs/aar/Android-debug.aar Maply/Maply.aar
+#  Debug vs. Release
+if [ -n "$1" ]; then
+    vers=${1}
 else
-    echo "Skipped"
+    vers="debug"
+fi
+
+if [[ $2 == "nocache" ]]; then
+    echo "Copying Maply $vers (nocache)..."
+    cp ../Android/build/outputs/aar/Android-$vers.aar Maply/Maply.aar
+else
+    if [ Maply/Maply.aar -ot ../Android/build/outputs/aar/Android-debug.aar ]; then
+        echo "Copying Maply $vers..."
+        cp ../Android/build/outputs/aar/Android-debug.aar Maply/Maply.aar
+    else
+        echo "Skipped"
+    fi
 fi
 
 exit 0
