d03aa023c2777d6573fae125b61b063e679d9d64
JM <jmnavarro@gmail.com>
2017-03-21 18:30:58 +0100

    Change paths

--- android/apps/AutoTesterAndroid/copy-maply.sh
+++ android/apps/AutoTesterAndroid/copy-maply.sh
@@ -11,11 +11,11 @@ fi
 
 if [[ $2 == "nocache" ]]; then
     echo "Copying Maply $vers (nocache)..."
-    cp ../Android/build/outputs/aar/Android-$vers.aar Maply/Maply.aar
+    cp ../../library/Android/build/outputs/aar/Android-$vers.aar Maply/Maply.aar
 else
-    if [ Maply/Maply.aar -ot ../Android/build/outputs/aar/Android-debug.aar ]; then
+    if [ Maply/Maply.aar -ot ../../library/Android/build/outputs/aar/Android-debug.aar ]; then
         echo "Copying Maply $vers..."
-        cp ../Android/build/outputs/aar/Android-debug.aar Maply/Maply.aar
+        cp ../../library/Android/build/outputs/aar/Android-debug.aar Maply/Maply.aar
     else
         echo "Skipped"
     fi
