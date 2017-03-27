1ae86fed87572c8ebf3e9debefad2c06a9cdc622
Steve Gifford <sjg@mousebirdconsulting.com>
2017-03-27 11:19:37 -0700

    Added a bit more sticker test code.
    Former-commit-id: 6d1ace6c022da46e85983d67b0263e0243acb2a0

--- ios/apps/AutoTester/AutoTester/testCases/StickersTestCase.swift
+++ ios/apps/AutoTester/AutoTester/testCases/StickersTestCase.swift
@@ -31,16 +31,40 @@ class StickersTestCase: MaplyTestCase {
 		}
 		baseViewC.addStickers(stickers, desc: [kMaplyFade: (1.0)])
 	}
+    
+    func addTestSticker (baseViewC: MaplyBaseViewController) {
+        let startImage = UIImage(named: "Smiley_Face_Avatar_by_PixelTwist")
+        
+        let sticker = MaplySticker()
+        sticker.coordSys = MaplySphericalMercator.init(webStandard: ())
+        sticker.ll = (sticker.coordSys?.geo(toLocal: MaplyCoordinateMakeWithDegrees(-223.9008, -63.4974) ))!
+        sticker.ur = (sticker.coordSys?.geo(toLocal: MaplyCoordinateMakeWithDegrees(-105.3074, 36.1572) ))!
+//        sticker.ll.x -= Float(2*M_PI)
+
+//        sticker.ll = MaplyCoordinateMakeWithDegrees(-223.9008, -63.4974)
+//        sticker.ur = MaplyCoordinateMakeWithDegrees(-105.3074, 36.1572)
+//        sticker.ll = MaplyCoordinateMakeWithDegrees(-76.4594, -19.9719)
+//        sticker.ur = MaplyCoordinateMakeWithDegrees(19.954, 61.5041)
+//        sticker.image = startImage
+        baseViewC.addStickers([sticker], desc: [kMaplyColor: UIColor.init(white: 0.25, alpha: 0.25)] )
+//        baseViewC.addStickers([sticker], desc: [kMaplyColor: UIColor.init(white: 1.0, alpha: 1.0)] )
+    }
 	
 	override func setUpWithGlobe(_ globeVC: WhirlyGlobeViewController) {
-		let baseLayer = VectorsTestCase()
-		baseLayer.setUpWithGlobe(globeVC)
-		addStickers(baseLayer.compList, baseViewC: globeVC)
+        let baseLayer = VectorsTestCase()
+        baseLayer.setUpWithGlobe(globeVC)
+
+        addStickers(baseLayer.compList, baseViewC: globeVC)
+        
+//        addTestSticker(baseViewC: globeVC)
 	}
 	
 	override func setUpWithMap(_ mapVC: MaplyViewController) {
 		let baseLayer = VectorsTestCase()
 		baseLayer.setUpWithMap(mapVC)
-		addStickers(baseLayer.compList, baseViewC: mapVC)
+
+        addStickers(baseLayer.compList, baseViewC: mapVC)
+        
+//        addTestSticker(baseViewC: mapVC)
 	}
 }
