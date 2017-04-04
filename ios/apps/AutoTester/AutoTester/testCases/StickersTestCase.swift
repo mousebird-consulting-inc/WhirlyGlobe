1e7377cfe2e459f26cfd67c8fab352c73129493b
Steve Gifford <sjg@mousebirdconsulting.com>
2017-04-04 13:30:09 -0700

    Added to stickers test case.

--- ios/apps/AutoTester/AutoTester/testCases/StickersTestCase.swift
+++ ios/apps/AutoTester/AutoTester/testCases/StickersTestCase.swift
@@ -33,21 +33,23 @@ class StickersTestCase: MaplyTestCase {
 	}
     
     func addTestSticker (baseViewC: MaplyBaseViewController) {
-        let startImage = UIImage(named: "Smiley_Face_Avatar_by_PixelTwist")
+        let startImage = UIImage(named: "greensquare.png")
         
         let sticker = MaplySticker()
         sticker.coordSys = MaplySphericalMercator.init(webStandard: ())
-        sticker.ll = (sticker.coordSys?.geo(toLocal: MaplyCoordinateMakeWithDegrees(-223.9008, -63.4974) ))!
-        sticker.ur = (sticker.coordSys?.geo(toLocal: MaplyCoordinateMakeWithDegrees(-105.3074, 36.1572) ))!
+//        sticker.ll = (sticker.coordSys?.geo(toLocal: MaplyCoordinateMakeWithDegrees(-223.9008, -63.4974) ))!
+//        sticker.ur = (sticker.coordSys?.geo(toLocal: MaplyCoordinateMakeWithDegrees(-105.3074, 36.1572) ))!
 //        sticker.ll.x -= Float(2*M_PI)
+        sticker.ll = (sticker.coordSys?.geo(toLocal: MaplyCoordinateMakeWithDegrees(-76.4594, -19.9719) ))!
+        sticker.ur = (sticker.coordSys?.geo(toLocal: MaplyCoordinateMakeWithDegrees( 19.954, 61.5041) ))!
 
 //        sticker.ll = MaplyCoordinateMakeWithDegrees(-223.9008, -63.4974)
 //        sticker.ur = MaplyCoordinateMakeWithDegrees(-105.3074, 36.1572)
 //        sticker.ll = MaplyCoordinateMakeWithDegrees(-76.4594, -19.9719)
 //        sticker.ur = MaplyCoordinateMakeWithDegrees(19.954, 61.5041)
-//        sticker.image = startImage
-        baseViewC.addStickers([sticker], desc: [kMaplyColor: UIColor.init(white: 0.25, alpha: 0.25)] )
-//        baseViewC.addStickers([sticker], desc: [kMaplyColor: UIColor.init(white: 1.0, alpha: 1.0)] )
+        sticker.image = startImage
+//        baseViewC.addStickers([sticker], desc: [kMaplyColor: UIColor.init(white: 0.25, alpha: 0.25)] )
+        baseViewC.addStickers([sticker], desc: [kMaplyColor: UIColor.init(white: 1.0, alpha: 1.0)] )
     }
 	
 	override func setUpWithGlobe(_ globeVC: WhirlyGlobeViewController) {
@@ -65,6 +67,6 @@ class StickersTestCase: MaplyTestCase {
 
         addStickers(baseLayer.compList, baseViewC: mapVC)
         
-//        addTestSticker(baseViewC: mapVC)
+        addTestSticker(baseViewC: mapVC)
 	}
 }
