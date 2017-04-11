9913c7fb5fd4f09581614357e45d6ac782a27bdc
Jean-Michel Cazaux <jeanmichel.cazaux@ironbird.fr>
2017-04-11 17:34:20 +0200

    Test case for issue #894

--- ios/apps/AutoTester/AutoTester/testCases/IBSIssueTesterTestCase.swift
+++ ios/apps/AutoTester/AutoTester/testCases/IBSIssueTesterTestCase.swift
@@ -78,81 +78,76 @@ class IBSIssueTesterTestCase: MaplyTestCase {
 
     private func addObjects(toController vc: MaplyBaseViewController) {
 
-        let lat: Float = 44
-        let lon: Float = 4
-        let loc = MaplyCoordinateMakeWithDegrees(lon, lat)
-
-        // Adding the text at the bottom
-        var label = MaplyScreenLabel()
-        label.text = "The quick brown fox is at the bottom"
-        label.loc = loc
-
-        var info = Dictionary<AnyHashable, Any>()
-
-        info[kMaplyTextColor] = UIColor.brown
-        info[kMaplyTextOutlineColor] = UIColor.white
-        info[kMaplyTextOutlineSize] = 2.0
-        info[kMaplyDrawPriority] = 10
-        info[kMaplyEnable] = true
-        info[kMaplyFont] = UIFont.init(name: "Helvetica-Bold", size: 24.0)
-
-        vc.addScreenLabels([label], desc: info, mode: MaplyThreadMode.current)
+        let loc = MaplyCoordinateMakeWithDegrees(4, 44)
 
 
-        // Adding a marker above the text
-        let marker = MaplyScreenMarker()
+        // Adding a standard marker
+        var marker = MaplyScreenMarker()
         marker.image = UIImage(named:"Smiley_Face_Avatar_by_PixelTwist")
         marker.loc = loc
-        marker.size = CGSize(width: 100, height: 100)
+        marker.size = CGSize(width: 50, height: 50)
 
-        info = Dictionary<AnyHashable, Any>()
+        var info = Dictionary<AnyHashable, Any>()
 
         info[kMaplyDrawPriority] = 20
 
         vc.addScreenMarkers([marker], desc: info, mode: MaplyThreadMode.current)
 
-
-
-        // Adding a cross made of wide vectors above the marker
-        let ur = MaplyCoordinateMakeWithDegrees(lon + 1, lat + 1)
-        let ll = MaplyCoordinateMakeWithDegrees(lon - 1, lat - 1)
-        let ul = MaplyCoordinateMakeWithDegrees(lon - 1, lat + 1)
-        let lr = MaplyCoordinateMakeWithDegrees(lon + 1, lat - 1)
-        var ls1 = [ur, ll]
-        let wv1 = MaplyVectorObject(lineString: &ls1, numCoords: 2, attributes: nil)
-        var ls2 = [ul, lr]
-        let wv2 = MaplyVectorObject(lineString: &ls2, numCoords: 2, attributes: nil)
-
-        var lineInfo = Dictionary<AnyHashable, Any>()
-        lineInfo[kMaplyVecWidth] = 50
-        lineInfo[kMaplyColor] = UIColor.red
-        lineInfo[kMaplyDrawPriority] = 30
-        lineInfo[kMaplyEnable] = true
-        lineInfo[kMaplyFilled] = false
-        lineInfo[kMaplyWideVecCoordType] = kMaplyWideVecCoordTypeScreen
-
-        vc.addWideVectors([wv1, wv2], desc: lineInfo, mode: MaplyThreadMode.current)
-
-
-
-
-        // Adding some text above
-        label = MaplyScreenLabel()
-        label.text = "The lazzy dog is at the top"
-        label.loc = MaplyCoordinateMakeWithDegrees(lon, lat)
+        var label = MaplyScreenLabel()
+        label.text = "A colored marker shows here"
+        label.loc = loc
 
         info = Dictionary<AnyHashable, Any>()
 
         info[kMaplyTextColor] = UIColor.blue
         info[kMaplyTextOutlineColor] = UIColor.white
-        info[kMaplyTextOutlineSize] = 1.0
+        info[kMaplyTextOutlineSize] = 2.0
         info[kMaplyDrawPriority] = 50
         info[kMaplyEnable] = true
         info[kMaplyFont] = UIFont.init(name: "Helvetica-Bold", size: 16.0)
 
         vc.addScreenLabels([label], desc: info, mode: MaplyThreadMode.current)
-        
+
+
+        let loc2 = MaplyCoordinateMakeWithDegrees(5, 45)
+
+        // Adding some "warning" text
+        label = MaplyScreenLabel()
+        label.text = "A black & white marker should show here"
+        label.loc = loc2
+
+        info = Dictionary<AnyHashable, Any>()
+
+        info[kMaplyTextColor] = UIColor.red
+        info[kMaplyTextOutlineColor] = UIColor.white
+        info[kMaplyTextOutlineSize] = 2.0
+        info[kMaplyDrawPriority] = 50
+        info[kMaplyEnable] = true
+        info[kMaplyFont] = UIFont.init(name: "Helvetica-Bold", size: 16.0)
+
+        vc.addScreenLabels([label], desc: info, mode: MaplyThreadMode.current)
+
+
+        // Filtering the image
+        let colorMatrixFilter = CIFilter(name: "CIPhotoEffectMono")!
+        colorMatrixFilter.setDefaults()
+        colorMatrixFilter.setValue(CIImage(image: UIImage(named:"Smiley_Face_Avatar_by_PixelTwist")!)!, forKey: kCIInputImageKey)
+
+        if let ciImage = colorMatrixFilter.outputImage {
+            let bwImage = UIImage(ciImage: ciImage, scale: 1.0, orientation: UIImageOrientation.up)
+
+            marker = MaplyScreenMarker()
+            marker.image = bwImage
+            marker.loc = loc2
+            marker.size = CGSize(width: 50, height: 50)
+
+            var info = Dictionary<AnyHashable, Any>()
+
+            info[kMaplyDrawPriority] = 20
+
+            vc.addScreenMarkers([marker], desc: info, mode: MaplyThreadMode.current)
+        } else {
+            print("*** ciImage is nil ***")
+        }
     }
-
-
 }
