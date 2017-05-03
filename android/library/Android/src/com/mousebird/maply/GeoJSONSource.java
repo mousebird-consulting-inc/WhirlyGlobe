dbcec5b1f06903ebc7bf40d82f7c34434ee473e1
Ranen Ghosh <ranen@ranenghosh.com>
2017-05-03 13:30:22 -0400

    flatten GeoJSONSource

--- android/library/Android/src/com/mousebird/maply/GeoJSONSource.java
+++ android/library/Android/src/com/mousebird/maply/GeoJSONSource.java
@@ -68,28 +68,83 @@ public class GeoJSONSource {
     }
 
     public void startParse(final Runnable completionBlock) {
-        ParseTask parseTask = new ParseTask(jsonStream, styleSet, baseController);
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int nRead;
+        byte[] data = new byte[16384];
+        HashMap<String, ArrayList<VectorObject>> featureStyles = new HashMap<String, ArrayList<VectorObject>>();
+        ArrayList<ComponentObject> componentObjects = new ArrayList<ComponentObject>();
 
-        class MyRunnable implements Runnable {
-            ParseTask parseTask;
-            public void setParseTask(ParseTask parseTask) {
-                this.parseTask = parseTask;
+        try {
+
+            while ((nRead = jsonStream.read(data, 0, data.length)) != -1) {
+                buffer.write(data, 0, nRead);
+            }
+            buffer.flush();
+
+            VectorObject[] vecs = parseData(buffer.toString());
+
+            if (vecs != null) {
+                MaplyTileID nullTileID = new MaplyTileID(0,0,0);
+                for (VectorObject vecObj : vecs) {
+                    VectorStyle[] styles = styleSet.stylesForFeature(vecObj.getAttributes(), nullTileID, "", baseController);
+                    if (styles == null || styles.length == 0)
+                        continue;;
+                    for (VectorStyle style : styles) {
+                        ArrayList<VectorObject> featuresForStyle = featureStyles.get(style.getUuid());
+                        if (featuresForStyle == null) {
+                            featuresForStyle = new ArrayList<VectorObject>();
+                            featureStyles.put(style.getUuid(), featuresForStyle);
+                        }
+                        featuresForStyle.add(vecObj);
+                    }
+                }
             }
 
-            @Override
-            public void run() {
-                loaded = true;
-                enabled = true;
-                componentObjects = this.parseTask.getComponentObjects();
-                this.parseTask = null;
-                completionBlock.run();
+            if (vecs != null) {
+                MaplyTileID nullTileID = new MaplyTileID(0, 0, 0);
+                for (String uuid : featureStyles.keySet()) {
+                    VectorStyle style = styleSet.styleForUUID(uuid, baseController);
+                    ArrayList<VectorObject> featuresForStyle = featureStyles.get(uuid);
+                    //List<VectorObject> objects, MaplyTileID tileID, MaplyBaseController controller)
+                    componentObjects.addAll(Arrays.asList(style.buildObjects(featuresForStyle, nullTileID, baseController)));
+                }
             }
+            baseController.enableObjects(componentObjects, MaplyBaseController.ThreadMode.ThreadAny);
+
+            this.componentObjects = componentObjects;
+            loaded = true;
+            enabled = true;
+            completionBlock.run();
+
+        } catch (Exception exception) {
+            Log.e("ParseTask", "exception", exception);
         }
-        MyRunnable runnable = new MyRunnable();
-        runnable.setParseTask(parseTask);
-        parseTask.setPostRun(runnable);
-        parseTask.execute(Integer.valueOf(0));
     }
+
+
+//    public void startParse(final Runnable completionBlock) {
+//        ParseTask parseTask = new ParseTask(jsonStream, styleSet, baseController);
+//
+//        class MyRunnable implements Runnable {
+//            ParseTask parseTask;
+//            public void setParseTask(ParseTask parseTask) {
+//                this.parseTask = parseTask;
+//            }
+//
+//            @Override
+//            public void run() {
+//                loaded = true;
+//                enabled = true;
+//                componentObjects = this.parseTask.getComponentObjects();
+//                this.parseTask = null;
+//                completionBlock.run();
+//            }
+//        }
+//        MyRunnable runnable = new MyRunnable();
+//        runnable.setParseTask(parseTask);
+//        parseTask.setPostRun(runnable);
+//        parseTask.execute(Integer.valueOf(0));
+//    }
 
     public void startParse() {
         startParse(new Runnable() {
