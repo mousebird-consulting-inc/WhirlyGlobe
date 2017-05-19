e9eeb1edd4c5a70aff260e1e365527ac2743d3ed
JM <jmnavarro@gmail.com>
2017-05-20 00:59:03 +0200

    Remove cocoapods (not needed anymore)

--- ios/apps/AutoTester/Pods/Target Support Files/MBProgressHUD/MBProgressHUD-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "MBProgressHUD.h"
-
-FOUNDATION_EXPORT double MBProgressHUDVersionNumber;
-FOUNDATION_EXPORT const unsigned char MBProgressHUDVersionString[];
-
