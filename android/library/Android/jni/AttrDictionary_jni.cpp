315d2eccbbf7a1ee49796da639900776512a8e4d
Ranen Ghosh <ranen@ranenghosh.com>
2017-07-03 18:22:13 -0400

    AttrDictionary addEntries

--- android/library/Android/jni/AttrDictionary_jni.cpp
+++ android/library/Android/jni/AttrDictionary_jni.cpp
@@ -305,5 +305,25 @@ JNIEXPORT jobject JNICALL Java_com_mousebird_maply_AttrDictionary_get
 	return NULL;
 }
 
+JNIEXPORT void JNICALL Java_com_mousebird_maply_AttrDictionary_addEntries
+(JNIEnv *env, jobject obj, jobject other)
+{
+	try
+	{
+		AttrDictClassInfo *classInfo = AttrDictClassInfo::getClassInfo();
+		Dictionary *dict = classInfo->getObject(env,obj);
+		Dictionary *otherDict = classInfo->getObject(env,other);
+		if (!dict || !other)
+			return;
+
+        dict->addEntries(otherDict);
+
+
+	}
+	catch (...)
+	{
+		__android_log_print(ANDROID_LOG_VERBOSE, "Maply", "Crash in Dictionary::addEntries()");
+	}
+}
 
 
