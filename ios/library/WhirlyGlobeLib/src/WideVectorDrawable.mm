476a1ab07ee2215576d1dcb40b2e1b9d5ac9e232
Steve Gifford <sjg@mousebirdconsulting.com>
2018-09-05 13:20:41 -0700

    Switched to high precision for wide vector shaders.  Fixes a problem when zoomed in very close to the globe.  Also switched to multi-line C++ string literals because they're awesome.
    Former-commit-id: f861bcbb59fa6850e09406c6b9c05c6f94fbc08e

--- ios/library/WhirlyGlobeLib/src/WideVectorDrawable.mm
+++ ios/library/WhirlyGlobeLib/src/WideVectorDrawable.mm
@@ -171,132 +171,135 @@ void WideVectorDrawable::draw(WhirlyKitRendererFrameInfo *frameInfo, Scene *scen
 //    }
 }
 
-static const char *vertexShaderTri =
-"uniform mat4  u_mvpMatrix;\n"
-"uniform mat4  u_mvMatrix;"
-"uniform mat4  u_mvNormalMatrix;"
-"uniform float u_fade;\n"
-"uniform float u_w2;\n"
-"uniform float u_real_w2;\n"
-"uniform float u_texScale;\n"
-"uniform vec4 u_color;\n"
-"\n"
-"attribute vec3 a_position;\n"
-"attribute vec3 a_normal;\n"
-"attribute vec4 a_texinfo;\n"
-//"attribute vec4 a_color;\n"
-"attribute vec3 a_p1;\n"
-"attribute vec3 a_n0;\n"
-"attribute float a_c0;\n"
-"\n"
-"varying vec2 v_texCoord;\n"
-//"varying vec4 v_color;\n"
-"\n"
-"void main()\n"
-"{\n"
+static const char *vertexShaderTri = R"(
+precision highp float;
+uniform mat4  u_mvpMatrix;
+uniform mat4  u_mvMatrix;
+    uniform mat4  u_mvNormalMatrix;
+uniform float u_fade;
+uniform float u_w2;
+uniform float u_real_w2;
+uniform float u_texScale;
+uniform vec4 u_color;
+
+attribute vec3 a_position;
+attribute vec3 a_normal;
+attribute vec4 a_texinfo;
+//"attribute vec4 a_color;
+attribute vec3 a_p1;
+attribute vec3 a_n0;
+attribute float a_c0;
+
+varying vec2 v_texCoord;
+//"varying vec4 v_color;
+
+void main()
+{
 //"   v_color = a_color;\n"
 //  Position along the line
-"   float t0 = a_c0 * u_real_w2;\n"
-"   t0 = clamp(t0,0.0,1.0);\n"
-"   vec3 realPos = (a_p1 - a_position) * t0 + a_n0 * u_real_w2 + a_position;\n"
-"   float texPos = ((a_texinfo.z - a_texinfo.y) * t0 + a_texinfo.y + a_texinfo.w * u_real_w2) * u_texScale;\n"
-"   v_texCoord = vec2(a_texinfo.x, texPos);\n"
-"   vec4 screenPos = u_mvpMatrix * vec4(realPos,1.0);\n"
-"   screenPos /= screenPos.w;\n"
-"   gl_Position = vec4(screenPos.xy,0,1.0);\n"
-"}\n"
-;
+   float t0 = a_c0 * u_real_w2;
+   t0 = clamp(t0,0.0,1.0);
+   vec3 realPos = (a_p1 - a_position) * t0 + a_n0 * u_real_w2 + a_position;
+   float texPos = ((a_texinfo.z - a_texinfo.y) * t0 + a_texinfo.y + a_texinfo.w * u_real_w2) * u_texScale;
+   v_texCoord = vec2(a_texinfo.x, texPos);
+   vec4 screenPos = u_mvpMatrix * vec4(realPos,1.0);
+   screenPos /= screenPos.w;
+   gl_Position = vec4(screenPos.xy,0,1.0);
+}
+)";
 
-static const char *vertexGlobeShaderTri =
-"uniform mat4  u_mvpMatrix;\n"
-"uniform mat4  u_mvMatrix;"
-"uniform mat4  u_mvNormalMatrix;"
-"uniform float u_fade;\n"
-"uniform float u_w2;\n"
-"uniform float u_real_w2;\n"
-"uniform float u_texScale;\n"
-"uniform vec4 u_color;\n"
-"\n"
-"attribute vec3 a_position;\n"
-"attribute vec3 a_normal;\n"
-"attribute vec4 a_texinfo;\n"
-//"attribute vec4 a_color;\n"
-"attribute vec3 a_p1;\n"
-"attribute vec3 a_n0;\n"
-"attribute float a_c0;\n"
-"\n"
-"varying vec2 v_texCoord;\n"
-//"varying vec4 v_color;\n"
-"varying float      v_dot;\n"
-"\n"
-"void main()\n"
-"{\n"
-//"   v_color = a_color;\n"
+static const char *vertexGlobeShaderTri = R"(
+precision highp float;
+    
+uniform mat4  u_mvpMatrix;
+uniform mat4  u_mvMatrix;
+uniform mat4  u_mvNormalMatrix;
+uniform float u_fade;
+uniform float u_w2;
+uniform float u_real_w2;
+uniform float u_texScale;
+uniform vec4 u_color;
+
+attribute vec3 a_position;
+attribute vec3 a_normal;
+attribute vec4 a_texinfo;
+//"attribute vec4 a_color;
+attribute vec3 a_p1;
+attribute vec3 a_n0;
+attribute float a_c0;
+
+varying vec2 v_texCoord;
+//"varying vec4 v_color;
+varying float      v_dot;
+
+void main()
+{
+//"   v_color = a_color;
 //  Position along the line
-"   float t0 = a_c0 * u_real_w2;\n"
-"   t0 = clamp(t0,0.0,1.0);\n"
-"   vec3 realPos = (a_p1 - a_position) * t0 + a_n0 * u_real_w2 + a_position;\n"
-"   vec4 pt = u_mvMatrix * vec4(a_position,1.0);\n"
-"   pt /= pt.w;\n"
-"   vec4 testNorm = u_mvNormalMatrix * vec4(a_normal,0.0);\n"
-"   v_dot = dot(-pt.xyz,testNorm.xyz);\n"
-"   float texPos = ((a_texinfo.z - a_texinfo.y) * t0 + a_texinfo.y + a_texinfo.w * u_real_w2) * u_texScale;\n"
-"   v_texCoord = vec2(a_texinfo.x, texPos);\n"
-"   vec4 screenPos = u_mvpMatrix * vec4(realPos,1.0);\n"
-"   screenPos /= screenPos.w;\n"
-"   gl_Position = vec4(screenPos.xy,0,1.0);\n"
-"}\n"
-;
+   float t0 = a_c0 * u_real_w2;
+   t0 = clamp(t0,0.0,1.0);
+   vec3 realPos = (a_p1 - a_position) * t0 + a_n0 * u_real_w2 + a_position;
+   vec4 pt = u_mvMatrix * vec4(a_position,1.0);
+   pt /= pt.w;
+    vec4 testNorm = u_mvNormalMatrix * vec4(a_normal,0.0);
+   v_dot = dot(-pt.xyz,testNorm.xyz);
+   float texPos = ((a_texinfo.z - a_texinfo.y) * t0 + a_texinfo.y + a_texinfo.w * u_real_w2) * u_texScale;
+   v_texCoord = vec2(a_texinfo.x, texPos);
+   vec4 screenPos = u_mvpMatrix * vec4(realPos,1.0);
+   screenPos /= screenPos.w;
+   gl_Position = vec4(screenPos.xy,0,1.0);
+}
+)";
 
-static const char *fragmentShaderTriAlias =
-"precision mediump float;\n"
-"\n"
-"uniform sampler2D s_baseMap0;\n"
-"uniform bool  u_hasTexture;\n"
-"uniform float u_w2;\n"
-"uniform float u_edge;\n"
-"uniform vec4 u_color;\n"
-"\n"
-"varying vec2      v_texCoord;\n"
-"\n"
-"void main()\n"
-"{\n"
-"  float patternVal = u_hasTexture ? texture2D(s_baseMap0, vec2(0.5,v_texCoord.y)).a : 1.0;\n"
-"  float alpha = 1.0;\n"
-"  float across = v_texCoord.x * u_w2;\n"
-"  if (across < u_edge)\n"
-"    alpha = across/u_edge;\n"
-"  if (across > u_w2-u_edge)\n"
-"    alpha = (u_w2-across)/u_edge;\n"
-"  gl_FragColor = u_color * alpha * patternVal;\n"
-"}\n"
-;
+static const char *fragmentShaderTriAlias = R"(
+precision highp float;
 
-static const char *fragmentGlobeShaderTriAlias =
-"precision mediump float;\n"
-"\n"
-"uniform sampler2D s_baseMap0;\n"
-"uniform bool  u_hasTexture;\n"
-"uniform float u_w2;\n"
-"uniform float u_edge;\n"
-"uniform vec4 u_color;\n"
-"\n"
-"varying vec2      v_texCoord;\n"
-"varying float      v_dot;\n"
-//"varying vec4      v_color;\n"
-"\n"
-"void main()\n"
-"{\n"
-"  float patternVal = u_hasTexture ? texture2D(s_baseMap0, vec2(0.5,v_texCoord.y)).a : 1.0;\n"
-"  float alpha = 1.0;\n"
-"  float across = v_texCoord.x * u_w2;\n"
-"  if (across < u_edge)\n"
-"    alpha = across/u_edge;\n"
-"  if (across > u_w2-u_edge)\n"
-"    alpha = (u_w2-across)/u_edge;\n"
-"  gl_FragColor = (v_dot > 0.0 ? u_color * alpha * patternVal : vec4(0.0,0.0,0.0,0.0));\n"
-"}\n"
-;
+uniform sampler2D s_baseMap0;
+uniform bool  u_hasTexture;
+uniform float u_w2;
+uniform float u_edge;
+uniform vec4 u_color;
+
+varying vec2      v_texCoord;
+
+void main()
+{
+  float patternVal = u_hasTexture ? texture2D(s_baseMap0, vec2(0.5,v_texCoord.y)).a : 1.0;
+  float alpha = 1.0;
+  float across = v_texCoord.x * u_w2;
+  if (across < u_edge)
+    alpha = across/u_edge;
+  if (across > u_w2-u_edge)
+    alpha = (u_w2-across)/u_edge;
+  gl_FragColor = u_color * alpha * patternVal;
+}
+)";
+
+static const char *fragmentGlobeShaderTriAlias = R"(
+precision highp float;
+
+uniform sampler2D s_baseMap0;
+uniform bool  u_hasTexture;
+uniform float u_w2;
+uniform float u_edge;
+uniform vec4 u_color;
+
+varying vec2      v_texCoord;
+varying float      v_dot;
+//"varying vec4      v_color;
+
+void main()
+{
+  float patternVal = u_hasTexture ? texture2D(s_baseMap0, vec2(0.5,v_texCoord.y)).a : 1.0;
+  float alpha = 1.0;
+  float across = v_texCoord.x * u_w2;
+  if (across < u_edge)
+    alpha = across/u_edge;
+  if (across > u_w2-u_edge)
+    alpha = (u_w2-across)/u_edge;
+  gl_FragColor = (v_dot > 0.0 ? u_color * alpha * patternVal : vec4(0.0,0.0,0.0,0.0));
+}
+)";
 
 WhirlyKit::OpenGLES2Program *BuildWideVectorProgram()
 {
