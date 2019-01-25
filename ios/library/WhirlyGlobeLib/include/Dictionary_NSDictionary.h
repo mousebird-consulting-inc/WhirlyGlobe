89f63552118373cc13b1214481397e709919d4f8
Steve Gifford <sjg@mousebirdconsulting.com>
2019-01-24 17:29:12 -0800

    C++ Dictionary wrapped around NSDictionary.

---
+++ ios/library/WhirlyGlobeLib/include/Dictionary_NSDictionary.h
@@ -0,0 +1,95 @@
+/*
+ *  Dictionary_NSDictionary.h
+ *  WhirlyGlobeLib
+ *
+ *  Created by Steve Gifford on 1/24/19.
+ *  Copyright 2011-2019 mousebird consulting
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+#import <Foundation/Foundation.h>
+#import "Dictionary.h"
+
+namespace WhirlyKit
+{
+    
+/// The Dictionary is my cross platform replacement for NSDictionary
+/// On iOS its just a wrapper
+class iosDictionary : public Dictionary
+{
+public:
+    iosDictionary();
+    iosDictionary(NSDictionary *dict);
+    // Copy constructor
+    virtual iosDictionary(const iosDictionary &that);
+    virtual ~iosDictionary();
+    
+    /// Returns true if the field exists
+    bool hasField(const std::string &name) const;
+    
+    /// Returns the field type
+    DictionaryType getType(const std::string &name) const;
+    
+    /// Return an int, using the default if it's missing
+    int getInt(const std::string &name,int defVal=0.0) const;
+    /// Return a 64 bit unique identity or 0 if missing
+    SimpleIdentity getIdentity(const std::string &name) const;
+    /// Interpret an int as a boolean
+    bool getBool(const std::string &name,bool defVal=false) const;
+    /// Interpret an int as a RGBA color
+    RGBAColor getColor(const std::string &name,const RGBAColor &defVal) const;
+    /// Return a double, using the default if it's missing
+    double getDouble(const std::string &name,double defVal=0.0) const;
+    /// Return a string, or empty if it's missing
+    std::string getString(const std::string &name) const;
+    /// Return a string, using the default if it's missing
+    std::string getString(const std::string &name,const std::string &defVal) const;
+    
+protected:
+    NSDictionary *dict;
+};
+
+/// This version of the dictionary can be modified
+class iosMutableDictionary : public MutableDictionary, public Dictionary
+{
+public:
+    iosMutableDictionary();
+    iosMutableDictionary(NSMutableDictionary *);
+    // Assignment operator
+    virtual iosMutableDictionary &operator = (const iosMutableDictionary &that);
+    virtual ~iosMutableDictionary()
+
+    /// Clean out the contents
+    void clear();
+    
+    /// Remove the given field by name
+    void removeField(const std::string &name);
+    
+    /// Set field as int
+    void setInt(const std::string &name,int val);
+    /// Set field as 64 bit unique value
+    void setIdentifiable(const std::string &name,SimpleIdentity val);
+    /// Set field as double
+    void setDouble(const std::string &name,double val);
+    /// Set field as string
+    void setString(const std::string &name,const std::string &val);
+    
+    // Merge in key-value pairs from another dictionary
+    void addEntries(const Dictionary *other);
+    
+protected:
+    NSMutableDictionary *dict;
+};
+    
+}
---
+++ ios/library/WhirlyGlobeLib/src/Dictinary_NSDictionary.mm
@@ -0,0 +1,189 @@
+/*
+ *  Dictionary_NSDictionary.h
+ *  WhirlyGlobeLib
+ *
+ *  Created by Steve Gifford on 1/24/19.
+ *  Copyright 2011-2019 mousebird consulting
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+#import "Dictionary_NSDictionary.h"
+#import "NSDictionary+Stuff.h"
+
+namespace WhirlyKit {
+    
+static NSString *StdStringToString(const std::string &str)
+{
+    return [NSString stringWithFormat:@"%s",str.c_str()];
+}
+    
+static std::string StringToStdString(NSString *str)
+{
+    return std::string([str cStringUsingEncoding:NSASCIIStringEncoding]);
+}
+    
+iosDictionary::iosDictionary()
+{
+    dict = [[NSDictionary alloc] init];
+}
+
+iosDictionary::iosDictionary(NSDictionary *inDict)
+{
+    dict = inDict;
+}
+
+iosDictionary::iosDictionary(const iosDictionary &that)
+{
+    dict = [[NSDictionary alloc] initWithDictionary:that.dict];
+}
+
+iosDictionary::~iosDictionary()
+{
+    dict = nil;
+}
+
+/// Returns true if the field exists
+bool iosDictionary::hasField(const std::string &name) const
+{
+    return [dict objectForKey:StdStringToString(name)] != nil;
+}
+
+/// Returns the field type
+DictionaryType iosDictionary::getType(const std::string &name) const
+{
+    id obj = [dict objectForKey:StdStringToString(name)];
+    if (obj == nil)
+        return DictTypeNone;
+    
+    if ([obj isKindOfClass:[NSString class]])
+        return DictTypeString;
+    else if ([obj isKindOfClass:[NSNumber class]]) {
+        // Note: Can't tell the difference between the types
+        return DictTypeDouble;
+    }
+    
+    return DictTypeObject;
+}
+
+/// Return an int, using the default if it's missing
+int iosDictionary::getInt(const std::string &name,int defVal) const
+{
+    NSString *theName = StdStringToString(name);
+    
+    return [dict intForKey:theName default:defVal];
+}
+
+/// Return a 64 bit unique identity or 0 if missing
+SimpleIdentity iosDictionary::getIdentity(const std::string &name) const
+{
+    NSString *theName = StdStringToString(name);
+
+    return [dict intForKey:theName default:defVal];
+}
+
+/// Interpret an int as a boolean
+bool iosDictionary::getBool(const std::string &name,bool defVal) const
+{
+    NSString *theName = StdStringToString(name);
+
+    return [dict boolForKey:theName default:defVal];
+}
+
+/// Interpret an int as a RGBA color
+RGBAColor iosDictionary::getColor(const std::string &name,const RGBAColor &defVal) const
+{
+    NSString *theName = StdStringToString(name);
+
+    throw "Not implemented";
+    return RGBAColor();
+}
+
+/// Return a double, using the default if it's missing
+double iosDictionary::getDouble(const std::string &name,double defVal) const
+{
+    NSString *theName = StdStringToString(name);
+
+    return [dict doubleForKey:theName default:defVal];
+}
+
+/// Return a string, or empty if it's missing
+std::string iosDictionary::getString(const std::string &name) const
+{
+    NSString *theName = StdStringToString(name);
+
+    return StringToStdString([dict stringForKey:theName default:nil]);
+}
+
+/// Return a string, using the default if it's missing
+std::string iosDictionary::getString(const std::string &name,const std::string &defVal) const
+{
+    NSString *theName = StdStringToString(name);
+
+    return StringToStdString([dict stringForKey:theName default:StdStringToString(defVal)]);
+}
+
+
+iosMutableDictionary::iosMutableDictionary()
+{
+    dict = [[NSMutableDictionary alloc] init];
+}
+    
+// Assignment operator
+iosMutableDictionary & iosMutableDictionary::operator = (const iosMutableDictionary &that)
+{
+    dict = [[NSMutableDictionary alloc] initWithDictionary:that.dict];
+}
+
+iosMutableDictionary::~iosMutableDictionary()
+{
+    dict = nil;
+}
+
+/// Remove the given field by name
+void iosMutableDictionary::removeField(const std::string &name)
+{
+    [dict removeObjectForKey:StdStringToString(name)];
+}
+
+/// Set field as int
+void iosMutableDictionary::setInt(const std::string &name,int val)
+{
+    dict[StdStringToString(name)] = @(val);
+}
+
+/// Set field as 64 bit unique value
+void iosMutableDictionary::setIdentifiable(const std::string &name,SimpleIdentity val)
+{
+    dict[StdStringToString(name)] = @(val);
+}
+
+/// Set field as double
+void iosMutableDictionary::setDouble(const std::string &name,double val)
+{
+    dict[StdStringToString(name)] = @(val);
+}
+
+/// Set field as string
+void iosMutableDictionary::setString(const std::string &name,const std::string &val)
+{
+    dict[StdStringToString(name)] = StdStringToString(val);
+}
+
+// Merge in key-value pairs from another dictionary
+void iosMutableDictionary::addEntries(const Dictionary *other)
+{
+    
+}
+
+}
