a8f06675c8430636d6c8d785f2111e5a2dc1c078
Antonio <antonio@igenius.ai>
2019-11-08 14:18:06 +0100

    migarated AutoTesterAndroid to AndroidX

--- android/apps/AutoTesterAndroid/app/src/main/java/com/mousebirdconsulting/autotester/ResultActivity.java
+++ android/apps/AutoTesterAndroid/app/src/main/java/com/mousebirdconsulting/autotester/ResultActivity.java
@@ -2,17 +2,18 @@ package com.mousebirdconsulting.autotester;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.mousebirdconsulting.autotester.Framework.MaplyTestResult;
 
