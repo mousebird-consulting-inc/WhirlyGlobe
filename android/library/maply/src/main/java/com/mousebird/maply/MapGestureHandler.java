ec044763f9514b7b3c7be52d04092c16cb2e10e7
Steve Gifford <sjg@mousebirdconsulting.com>
2019-12-02 10:54:22 -0800

    Allow zooming even if we're outside the valid bounds.

--- android/library/maply/src/main/java/com/mousebird/maply/MapGestureHandler.java
+++ android/library/maply/src/main/java/com/mousebird/maply/MapGestureHandler.java
@@ -198,7 +198,7 @@ public class MapGestureHandler
 				Point3d pos = maplyControl.mapView.getLoc();
 				mapView.cancelAnimation();
 				Point3d newPos = new Point3d(pos.getX(),pos.getY(),startZ*scale);
-				if (withinBounds(mapView,maplyControl.getViewSize(),newPos,maplyControl.viewBounds)) {
+				if (withinBounds(mapView,maplyControl.getViewSize(),newPos,maplyControl.viewBounds) || scale < 1.0) {
 					double newZ = newPos.getZ();
 					newZ = Math.min(newZ,zoomLimitMax);
 					newZ = Math.max(newZ,zoomLimitMin);
