a34992a0d7c9d11f8dc10a4ec1d787967902d0d4
Steve Gifford <sjg@mousebirdconsulting.com>
2020-03-05 15:30:07 -0800

    Broke Android.  Fixed.

--- android/library/maply/jni/src/scene/Scene_jni.cpp
+++ android/library/maply/jni/src/scene/Scene_jni.cpp
@@ -162,7 +162,7 @@ JNIEXPORT void JNICALL Java_com_mousebird_maply_Scene_addRenderTargetNative
         
         ChangeSet changes;
         RGBAColor color(r,g,b,a);
-        changes.push_back(new AddRenderTargetReq(renderTargetID,width,height,texID,clearEveryFrame,blend,color,0.0));
+        changes.push_back(new AddRenderTargetReq(renderTargetID,width,height,texID,clearEveryFrame,blend,color,0.0,RenderTargetMipmapNone,false));
         
         scene->addChangeRequests(changes);
     }
