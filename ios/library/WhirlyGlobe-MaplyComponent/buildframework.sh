1e5839b0461055df4e150308ebdfc60d0501b433
Tim Sylvester <tim@mousebirdconsulting.com>
2020-11-02 13:53:16 -0800

    Apply updates AS claims necessary to build. Update build script to account for new devices and architectures.

--- ios/library/WhirlyGlobe-MaplyComponent/buildframework.sh
+++ ios/library/WhirlyGlobe-MaplyComponent/buildframework.sh
@@ -9,7 +9,8 @@
 BUILT_PRODUCTS_SIMULATOR=`xcodebuild -target WhirlyGlobeMaplyComponent -scheme WhirlyGlobeMaplyComponent -configuration Release -sdk iphonesimulator -showBuildSettings OTHER_CFLAGS='-fembed-bitcode' | grep -m 1 "BUILT_PRODUCTS_DIR" | grep -oEi "\/.*"`
 BUILT_PRODUCTS_IPHONEOS=`xcodebuild -target WhirlyGlobeMaplyComponent -scheme WhirlyGlobeMaplyComponent -configuration Release -sdk iphoneos -showBuildSettings OTHER_CFLAGS='-fembed-bitcode' | grep -m 1 "BUILT_PRODUCTS_DIR" | grep -oEi "\/.*"`
 
-xcodebuild -target WhirlyGlobeMaplyComponent -scheme WhirlyGlobeMaplyComponent -configuration Archive -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPad Air (3rd generation)' OTHER_CFLAGS='-fembed-bitcode' clean build
+DEST="platform=iOS Simulator,name=iPad (8th generation)"
+xcodebuild -target WhirlyGlobeMaplyComponent -scheme WhirlyGlobeMaplyComponent -configuration dddhive -sdk iphonesimulator -destination "$DEST" OTHER_CFLAGS='-fembed-bitcode' clean build
 xcodebuild -target WhirlyGlobeMaplyComponent -scheme WhirlyGlobeMaplyComponent -configuration Archive -sdk iphoneos -DONLY_ACTIVE_ARCH=NO OTHER_CFLAGS='-fembed-bitcode'
 
 # name and build location
@@ -42,6 +43,11 @@ echo "Framework: Creating library..."
 # lipo -create build/Debug-iphoneos/libWhirlyGlobeLib.a build/Debug-iphonesimulator/libWhirlyGlobeLib.a -output "$FRAMEWORK_DIR/Versions/Current/$FRAMEWORK_NAME"
 echo "  Linking libraries in $BUILT_PRODUCTS_IPHONEOS and $BUILT_PRODUCTS_SIMULATOR"
 lipo -create $BUILT_PRODUCTS_IPHONEOS/WhirlyGlobeMaplyComponent.framework/WhirlyGlobeMaplyComponent $BUILT_PRODUCTS_SIMULATOR/WhirlyGlobeMaplyComponent.framework/WhirlyGlobeMaplyComponent -output "$FRAMEWORK_DIR/${FRAMEWORK_NAME}"
+
+if [ $? -ne 0 ]; then
+    # Lipo failed, assume that's because the outputs are the same architecture, and just use one as-is
+    cp "$BUILT_PRODUCTS_IPHONEOS/${FRAMEWORK_NAME}.framework/${FRAMEWORK_NAME}" "$FRAMEWORK_DIR/${FRAMEWORK_NAME}"
+fi
 
 # lipo -create "${PROJECT_DIR}/build/${BUILD_STYLE}-iphoneos/lib${PROJECT_NAME}.a" "${PROJECT_DIR}/build/${BUILD_STYLE}-iphonesimulator/lib${PROJECT_NAME}.a" -o "$FRAMEWORK_DIR/Versions/Current/$FRAMEWORK_NAME"
 
