2359b6e0e473b18c2cf651e5392f48ebf5a1fe86
Tim Sylvester <tim@mousebirdconsulting.com>
2021-01-18 17:25:26 -0800

    Add a hook for rewriting the stylesheet in test cases.

--- ios/library/WhirlyGlobe-MaplyComponent/src/helpers/MapboxKindaMap.swift
+++ ios/library/WhirlyGlobe-MaplyComponent/src/helpers/MapboxKindaMap.swift
@@ -47,7 +47,10 @@ public class MapboxKindaMap {
     //  font name in the style.  Font names in the style often don't map
     //  directly to local font names.
     public var fontOverride : (_ name: String) -> UIFontDescriptor? = { _ in return nil }
-    
+
+    // If set, we'll run the stylesheet JSON through it before loading it into a style delegate
+    public var preStyle: (_ : [AnyHashable:Any]) -> [AnyHashable:Any] = { $0 }
+
     // If set, this will be called right after everything is set up
     // This is after all the configuration files are fetched so
     //  you can make any final tweaks to loading objects here
@@ -326,18 +329,32 @@ public class MapboxKindaMap {
             // Go get the style sheet (this will also handle local
             let fetchIdx = self.outstandingFetches.count
             let dataTask = URLSession.shared.dataTask(with: self.makeURLRequest(styleURL)) { (data, _, error) in
-                guard error == nil, let data = data else {
+                guard error == nil, var data = data else {
                     print("Error fetching style sheet:\n\(String(describing: error))")
                     
                     self.stop()
                     return
                 }
-                
+
+                var jsonDict: [AnyHashable: Any]?
+                do {
+                    let result = try JSONSerialization.jsonObject(with: data)
+                    jsonDict = result as? [AnyHashable: Any]
+                    if jsonDict != nil {
+                        jsonDict = self.preStyle(jsonDict!)
+                        if let jd = jsonDict {
+                            data = try JSONSerialization.data(withJSONObject: jd)
+                        }
+                    }
+                } catch {
+                    print("Failed to parse stylesheet: \(String(describing: error))")
+                }
+
                 DispatchQueue.main.async {
                     self.outstandingFetches[fetchIdx] = nil
-                    guard let styleSheet = MapboxVectorStyleSet(json: data,
-                                                          settings: self.styleSettings,
-                                                            viewC: viewC) else {
+                    guard let styleSheet = MapboxVectorStyleSet(dict: jsonDict!,
+                                                                settings: self.styleSettings,
+                                                                viewC: viewC) else {
                         print("Failed to parse style sheet")
                         self.stop()
                         return
