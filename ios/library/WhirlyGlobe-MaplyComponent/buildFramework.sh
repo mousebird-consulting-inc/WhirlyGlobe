e0e365a1bdb0cd6ed9ca037d8735e95a2b525afa
Tim Sylvester <tim@mousebirdconsulting.com>
2021-05-03 16:16:36 -0700

    Re-enable simulator in framework build

--- ios/library/WhirlyGlobe-MaplyComponent/buildFramework.sh
+++ ios/library/WhirlyGlobe-MaplyComponent/buildFramework.sh
@@ -12,7 +12,7 @@ echo iPhoneOS products: $BUILT_PRODUCTS_IPHONEOS
 # simulator build disabled for now to stay under CI time limit
 #echo Building for simulator...
 #DEST="platform=iOS Simulator,name=iPad (8th generation)"
-#xcodebuild -target WhirlyGlobeMaplyComponent -scheme WhirlyGlobeMaplyComponent -configuration Archive -sdk iphonesimulator -destination "$DEST" OTHER_CFLAGS='-fembed-bitcode' build
+xcodebuild -target WhirlyGlobeMaplyComponent -scheme WhirlyGlobeMaplyComponent -configuration Archive -sdk iphonesimulator -destination "$DEST" OTHER_CFLAGS='-fembed-bitcode' build
 echo Building for iPhoneOS
 xcodebuild -target WhirlyGlobeMaplyComponent -scheme WhirlyGlobeMaplyComponent -configuration Archive -sdk iphoneos -DONLY_ACTIVE_ARCH=NO OTHER_CFLAGS='-fembed-bitcode'
 
