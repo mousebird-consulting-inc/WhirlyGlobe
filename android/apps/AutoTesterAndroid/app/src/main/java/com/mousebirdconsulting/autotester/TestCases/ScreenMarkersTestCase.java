976397540345e0dcae2d67d780f13e7eebbbe547
Tim Sylvester <tim@mousebirdconsulting.com>
2021-05-04 15:06:13 -0700

    Adjust screen marker test cases to check the behavior of overlapping markers with identical priority

--- android/apps/AutoTesterAndroid/app/src/main/java/com/mousebirdconsulting/autotester/TestCases/ScreenMarkersTestCase.java
+++ android/apps/AutoTesterAndroid/app/src/main/java/com/mousebirdconsulting/autotester/TestCases/ScreenMarkersTestCase.java
@@ -26,24 +26,16 @@ import java.util.ArrayList;
  */
 public class ScreenMarkersTestCase extends MaplyTestCase
 {
-	private ArrayList<ComponentObject> componentObjects = new ArrayList<>();
-
 	public ScreenMarkersTestCase(Activity activity) {
-		super(activity);
-		setTestName("Screen Markers");
+		super(activity, "Screen Markers", TestExecutionImplementation.Both);
 		setDelay(1000);
-		this.implementation = TestExecutionImplementation.Both;
-	}
-
-	public ArrayList<ComponentObject> getComponentObjects() {
-		return componentObjects;
 	}
 
 	@Override
 	public boolean setUpWithMap(MapController mapVC) throws Exception {
-		VectorsTestCase baseView = new VectorsTestCase(getActivity());
-		baseView.setUpWithMap(mapVC);
-		insertMarkers(baseView.getVectors(), mapVC);
+		baseCase = new VectorsTestCase(getActivity());
+		baseCase.setUpWithMap(mapVC);
+		insertMarkers(baseCase.getVectors(), mapVC);
 		Point2d loc = Point2d.FromDegrees(-3.6704803, 40.5023056);
 		mapVC.setPositionGeo(loc.getX(), loc.getX(), 2);
 		return true;
@@ -51,33 +43,52 @@ public class ScreenMarkersTestCase extends MaplyTestCase
 
 	@Override
 	public boolean setUpWithGlobe(GlobeController globeVC) throws Exception {
-		VectorsTestCase baseView = new VectorsTestCase(getActivity());
-		baseView.setUpWithGlobe(globeVC);
-		insertMarkers(baseView.getVectors(), globeVC);
+		baseCase = new VectorsTestCase(getActivity());
+		baseCase.setUpWithGlobe(globeVC);
+		insertMarkers(baseCase.getVectors(), globeVC);
 		Point2d loc = Point2d.FromDegrees(-3.6704803, 40.5023056);
 		globeVC.animatePositionGeo(loc.getX(), loc.getX(), 0.9, 1);
 		return true;
 	}
 
+	@Override
+	public void shutdown() {
+		controller.removeObjects(componentObjects, RenderControllerInterface.ThreadMode.ThreadCurrent);
+		componentObjects.clear();
+		if (baseCase != null) {
+			baseCase.shutdown();
+			baseCase = null;
+		}
+		super.shutdown();
+	}
+
 	private void insertMarkers(ArrayList<VectorObject> vectors, BaseController baseVC) {
 		MarkerInfo markerInfo = new MarkerInfo();
-		Bitmap icon = BitmapFactory.decodeResource(getActivity().getResources(), R.drawable.testtarget);
-		MaplyTexture tex = baseVC.addTexture(icon,null, RenderControllerInterface.ThreadMode.ThreadCurrent);
+		Bitmap[] icons = new Bitmap[]{
+				BitmapFactory.decodeResource(getActivity().getResources(), R.drawable.testtarget),
+				BitmapFactory.decodeResource(getActivity().getResources(), R.drawable.teardrop),
+		};
+		MaplyTexture[] textures = new MaplyTexture[] {
+				baseVC.addTexture(icons[0],null, RenderControllerInterface.ThreadMode.ThreadCurrent),
+				baseVC.addTexture(icons[1],null, RenderControllerInterface.ThreadMode.ThreadCurrent),
+		};
 //		markerInfo.setMinVis(0.f);
 //		markerInfo.setMaxVis(2.5f);
-//		markerInfo.setClusterGroup(0);
-		markerInfo.setLayoutImportance(1.f);
 
-		ArrayList<ScreenMarker> markers = new ArrayList<ScreenMarker>();
+		ArrayList<ScreenMarker> markers = new ArrayList<>();
 		for (VectorObject vector : vectors) {
-			ScreenMarker marker = new ScreenMarker();
-			marker.tex = tex;
 			Point2d centroid = vector.centroid();
 			if (centroid != null) {
+				ScreenMarker marker = new ScreenMarker();
+				marker.tex = textures[markers.size() % textures.length];
 				marker.loc = centroid;
 				marker.size = new Point2d(128, 128);
 				marker.rotation = Math.random() * 2.f * Math.PI;
 				marker.selectable = true;
+
+				// Use the layout engine for some of the markers and not others
+				marker.layoutImportance = (centroid.getX() < 0) ? 1 : Float.MAX_VALUE;
+
 //				marker.offset = new Point2d(0.0,-200);
 //				marker.layoutSize = new Point2d(0.0, 64.0);
 				AttrDictionary attrs = vector.getAttributes();
@@ -93,4 +104,7 @@ public class ScreenMarkersTestCase extends MaplyTestCase
 			componentObjects.add(object);
 		}
 	}
+
+	private VectorsTestCase baseCase;
+	private final ArrayList<ComponentObject> componentObjects = new ArrayList<>();
 }
