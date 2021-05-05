3a5a178a5239d03f50de5c070e2325fe09502244
Tim Sylvester <tim@mousebirdconsulting.com>
2021-05-05 14:09:07 -0700

    Fix build problems

--- android/apps/AutoTesterAndroid/app/src/main/java/com/mousebirdconsulting/autotester/TestCases/VectorStyleTestCase.kt
+++ android/apps/AutoTesterAndroid/app/src/main/java/com/mousebirdconsulting/autotester/TestCases/VectorStyleTestCase.kt
@@ -1,9 +1,8 @@
-/*
- *  VectorStyleTestCase.kt
+/*  VectorStyleTestCase.kt
  *  WhirlyGlobeLib
  *
  *  Created by Steve Gifford.
- *  Copyright 2011-2019 mousebird consulting
+ *  Copyright 2011-2021 mousebird consulting
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -15,36 +14,21 @@
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- *
  */
 package com.mousebirdconsulting.autotester.TestCases
 
 import android.app.Activity
-import android.content.Context
 import com.mousebird.maply.*
-import com.mousebirdconsulting.autotester.ConfigOptions
 import com.mousebirdconsulting.autotester.Framework.MaplyTestCase
-import java.io.File
-import android.content.ContextWrapper
-import android.graphics.Color
 import okio.*
-import java.io.FileOutputStream
-import java.io.IOException
-import java.nio.charset.Charset
-import java.util.*
-
 
 /**
- * Loads the USA geojon and applies a simple style.
+ * Loads the USA geojson and applies a simple style.
  */
-class VectorStyleTestCase : MaplyTestCase {
+class VectorStyleTestCase(activity: Activity) :
+        MaplyTestCase(activity, "Vector Style Test", TestExecutionImplementation.Both) {
 
-    constructor(activity: Activity) : super(activity) {
-        setTestName("Vector Style Test")
-        implementation = TestExecutionImplementation.Both
-    }
-
-    fun setupOverlay(control: BaseController) {
+    private fun setupOverlay(control: BaseController) {
         val json = activity.assets.open("country_json_50m/USA.geojson").use { stream ->
             stream.source().use { source ->
                 source.buffer().use { buffer ->
@@ -54,31 +38,40 @@ class VectorStyleTestCase : MaplyTestCase {
         }
 
         val vecObj = VectorObject.createFromGeoJSON(json)
-
         val styleGen = VectorStyleSimpleGenerator(control)
-        VectorStyleProcessor.UseStyle(arrayOf(vecObj),styleGen,control)
+        VectorStyleProcessor.UseStyle(arrayOf(vecObj),styleGen,control)?.let { cos ->
+            objs.addAll(cos)
+        }
     }
 
-    var baseCase: CartoLightTestCase? = null
-
     override fun setUpWithMap(mapVC: MapController?): Boolean {
-        baseCase = CartoLightTestCase(mapVC!!.getActivity())
-        baseCase?.setUpWithMap(mapVC)
-
-        setupOverlay(mapVC)
-
+        mapVC?.activity?.let { activity ->
+            baseCase = CartoLightTestCase(activity).apply {
+                setUpWithMap(mapVC)
+            }
+            setupOverlay(mapVC)
+        }
         return true
     }
 
     override fun setUpWithGlobe(globeVC: GlobeController?): Boolean {
-        baseCase = CartoLightTestCase(globeVC!!.getActivity())
-        baseCase?.setUpWithGlobe(globeVC)
-
-        setupOverlay(globeVC)
-
+        globeVC?.activity?.let { activity ->
+            baseCase = CartoLightTestCase(activity).apply {
+                setUpWithGlobe(globeVC)
+            }
+            setupOverlay(globeVC)
+        }
         return true
     }
-
-    private val MBTILES_DIR = "mbtiles"
-
+    
+    override fun shutdown() {
+        controller?.removeObjects(objs, ThreadMode.ThreadCurrent)
+        objs.clear()
+        baseCase?.shutdown()
+        baseCase = null
+        super.shutdown()
+    }
+    
+    private var baseCase: MaplyTestCase? = null
+    private val objs = ArrayList<ComponentObject>()
 }
