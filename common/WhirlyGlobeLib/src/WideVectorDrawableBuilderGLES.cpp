bae131074b51c3da4d1d4925f600058314d9864a
Tim Sylvester <tim@mousebirdconsulting.com>
2021-05-06 15:53:00 -0700

    Set wide vector offset from expression. "Real" pixel scale didn't match iOS, it was using the larger of the width and height instead of the smaller.

--- common/WhirlyGlobeLib/src/WideVectorDrawableBuilderGLES.cpp
+++ common/WhirlyGlobeLib/src/WideVectorDrawableBuilderGLES.cpp
@@ -35,10 +35,11 @@ void WideVectorTweakerGLES::tweakForFrame(Drawable *inDraw,RendererFrameInfo *fr
         return;
     }
 
-    const double scale = std::max(frameInfo->sceneRenderer->framebufferWidth,frameInfo->sceneRenderer->framebufferHeight);
-    const double screenSize = frameInfo->screenSizeInDisplayCoords.x();
-    const double pixDispSize = std::min(frameInfo->screenSizeInDisplayCoords.x(),frameInfo->screenSizeInDisplayCoords.y()) / scale;
-    const double texScale = scale / (screenSize * texRepeat);
+    const double frameSize = std::min(frameInfo->sceneRenderer->framebufferWidth, frameInfo->sceneRenderer->framebufferHeight);
+    const double screenSize = std::min(frameInfo->screenSizeInDisplayCoords.x(), frameInfo->screenSizeInDisplayCoords.y());
+    const double screenWidth = frameInfo->screenSizeInDisplayCoords.x();
+    const double pixDispScale = screenSize / frameSize;
+    const double texScale = frameSize / (screenWidth * texRepeat);
     const float zoom = (opacityExp || colorExp || widthExp) ? getZoom(*inDraw,*frameInfo->scene,0.0f) : 0.0f;
 
     Vector4f c = colorExp ? colorExp->evaluateF(zoom,color) : color.asRGBAVecF();
@@ -49,16 +50,22 @@ void WideVectorTweakerGLES::tweakForFrame(Drawable *inDraw,RendererFrameInfo *fr
     }
 
     // Multiply the alpha through, otherwise you just get the max color
-    c[0] *= c[3];  c[1] *= c[3];  c[2] *= c[3];
-    const RGBAColor newC = RGBAColor::FromUnitFloats(&c[0]);
-    basicDraw->setOverrideColor(newC);
+    c *= c.w();
+
+    basicDraw->setOverrideColor(RGBAColor(c));
 
     const float width = (widthExp ? widthExp->evaluate(zoom, lineWidth) : lineWidth) + 2 * edgeSize;
     basicDraw->setUniform(u_w2NameID, width);
-    basicDraw->setUniform(u_Realw2NameID, (float)(pixDispSize * width));
+    basicDraw->setUniform(u_Realw2NameID, (float)(pixDispScale * width));
 
     basicDraw->setUniform(u_EdgeNameID, edgeSize);
     basicDraw->setUniform(u_texScaleNameID, (float)texScale);
+
+    if (offsetSet)
+    {
+        const float theOffset = offsetExp ? offsetExp->evaluate(zoom, offset) : offset;
+        basicDraw->setUniform(u_wideOffsetNameID, theOffset);
+    }
 }
 
 WideVectorDrawableBuilderGLES::WideVectorDrawableBuilderGLES(const std::string &name,const SceneRenderer *sceneRenderer,Scene *scene) :
@@ -116,7 +123,9 @@ uniform mat4  u_mvNormalMatrix;
 uniform float u_fade;
 uniform float u_w2;
 uniform float u_real_w2;
+uniform float u_edge;
 uniform float u_texScale;
+uniform float u_wideOffset;
 
 attribute vec3 a_position;
 attribute vec3 a_normal;
@@ -137,9 +146,11 @@ void main()
     float t0 = a_c0 * u_real_w2;
     t0 = clamp(t0,-4.0,5.0);
     vec3 dir = normalize(a_p1 - a_position);
+    float centerLineSign = a_offset.z;
+    float realCenterLine = centerLineSign * u_wideOffset * u_real_w2 / u_w2;
     vec3 realPosOffset = (a_p1 - a_position) * t0 +
                     dir * u_real_w2 * a_offset.y +
-                    a_n0 * u_real_w2 +
+                    a_n0 * (realCenterLine + u_real_w2) +
                     a_n0 * u_real_w2 * a_offset.x;
     float texPos = ((a_texinfo.z - a_texinfo.y) * t0 + a_texinfo.y + a_texinfo.w * u_real_w2) * u_texScale;
     v_texCoord = vec2(a_texinfo.x, texPos);
