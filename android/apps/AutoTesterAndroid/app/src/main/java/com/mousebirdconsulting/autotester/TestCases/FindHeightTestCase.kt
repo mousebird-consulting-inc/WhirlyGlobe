a8cb043598bcd69e19be8635357f863ecd14231a
Tim Sylvester <tim@mousebirdconsulting.com>
2021-10-01 13:16:15 -0700

    Fix initialization race that made `findHeight` not work sometimes.

--- android/apps/AutoTesterAndroid/app/src/main/java/com/mousebirdconsulting/autotester/TestCases/FindHeightTestCase.kt
+++ android/apps/AutoTesterAndroid/app/src/main/java/com/mousebirdconsulting/autotester/TestCases/FindHeightTestCase.kt
@@ -1,7 +1,6 @@
 package com.mousebirdconsulting.autotester.TestCases
 
 import android.app.Activity
-import android.os.Handler
 import com.mousebird.maply.*
 import com.mousebirdconsulting.autotester.Framework.MaplyTestCase
 
@@ -11,44 +10,54 @@ import com.mousebirdconsulting.autotester.Framework.MaplyTestCase
 class FindHeightTestCase(activity: Activity) : MaplyTestCase(activity, "Find Height") {
     
     // Run a findHeight
-    fun runDelayedFind(vc: BaseController) {
-        Handler().postDelayed(Runnable {
+    private fun runDelayedFind(vc: BaseController) {
+        vc.addPostSurfaceRunnable {
             val bbox = Mbr(Point2d.FromDegrees(7.05090689853, 47.7675500593),
-                    Point2d.FromDegrees(8.06813647023, 49.0562323851))
-            val center = Point2d.FromDegrees((7.05090689853+8.06813647023)/2, (47.7675500593+49.0562323851)/2)
+                           Point2d.FromDegrees(8.06813647023, 49.0562323851))
+            val center = Point2d.FromDegrees((7.05090689853+8.06813647023)/2,
+                                              (47.7675500593+49.0562323851)/2)
             if (vc is GlobeController) {
                 val height = vc.findHeightToViewBounds(bbox, center)
-                vc.animatePositionGeo(center.x,center.y,height,1.0)
+                vc.animatePositionGeo(center.x,center.y,height,5.0)
             } else if (vc is MapController) {
                 val height = vc.findHeightToViewBounds(bbox, center)
-                vc.animatePositionGeo(center.x,center.y,height,1.0)
+                vc.animatePositionGeo(center.x,center.y,height,5.0)
             }
-        }, 1000)
+        }
     }
 
-    var baseCase : MaplyTestCase? = null
+    var baseCase = CartoLightTestCase(activity)
 
     override fun setUpWithMap(mapVC: MapController?): Boolean {
-        baseCase = CartoLightTestCase(getActivity())
-        baseCase?.setUpWithMap(mapVC)
+        if (mapVC == null) {
+            return false
+        }
+        baseCase.setUpWithMap(mapVC)
 
         val loc = Point2d.FromDegrees(-98.58, 39.83)
-        mapVC?.setPositionGeo(loc.x,loc.y,1.0)
+        mapVC.setPositionGeo(loc.x,loc.y,1.0)
 
-        runDelayedFind(mapVC!!)
+        runDelayedFind(mapVC)
 
         return true
     }
 
     override fun setUpWithGlobe(globeVC: GlobeController?): Boolean {
-        baseCase = CartoLightTestCase(getActivity())
-        baseCase?.setUpWithGlobe(globeVC)
+        if (globeVC == null) {
+            return false
+        }
+        baseCase.setUpWithGlobe(globeVC)
 
         val loc = Point2d.FromDegrees(-98.58, 39.83)
-        globeVC?.setPositionGeo(loc.x,loc.y,1.5)
+        globeVC.setPositionGeo(loc.x,loc.y,1.5)
 
-        runDelayedFind(globeVC!!)
+        runDelayedFind(globeVC)
 
         return true
+    }
+    
+    override fun shutdown() {
+        baseCase.shutdown()
+        super.shutdown()
     }
 }
