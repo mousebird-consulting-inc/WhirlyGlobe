e60aa36d55a6fb607c47843a8e8976217e25ce15
Tim Sylvester <tim@mousebirdconsulting.com>
2022-01-31 17:32:47 -0800

    Add some wide vector variations to the active object case for memory leak troubleshooting, plus demonstrating the miter/bevel stuff at various angles on open and closed linears.

--- ios/apps/AutoTester/AutoTester/testCases/ActiveObjectTestCase.swift
+++ ios/apps/AutoTester/AutoTester/testCases/ActiveObjectTestCase.swift
@@ -56,7 +56,7 @@ class ActiveObjectTestCase : MaplyTestCase {
 class SimpleActiveObject: MaplyActiveObject {
     init(viewController viewC: MaplyBaseViewController, image inTex: MaplyTexture?) {
         super.init(viewController: viewC)
-        tex = inTex
+        markerTex = inTex
     }
 
     override func hasUpdate() -> Bool {
@@ -65,31 +65,74 @@ class SimpleActiveObject: MaplyActiveObject {
     
     override func update(forFrame frameInfo: UnsafeMutableRawPointer?) {
         guard let vc = viewC else { return }
-        // Delete it
-        if let compObj = compObj {
-            vc.remove([compObj], mode: .current)
-            self.compObj = nil
-        }
+
+        let oldObjs = [MaplyComponentObject](compObjs)
+        compObjs.removeAll()
 
         let offset = MaplyCoordinateMakeWithDegrees(Float(-1.0 + 2.0 * drand48()),
                                                     Float(-1.0 + 2.0 * drand48()))
-        coord = MaplyCoordinate(x: coord.x + offset.x, y: coord.y + offset.y)
+        markerCoord = MaplyCoordinate(x: markerCoord.x + offset.x,
+                                      y: markerCoord.y + offset.y)
 
         let marker = MaplyScreenMarker()
-        marker.loc = coord
-        marker.image = tex
+        marker.loc = markerCoord
+        marker.image = markerTex
         marker.size = CGSize(width: 64, height: 64)
         marker.color = UIColor.init(white: 1.0, alpha: 0.5)
         marker.layoutImportance = MAXFLOAT
 
         let desc = [
-            kMaplyFade: false
+            kMaplyFade: false,
+            kMaplyEnable: false,
         ];
 
-        compObj = vc.addScreenMarkers([marker], desc: desc, mode: .current)
+        if let co = vc.addScreenMarkers([marker], desc: desc, mode: .current) {
+            compObjs.append(co)
+        }
+
+        for i in 0..<2 {
+            for j in 0..<2 {
+                let lat = 50.0 + Float(i + 2 * j)
+                let lon = Float(-110.0)
+                var pts = [
+                    MaplyCoordinateMakeWithDegrees(lon, lat),
+                    MaplyCoordinateMakeWithDegrees(lon + 1, lat),
+                    MaplyCoordinateMakeWithDegrees(lon + Float(wideLinePos), lat + 0.1),
+                    MaplyCoordinateMakeWithDegrees(lon, lat),
+                ]
+
+                let vec = MaplyVectorObject(lineString: &pts, numCoords: (j==0) ? 3 : 4)
+                let wideDesc = [
+                    kMaplyEnable: false,
+                    kMaplyColor: UIColor.red.withAlphaComponent(0.5),
+                    kMaplyVecWidth: 20,
+                    kMaplyWideVecEdgeFalloff: 5,
+                    kMaplyWideVecJoinType: kMaplyWideVecMiterJoin,
+                    kMaplyWideVecMiterLimit: miterLimit,
+                    kMaplyWideVecImpl: (i==0) ? kMaplyWideVecImplPerf : kMaplyWideVecImplDefault,
+                ]  as [String : Any]
+                if let co = vc.addWideVectors([vec], desc: wideDesc, mode: .current) {
+                    compObjs.append(co)
+                }
+            }
+        }
+
+        wideLinePos += wideLineDir * wideLineDif
+        if wideLinePos > 2 || wideLinePos < -2 {
+            wideLineDir = -wideLineDir
+            wideLinePos += wideLineDir * wideLineDif
+        }
+        
+        
+        vc.enable(compObjs, mode: .current)
+        vc.remove(oldObjs, mode: .current)
     }
 
-    private var coord = MaplyCoordinate(x: 0, y: 0)
-    private var compObj: MaplyComponentObject?
-    private var tex: MaplyTexture?
+    private var compObjs = [MaplyComponentObject]()
+    private var markerTex: MaplyTexture?
+    private var markerCoord = MaplyCoordinate(x: 0, y: 0)
+    private var wideLinePos = 0.0
+    private let wideLineDif = 0.01
+    private var wideLineDir = -1.0
+    private var miterLimit = 0.0
 }
